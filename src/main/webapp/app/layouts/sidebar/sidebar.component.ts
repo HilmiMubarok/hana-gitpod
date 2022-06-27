fbba2e7967e5255ee65e79c0706b9f40d10b0b28
Ricky <ricky_riq@yahoo.com>
2022-06-27 18:06:02 +0700

    .

--- src/main/webapp/app/layouts/sidebar/sidebar.component.ts
+++ src/main/webapp/app/layouts/sidebar/sidebar.component.ts
@@ -1,56 +1,79 @@
 import { FlatTreeControl } from '@angular/cdk/tree';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
+import { Router } from '@angular/router';
 import { Account } from 'app/core/auth/account.model';
 import { AccountService } from 'app/core/auth/account.service';
 import { FlatNode, ISidebarMenuModel, SidebarMenuModel } from './sidebar.model';
+
+import * as lodash from 'lodash';
 
 @Component({
   selector: 'jhi-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit {
   public account: Account | null = null;
   private treeData: ISidebarMenuModel[] = [
     {
-      name: 'Fruit',
+      name: 'Menu 1',
       children: [
         {
-          name: 'Apple',
+          name: 'Facility Type',
           route: 'facility-type',
         },
         {
-          name: 'Banana',
-          route: 'facility-type',
+          name: 'Menu 1.2',
         },
         {
-          name: 'Fruit loops',
-          route: 'facility-type',
+          name: 'Menu 1.3',
+        },
+        {
+          name: 'Menu 1.4',
+        },
+        {
+          name: 'Menu 1.5',
         },
       ],
     },
     {
-      name: 'Vegetables',
+      name: 'Menu 2',
       children: [
         {
-          name: 'Green',
-          route: 'faciltiy-types',
+          name: 'Menu 2.1',
         },
         {
-          name: 'Orange',
-          route: 'faciltiy-types',
+          name: 'Menu 2.2',
+        },
+        {
+          name: 'Menu 2.3',
+        },
+        {
+          name: 'Menu 2.4',
+        },
+        {
+          name: 'Menu 2.5',
         },
       ],
     },
     {
-      name: 'Vegetables',
+      name: 'Menu 3',
       children: [
         {
-          name: 'Green',
+          name: 'Menu 3.1',
         },
         {
-          name: 'Orange',
+          name: 'Menu 3.2',
+        },
+        {
+          name: 'Menu 3.3',
+        },
+        {
+          name: 'Menu 3.4',
+        },
+        {
+          name: 'Menu 3.5',
         },
       ],
     },
@@ -70,12 +93,17 @@ export class SidebarComponent implements OnInit {
 
   public dataSource: any = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
-  constructor(private accountService: AccountService) {
+  constructor(private accountService: AccountService, private router: Router) {
     this.dataSource.data = this.treeData;
   }
 
   ngOnInit(): void {
     this.checkLogin();
+  }
+
+  ngAfterViewInit(): void {
+    // set open first node for treeview
+    this.treeControl.expand(this.treeControl.dataNodes[0]);
   }
 
   private checkLogin(): void {
@@ -84,6 +112,14 @@ export class SidebarComponent implements OnInit {
         this.account = account;
       }
     });
+  }
+
+  public getClassActive(route: string): string {
+    const currentUrl = this.router.url;
+    if (currentUrl.indexOf(route) > -1) {
+      return 'selected';
+    }
+    return null;
   }
 
   private transformer(node: SidebarMenuModel, lvl: number): any {
