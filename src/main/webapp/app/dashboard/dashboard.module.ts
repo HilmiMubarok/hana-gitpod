23e73845a4b4fcf7aaed21712b87c3b8fd4133ce
Ricky <ricky_riq@yahoo.com>
2022-07-07 11:51:04 +0700

    add chart

--- src/main/webapp/app/dashboard/dashboard.module.ts
+++ src/main/webapp/app/dashboard/dashboard.module.ts
@@ -6,6 +6,19 @@ import { DashboardComponent } from './dashboard.component';
 import { DASHBOARD_ROUTE } from './dashboard.route';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { ChartModule } from '@syncfusion/ej2-angular-charts';
+import {
+  CategoryService,
+  DateTimeService,
+  ScrollBarService,
+  ColumnSeriesService,
+  LineSeriesService,
+  ChartAnnotationService,
+  RangeColumnSeriesService,
+  StackingColumnSeriesService,
+  LegendService,
+  TooltipService,
+} from '@syncfusion/ej2-angular-charts';
 
 @NgModule({
   imports: [
@@ -14,7 +27,22 @@ import { MatDividerModule } from '@angular/material/divider';
     // angular material
     MatCardModule,
     MatDividerModule,
+
+    // syncfusion
+    ChartModule,
   ],
   declarations: [DashboardComponent],
+  providers: [
+    CategoryService,
+    DateTimeService,
+    ScrollBarService,
+    LineSeriesService,
+    ColumnSeriesService,
+    ChartAnnotationService,
+    RangeColumnSeriesService,
+    StackingColumnSeriesService,
+    LegendService,
+    TooltipService,
+  ],
 })
 export class DashboardModule {}
