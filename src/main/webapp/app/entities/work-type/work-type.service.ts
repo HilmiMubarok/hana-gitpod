805b5efe4db0a38f6d850a87c48eee7077da0ae1
Ricky <ricky_riq@yahoo.com>
2022-07-13 10:09:19 +0700

    change work type service target

--- src/main/webapp/app/entities/work-type/work-type.service.ts
+++ src/main/webapp/app/entities/work-type/work-type.service.ts
@@ -4,12 +4,13 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { ApplicationConfigService } from 'app/core/config/application-config.service';
 import { IWorkType } from './work-type.model';
 import { AbstractEntityService } from 'app/shared/base/abstract-entity.service';
+import { MICROSERVICENAME } from 'app/shared/constants/base.constants';
 
 @Injectable({ providedIn: 'root' })
 export class WorkTypeService extends AbstractEntityService<IWorkType> {
   constructor(protected http: HttpClient, protected applicationConfigService: ApplicationConfigService) {
     super(http);
-    this.resourceUrl = this.applicationConfigService.getEndpointFor('services/los/api/work-types');
+    this.resourceUrl = this.applicationConfigService.getEndpointFor(MICROSERVICENAME.MASTERCONTROL + '/api/work-types');
   }
 
   protected isNew(entity: IWorkType): boolean {
