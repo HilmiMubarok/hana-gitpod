ef177ebeb9226ac3f8ea9bb7c48374ec758e7efc
andi lauw <andi.safariansyah7@gmail.com>
2022-08-03 00:27:46 +0700

    make-table-grid

--- src/main/webapp/app/entities/credit-proposal/credit-proposal-risk-acceptance-criteria-component.ts
+++ src/main/webapp/app/entities/credit-proposal/credit-proposal-risk-acceptance-criteria-component.ts
@@ -1,109 +1,95 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AccountService } from 'app/core/auth/account.service';
-import { ITEMS_PER_PAGE } from 'app/config/pagination.constants';
-import { ICreditProposal } from './credit-proposal.model';
-import { CreditProposalService } from './credit-proposal.service';
-import { ConfirmationService, MessageService } from 'primeng/api';
-import { AbstractEntityEj2GridComponent } from 'app/shared/base/abstract-entity-ej2-grid.component';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { BaseDataUtils } from 'app/shared/base/base-data-utils.service';
-import { ParseLinks } from 'app/core/util/parse-links.service';
-import { AlertService } from 'app/core/util/alert.service';
-import { EventManager } from 'app/core/util/event-manager.service';
-import { AnimationSettingsModel, DialogComponent } from '@syncfusion/ej2-angular-popups';
-import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'jhi-credit-proposal-risk-acceptance-criteria',
   templateUrl: './credit-proposal-risk-acceptance-criteria-component.html',
   styleUrls: ['./credit-proposal.css'],
 })
-export class CreditProposalRiskAcceptanceCriteriaComponent extends AbstractEntityEj2GridComponent<ICreditProposal> {
-  @ViewChild('findCifDialog')
-  public findCifDialog: DialogComponent;
+export class CreditProposalRiskAcceptanceCriteriaComponent implements OnInit {
+  public data: Object[];
 
-  public cifNumber: string;
-  public visiblePrompt: Boolean = false;
-  public animationSettings: AnimationSettingsModel = {
-    effect: 'Zoom',
-  };
-
-  constructor(
-    protected creditProposalService: CreditProposalService,
-    protected parseLinks: ParseLinks,
-    protected alertService: AlertService,
-    public accountService: AccountService,
-    protected activatedRoute: ActivatedRoute,
-    protected dataUtils: BaseDataUtils,
-    protected router: Router,
-    protected eventManager: EventManager,
-    protected messageService: MessageService,
-    protected modalService: NgbModal,
-    protected confirmationService: ConfirmationService
-  ) {
-    super(
-      creditProposalService,
-      parseLinks,
-      accountService,
-      activatedRoute,
-      dataUtils,
-      router,
-      eventManager,
-      messageService,
-      confirmationService
-    );
-
-    this.parentRoute = '/credit-proposal';
-    this.listChangeEventName = 'creditProposalListModification';
-    this.entityKeyName = 'id';
-
-    this.routeData = this.activatedRoute.data.subscribe(data => {
-      this.page = data.pagingParams.page;
-      this.previousPage = data.pagingParams.page;
-      this.reverse = false;
-      this.predicate = 'createdDate';
-      activatedRoute.queryParams.subscribe(params => {
-        this.itemsPerPage = params['size'] || ITEMS_PER_PAGE;
-        this.first = (this.page - 1) * this.itemsPerPage || 0;
-      });
-    });
-    this.currentSearch =
-      this.activatedRoute.snapshot && this.activatedRoute.snapshot.params['search'] ? this.activatedRoute.snapshot.params['search'] : '';
-  }
-
-  get creditProposals() {
-    return this.items['result'];
-  }
-
-  set creditProposals(creditProposal: ICreditProposal[]) {
-    this.items['result'] = creditProposal;
-  }
-
-  public openPromptFindCIF(): void {
-    this.findCifDialog.show();
-  }
-
-  public hidePromptFindCIF(): void {
-    this.findCifDialog.hide();
-  }
-
-  public buttonFindCifDialog = [
-    {
-      click: this.hidePromptFindCIF.bind(this),
-      buttonModel: {
-        content: 'Close',
-      },
-    },
-  ];
-
-  public findCif(): void {
-    this.creditProposalService.findByCif(this.cifNumber).subscribe((res: HttpResponse<ICreditProposal>) => {
-      const result: ICreditProposal = res.body;
-      if (result) {
-        const redirectUri = '/credit-proposal/' + result[0].id + '/edit';
-        this.router.navigate([redirectUri]);
-      }
-    });
+  ngOnInit(): void {
+    this.data = data;
   }
 }
+export const data: Object[] = [
+  {
+    No: 1,
+    Parameter: 'Masuk Gang atau lebar jalan < 3 meter.',
+    Verified: !0,
+    value: 'gang',
+  },
+  {
+    No: 2,
+    Parameter: 'Hasil site visit, survey, trade checking, dan verifikasi perihal usaha debitur ke rumah dan usaha positif.',
+    Verified: !2,
+    value: 'p',
+  },
+  {
+    No: 3,
+    Parameter: 'Berada dekat induk gardu listrik atau saluran udara tegangan ekstra tinggi (SUTET) dengan jarak \u{2264} 50 meter.',
+    Verified: !3,
+  },
+  {
+    No: 4,
+    Parameter: 'Terkena banjir (hingga masuk ke dalam property/asset yang menjadi jaminan) setiap menjadi hujan besar.',
+    Verified: !4,
+  },
+  {
+    No: 5,
+    Parameter: 'Ada rencana tata kota yang akan menyebabkan terjadinya penggusuran property/asset yang menjadi jaminan.',
+    Verified: !5,
+  },
+  {
+    No: 6,
+    Parameter:
+      'Dijadikan rumah ibadah, sekolah, panti jompo, panti asuhan, rumah duka, rumah sakit atau prasarana lain yang bersifat sosial kemanusiaan.',
+    Verified: !6,
+  },
+  {
+    No: 7,
+    Parameter: 'Berlokasi dekat pemakaman umum (berjarak \u{2264} 200 meter).',
+    Verified: !7,
+  },
+  {
+    No: 8,
+    Parameter: 'Berlokasi dekat dengan Tempat Pembuangan Sampah Akhir (TPA) dengan jarak \u{2264} 1 km.',
+    Verified: !8,
+  },
+  {
+    No: 9,
+    Parameter: 'Diginakan dan atau diperuntukan (zoning) sebagai sawah/ladang/pertanian/rawa-rawa.',
+    Verified: !9,
+  },
+  {
+    No: 10,
+    Parameter: 'Jaminan merupakan kawasan cagar budaya.',
+    Verified: !10,
+  },
+  {
+    No: 11,
+    Parameter: 'SHM atau HGB atau SHMSRS di atas Hak Pengelolaan.',
+    Verified: !11,
+  },
+  {
+    No: 12,
+    Parameter:
+      'Sebagian area tanahnya digunakan untuk mendirikan Base Transceiver Station atau BTS (tidak termasuk BTS yang didirikan diatas bangunan).',
+    Verified: !12,
+  },
+  {
+    No: 13,
+    Parameter: 'Rumah sarang burung.',
+    Verified: !13,
+  },
+  {
+    No: 14,
+    Parameter: 'HGB atau MoU di atas Hak Milik orang lain (Perumnas).',
+    Verified: !14,
+  },
+  {
+    No: 15,
+    Parameter: 'Terletak di pinggir laut (bukan pantai) atau rel kereta api.',
+    Verified: !15,
+  },
+];
