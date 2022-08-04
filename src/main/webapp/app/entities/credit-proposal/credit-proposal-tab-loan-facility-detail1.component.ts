dd3d0e1709fbfe59f550d35853209268d79cfa87
rendayvirta96 <rendayvirta96@gmail.com>
2022-08-04 20:11:21 +0700

    Fix

---
+++ src/main/webapp/app/entities/credit-proposal/credit-proposal-tab-loan-facility-detail1.component.ts
@@ -0,0 +1,157 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AccountService } from 'app/core/auth/account.service';
+import { ITEMS_PER_PAGE } from 'app/config/pagination.constants';
+import { ICreditProposal } from './credit-proposal.model';
+import { CreditProposalService } from './credit-proposal.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { AbstractEntityEj2GridComponent } from 'app/shared/base/abstract-entity-ej2-grid.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BaseDataUtils } from 'app/shared/base/base-data-utils.service';
+import { ParseLinks } from 'app/core/util/parse-links.service';
+import { AlertService } from 'app/core/util/alert.service';
+import { EventManager } from 'app/core/util/event-manager.service';
+import { AnimationSettingsModel, DialogComponent } from '@syncfusion/ej2-angular-popups';
+import { HttpResponse } from '@angular/common/http';
+import { MatAccordion } from '@angular/material/expansion';
+
+@Component({
+  selector: 'jhi-credit-proposal-tab-loan-facility-detail1',
+  templateUrl: './credit-proposal-tab-loan-facility-detail1.component.html',
+})
+export class CreditProposalTabLoanFacilityDetail1Component {
+  public pageSettings ;
+  accordion: MatAccordion;
+
+  public data: object[] = [
+    { 
+      indexNum: '1',
+      applicationType: 'New',
+      facilityType: 'KMK',
+      subLimit: 'No',
+      ccy: 'IDR',
+      initialLimit: '1',
+      oS: '0',
+      changes: '8500000',
+      creditLimit: '8500000',
+      interestRate: 'JIBOR 3 MONTH + 4 %',
+      provision: '0.25%',
+      tenor: '36',
+      availableLimit: '0',
+      date:'28/07/2022',
+    },
+    { 
+      indexNum: '2',
+      applicationType: 'New',
+      facilityType: 'KMK',
+      subLimit: 'No',
+      ccy: 'IDR',
+      initialLimit: '2',
+      oS: '2',
+      changes: '7500000',
+      creditLimit: '7500000',
+      interestRate: 'JIBOR 3 MONTH + 4 %',
+      provision: '0.25%' ,
+      tenor: '36',
+      availableLimit: '0',
+      date:'28/07/2022',
+    }
+  ];
+  public tools: object = {
+    items: [
+      'FontName', 'FontSize', 'Bold', 'Italic', 'Underline', 'StrikeThrough','FontColor', 'BackgroundColor',
+      'OrderedList', 'UnorderedList',
+      'Indent', 'Outdent','SuperScript', 'SubScript', 'Alignments','CreateLink']
+      // 'Image', 'FileManager']
+  };
+  
+  
+  
+  
+  // @ViewChild('findCifDialog')
+  // public findCifDialog: DialogComponent;
+
+  // public cifNumber: string;
+  // public visiblePrompt: Boolean = false;
+  // public animationSettings: AnimationSettingsModel = {
+  //   effect: 'Zoom',
+  // };
+
+  // constructor(
+  //   protected creditProposalService: CreditProposalService,
+  //   protected parseLinks: ParseLinks,
+  //   protected alertService: AlertService,
+  //   public accountService: AccountService,
+  //   protected activatedRoute: ActivatedRoute,
+  //   protected dataUtils: BaseDataUtils,
+  //   protected router: Router,
+  //   protected eventManager: EventManager,
+  //   protected messageService: MessageService,
+  //   protected modalService: NgbModal,
+  //   protected confirmationService: ConfirmationService
+  // ) {
+  //   super(
+  //     creditProposalService,
+  //     parseLinks,
+  //     accountService,
+  //     activatedRoute,
+  //     dataUtils,
+  //     router,
+  //     eventManager,
+  //     messageService,
+  //     confirmationService
+  //   );
+
+  //   this.parentRoute = '/credit-proposal';
+  //   this.listChangeEventName = 'creditProposalListModification';
+  //   this.entityKeyName = 'id';
+
+  //   this.routeData = this.activatedRoute.data.subscribe(data => {
+  //     this.page = data.pagingParams.page;
+  //     this.previousPage = data.pagingParams.page;
+  //     this.reverse = false;
+  //     this.predicate = 'createdDate';
+  //     activatedRoute.queryParams.subscribe(params => {
+  //       this.itemsPerPage = params['size'] || ITEMS_PER_PAGE;
+  //       this.first = (this.page - 1) * this.itemsPerPage || 0;
+  //     });
+  //   });
+  //   this.currentSearch =
+  //     this.activatedRoute.snapshot && this.activatedRoute.snapshot.params['search'] ? this.activatedRoute.snapshot.params['search'] : '';
+  // }
+
+  // get creditProposals() {
+  //   return this.items['result'];
+  // }
+
+  // set creditProposals(creditProposal: ICreditProposal[]) {
+  //   this.items['result'] = creditProposal;
+  // }
+
+  // public openPromptFindCIF(): void {
+  //   this.findCifDialog.show();
+  // }
+
+  // public hidePromptFindCIF(): void {
+  //   this.findCifDialog.hide();
+  // }
+
+  // public buttonFindCifDialog = [
+  //   {
+  //     click: this.hidePromptFindCIF.bind(this),
+  //     buttonModel: {
+  //       content: 'Close',
+  //     },
+  //   },
+  // ];
+
+  // public findCif(): void {
+  //   this.creditProposalService.findByCif(this.cifNumber).subscribe((res: HttpResponse<ICreditProposal>) => {
+  //     const result: ICreditProposal = res.body;
+  //     if (result) {
+  //       const redirectUri = '/credit-proposal/' + result[0].id + '/edit';
+  //       this.router.navigate([redirectUri]);
+  //     }
+  //   });
+  // }
+}
