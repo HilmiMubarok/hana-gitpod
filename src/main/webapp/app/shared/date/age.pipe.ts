68630ecc73189f5b3f2ed8ad1dd7151cd68a918f
Ricky <ricky_riq@yahoo.com>
2022-08-04 14:59:23 +0700

    .

--- src/main/webapp/app/shared/date/age.pipe.ts
+++ src/main/webapp/app/shared/date/age.pipe.ts
@@ -6,6 +6,15 @@ import moment from 'moment';
 })
 export class AgePipe implements PipeTransform {
   transform(value: any): string {
+    if (value) {
+      const diff = moment().diff(moment(value).format('YYYY-MM-DD').toString(), 'years');
+      if (diff === 1) {
+        return diff.toString() + ' year';
+      }
+
+      return diff.toString() + ' years';
+    }
+
     return null;
   }
 }
