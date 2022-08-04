4242570f408a0c28f981eca69551b2ca9fd6d4bf
Michael Rosario <michaelrosario098@gmail.com>
2022-08-04 17:50:53 +0700

    no message

--- src/main/webapp/app/entities/credit-proposal/credit-proposal-approval-list.component.ts
+++ src/main/webapp/app/entities/credit-proposal/credit-proposal-approval-list.component.ts
@@ -13,14 +13,16 @@ import { AlertService } from 'app/core/util/alert.service';
 import { EventManager } from 'app/core/util/event-manager.service';
 import { AnimationSettingsModel, DialogComponent } from '@syncfusion/ej2-angular-popups';
 import { HttpResponse } from '@angular/common/http';
+import { AbstractEntityComponent } from 'app/shared/base/abstract-entity.component';
 
 @Component({
   selector: 'jhi-credit-proposal',
   templateUrl: './credit-proposal-approval-list.component.html',
   styleUrls: ['./credit-proposal.component.css'],
 })
-export class CreditProposalApprovalListComponent extends AbstractEntityEj2GridComponent<ICreditProposal> {
+export class CreditProposalApprovalListComponent extends AbstractEntityComponent<ICreditProposal> {
   title = 'mydummy-data';
+  public searchValue: string;
   public data: object[] = [
     {
       noId: '1',
@@ -66,90 +68,7 @@ export class CreditProposalApprovalListComponent extends AbstractEntityEj2GridCo
     },
   ];
 
-  @ViewChild('findCifDialog')
-  public findCifDialog: DialogComponent;
-
-  public cifNumber: string;
-  public visiblePrompt: Boolean = false;
-  public animationSettings: AnimationSettingsModel = {
-    effect: 'Zoom',
-  };
-
-  constructor(
-    protected creditProposalService: CreditProposalService,
-    protected parseLinks: ParseLinks,
-    protected alertService: AlertService,
-    public accountService: AccountService,
-    protected activatedRoute: ActivatedRoute,
-    protected dataUtils: BaseDataUtils,
-    protected router: Router,
-    protected eventManager: EventManager,
-    protected messageService: MessageService,
-    protected modalService: NgbModal,
-    protected confirmationService: ConfirmationService
-  ) {
-    super(
-      creditProposalService,
-      parseLinks,
-      accountService,
-      activatedRoute,
-      dataUtils,
-      router,
-      eventManager,
-      messageService,
-      confirmationService
-    );
-
-    this.parentRoute = '/credit-proposal';
-    this.listChangeEventName = 'creditProposalListModification';
-    this.entityKeyName = 'id';
-
-    this.routeData = this.activatedRoute.data.subscribe(data => {
-      this.page = data.pagingParams.page;
-      this.previousPage = data.pagingParams.page;
-      this.reverse = false;
-      this.predicate = 'createdDate';
-      activatedRoute.queryParams.subscribe(params => {
-        this.itemsPerPage = params['size'] || ITEMS_PER_PAGE;
-        this.first = (this.page - 1) * this.itemsPerPage || 0;
-      });
-    });
-    this.currentSearch =
-      this.activatedRoute.snapshot && this.activatedRoute.snapshot.params['search'] ? this.activatedRoute.snapshot.params['search'] : '';
-  }
-
-  get creditProposals() {
-    return this.items['result'];
-  }
-
-  set creditProposals(creditProposal: ICreditProposal[]) {
-    this.items['result'] = creditProposal;
-  }
-
-  public openPromptFindCIF(): void {
-    this.findCifDialog.show();
-  }
-
-  public hidePromptFindCIF(): void {
-    this.findCifDialog.hide();
-  }
-
-  public buttonFindCifDialog = [
-    {
-      click: this.hidePromptFindCIF.bind(this),
-      buttonModel: {
-        content: 'Close',
-      },
-    },
-  ];
-
-  public findCif(): void {
-    this.creditProposalService.findByCif(this.cifNumber).subscribe((res: HttpResponse<ICreditProposal>) => {
-      const result: ICreditProposal = res.body;
-      if (result) {
-        const redirectUri = '/credit-proposal/' + result[0].id + '/edit';
-        this.router.navigate([redirectUri]);
-      }
-    });
+  search(searchValue) {
+    console.log('this work');
   }
 }
