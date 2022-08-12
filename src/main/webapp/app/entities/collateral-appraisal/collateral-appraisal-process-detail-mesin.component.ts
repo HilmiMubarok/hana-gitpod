9a4c876925bd745551e9afc3e2c53ed8dc6f2557
kiki <rizkirinaldi999@yahoo.com>
2022-08-12 17:19:55 +0700

    credit appraisal proses detail mesin

--- src/main/webapp/app/entities/collateral-appraisal/collateral-appraisal-process-detail-mesin.component.ts
+++ src/main/webapp/app/entities/collateral-appraisal/collateral-appraisal-process-detail-mesin.component.ts
@@ -6,18 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./collateral-appraisal-process-detail-mesin.component.css'],
 })
 export class CollateralAppraisalDetailProcessMesinComponent {
+  public namaMsn?: string;
+  public tipeDoc?: string;
+  public noDoc?: string;
+  public tanggal?: string;
+  public from?: string;
+  public amount?: string;
   public state = 'idle';
   public dialogVisible = false;
+  public dialogAddVisible = false;
   public width = '90%';
-  public height = '90%';
+  public height = 'auto';
   public animationSettings = { effect: 'Zoom', duration: 400, delay: 0 };
   public items = [
     {
       indexNum: 1,
+      namaMsn: 'Mesin CNC Vertical Machining Centre 2017',
+      tipeDoc: 'MFG',
+      noDoc: '1VMC085XX065',
+      tanggal: '12/02/2018',
+      from: 'Debitur',
+      amount: '278.000',
+
+      // merkMsn: 'Merek Mesin',
+      // buatan: 'Buatan',
+      // tahun: 'Tahun',
+      // typeMdl: 'Type Model',
+      // jenis: 'MFG Date',
+      // spesifikasi: 'Spesifikasi',
+      // kondisi: 'Kondisi',
+      // keterangan: 'Keterangan',
     },
   ];
 
   public onEdit(data: any): void {
+    this.namaMsn = data.namaMsn;
+    this.tipeDoc = data.tipeDoc;
+    this.noDoc = data.noDoc;
+    this.tanggal = data.tanggal;
+    this.from = data.from;
+    this.amount = data.amount;
+
     this.state = 'idle';
     this.dialogVisible = true;
   }
@@ -28,16 +57,46 @@ export class CollateralAppraisalDetailProcessMesinComponent {
   }
 
   public add(ev: any): void {
-    this.dialogVisible = false;
+    this.clearTextBox();
+    this.dialogAddVisible = true;
     this.state = 'add';
+  }
+
+  public clearTextBox(): void {
+    this.namaMsn = '';
+    this.tipeDoc = '';
+    this.noDoc = '';
+    this.tanggal = '';
+    this.from = '';
+    this.amount = '';
   }
 
   public onOverlayClick(): void {
     this.dialogVisible = false;
   }
 
+  public onOverlayAddClick(): void {
+    this.dialogAddVisible = false;
+  }
+
   public addToGrid(ev: any): void {
+    this.items = [
+      ...this.items,
+      {
+        indexNum: this.items.length + 1,
+        namaMsn: this.namaMsn,
+        tipeDoc: this.tipeDoc,
+        noDoc: this.noDoc,
+        tanggal: this.tanggal,
+        from: this.from,
+        amount: this.amount,
+      },
+    ];
+
+    // clear form values
+    this.clearTextBox();
+
     this.state = 'idle';
-    this.dialogVisible = false;
+    this.dialogAddVisible = false;
   }
 }
