94cf86e9657bd5d9b4b88d94b2606bff0e3a84ae
Ricky <ricky_riq@yahoo.com>
2022-08-18 16:44:17 +0700

    .

--- src/main/webapp/app/entities/collateral-appraisal/foto/collateral-appraisal-process.component.ts
+++ src/main/webapp/app/entities/collateral-appraisal/foto/collateral-appraisal-process.component.ts
@@ -1,83 +1,86 @@
-/* import { Component, ViewChild } from '@angular/core';
-import { ItemModel, OpenCloseMenuEventArgs, DropDownButtonComponent } from '@syncfusion/ej2-angular-splitbuttons';
-
-@Component({
-  selector: 'jhi-collateral-appraisal-process',
-  templateUrl: './collateral-appraisal-process.component.html',
-  styleUrls: ['./collateral-appraisal-process.css'],
-})
-export class CollateralAppraisalProcessComponent {
-  public BlodType: string[] = ['Objek Jaminan', '.........'];
-  // commented when open -- Start
-  @ViewChild('dropdownbutton')
-  public dropdownbutton: DropDownButtonComponent;
-  public data: ItemModel[] = [
-    {
-      text: 'Rincian',
-    },
-    {
-      text: 'Hapus',
-    },
-  ];
-
-  public onOpen(args: OpenCloseMenuEventArgs) {
-    args.element.parentElement.style.top =
-      this.dropdownbutton.element.getBoundingClientRect().top - args.element.parentElement.offsetHeight + 'px';
-  }
-  // commented when open -- End
-}*/
-
-import { HttpResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { ItemModel, MenuEventArgs } from '@syncfusion/ej2-angular-splitbuttons';
+import { Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import { ItemModel } from '@syncfusion/ej2-angular-splitbuttons';
 import { StorageService } from 'app/entities/storage/storage.service';
 import { formatBytes } from 'app/shared/helper/utils';
 import moment from 'moment';
+import lodash from 'lodash';
 
 @Component({
   selector: 'jhi-collateral-appraisal-process',
   templateUrl: './collateral-appraisal-process.component.html',
   styleUrls: ['./collateral-appraisal-process.scss'],
 })
-export class CollateralAppraisalProcessComponent implements OnInit {
-  categoryFilter: any;
+export class CollateralAppraisalProcessComponent implements OnChanges {
+  @ViewChild('uploader')
+  public uploader: ElementRef;
+
+  @Input()
+  public appraisalId: number;
+
+  public categoryFilter: string;
   isLoading = false; // Flag variable
   file: File = null; // Variable to store file
   sizeFile: string;
   storageBucket: any;
-  bucket = 'hana';
+  bucket: string;
   bucketIndex = 0;
   isDropup = true;
+  public items: ItemModel[] = [{ text: 'Cut' }, { text: 'Copy' }, { text: 'Paste' }];
+  public files: Object[] = [];
 
-  url = 'http://45.32.114.128:8190/storage/';
-  constructor(private storageService: StorageService) {}
-
-  ngOnInit(): void {
-    this.getBucket();
+  constructor(private storageService: StorageService) {
+    this.categoryFilter = '';
   }
 
-  public items: ItemModel[] = [
-    {
-      text: 'Cut',
-    },
-    {
-      text: 'Copy',
-    },
-    {
-      text: 'Paste',
-    },
-  ];
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['appraisalId']) {
+      this.getBucketName().then(val => {
+        this.getFilesByKey(`/appraisals/jaminan/${this.appraisalId}`);
+      });
+    }
+  }
 
-  public getBucket(): void {
-    this.storageService.getObjects(this.bucket, { key: 'collateral' }).subscribe((res: HttpResponse<any>) => {
-      const responseBody = res?.body;
-      if (responseBody.length > 0) {
-        this.storageBucket = responseBody;
-        this.bucketIndex = responseBody.length;
-        console.log(this.bucketIndex);
-      } else {
-        console.log('tidak ada');
-      }
+  public filterFilesByCategory(): void {
+    this.setViewAllFiles(this.files);
+    if (this.categoryFilter !== '') {
+      const selectedCategory: string = this.categoryFilter;
+      this.files.forEach(function (e) {
+        const cat: string = e['tags']['category'];
+        if (cat !== selectedCategory) {
+          e['display'] = false;
+        }
+      });
+    }
+  }
+
+  public deleteFile(key: string): void {
+    this.storageService.delete(this.bucket, key).subscribe(res => {
+      this.getFilesByKey(`/appraisals/jaminan/${this.appraisalId}`);
+    });
+  }
+
+  private setViewAllFiles(data: Object[]): void {
+    if (data.length > 0) {
+      data.forEach(function (e) {
+        e['display'] = true;
+      });
+    }
+  }
+
+  public getFilesByKey(_key: string): void {
+    const obj: Object = { key: _key };
+    this.storageService.getObjects(this.bucket, obj).subscribe(res => {
+      this.files = res.body;
+      this.setViewAllFiles(this.files);
+    });
+  }
+
+  public getBucketName(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.storageService.getBucketName().subscribe(res => {
+        this.bucket = res.body['bucket'];
+        resolve();
+      });
     });
   }
 
@@ -89,7 +92,8 @@ export class CollateralAppraisalProcessComponent implements OnInit {
     this.sizeFile = formatBytes(this.file.size);
 
     const metaData = {
-      objectName: `collateral/${this.bucketIndex + 1}/collateral_${this.bucketIndex + 1}_${currentDate}.${this.file.name.split('.')[1]}`,
+      objectName: `appraisals/jaminan/${this.appraisalId}/${currentDate}.${this.file.name.split('.')[1]}`,
+      category: this.categoryFilter,
     };
     const formData = new FormData();
     formData.append('file', this.file);
@@ -97,8 +101,8 @@ export class CollateralAppraisalProcessComponent implements OnInit {
     this.storageService.uploadMeta(this.bucket, formData, metaData).subscribe(res => {
       this.isLoading = false;
       this.file = null;
-      this.getBucket();
-      console.log(res);
+      this.uploader.nativeElement.value = '';
+      this.getFilesByKey(`/appraisals/jaminan/${this.appraisalId}`);
     });
   }
 }
