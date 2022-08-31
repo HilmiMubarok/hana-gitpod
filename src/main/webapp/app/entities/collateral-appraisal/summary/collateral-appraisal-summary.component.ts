6a2e29e8ac82ed7b739207fb69e1ca6855b9e8f3
ismoyo23 <ismoyorplb@gmail.com>
2022-08-31 17:10:04 +0700

    summary complates

--- src/main/webapp/app/entities/collateral-appraisal/summary/collateral-appraisal-summary.component.ts
+++ src/main/webapp/app/entities/collateral-appraisal/summary/collateral-appraisal-summary.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { ToolbarModule } from '@syncfusion/ej2-angular-navigations';
+import { ICreditProposal } from 'app/entities/credit-proposal/credit-proposal.model';
 
 @Component({
   selector: 'jhi-collateral-appraisal-summary',
@@ -7,6 +8,31 @@ import { ToolbarModule } from '@syncfusion/ej2-angular-navigations';
   styleUrls: ['./collateral-appraisal-summary.css'],
 })
 export class CollateralAppraisalSummaryComponent {
+  private _item: ICreditProposal;
+
+  @Output()
+  public criteriaEvent = new EventEmitter<any[]>();
+
+  @Input('item')
+  get item() {
+    return this._item;
+  }
+
+  set item(item: any) {
+    this._item = item;
+  }
+
+  ngOnInit() {
+    this.item['attributes'] = {
+      ...this.item['attributes'],
+      summary: {
+        keterangan: this.item['attributes'].summary === undefined ? '' : JSON.parse(this.item['attributes'].summary).keterangan,
+        marketbility: this.item['attributes'].summary === undefined ? '' : JSON.parse(this.item['attributes'].summary).marketbility,
+        returnNotes: this.item['attributes'].summary === undefined ? '' : JSON.parse(this.item['attributes'].summary).returnNotes,
+      },
+    };
+  }
+
   public tools: ToolbarModule = {
     items: [
       'FontName',
