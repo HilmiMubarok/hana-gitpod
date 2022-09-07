d854fa67dda1f654222a5b5b6ad00780f4ec3da9
andi lauw <andi.safariansyah7@gmail.com>
2022-09-08 00:35:48 +0700

    Request merge & Tiketing

--- src/main/webapp/app/entities/credit-proposal/credit-proposal-risk-acceptance-criteria-component.ts
+++ src/main/webapp/app/entities/credit-proposal/credit-proposal-risk-acceptance-criteria-component.ts
@@ -1,52 +1,234 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { DialogComponent, DialogUtility } from '@syncfusion/ej2-angular-popups';
 import { AbstractEntityEj2GridComponent } from 'app/shared/base/abstract-entity-ej2-grid.component';
-import { ICreditProposal } from './credit-proposal.model';
+import { CreditProposal, ICreditProposal } from './credit-proposal.model';
+
+// ini import interface position
+import { IPosition } from '../position/position.model';
+import { PositionService } from '../position/position.service';
+import { DropDownListComponent } from '@syncfusion/ej2-angular-dropdowns';
+import { CreditProposalService } from './credit-proposal.service';
+import { AbstractEntityComponent } from 'app/shared/base/abstract-entity.component';
+import { AlertService } from 'app/core/util/alert.service';
+import { PartyCif } from '../party-cif/party-cif.model';
+import { ApplicationService } from '../application/application.service';
+// import { ConfirmationService, MessageService } from 'primeng/api';
+// import { TranslateService } from '@ngx-translate/core';
+import { EventManager } from '@angular/platform-browser';
+import { AccountService } from 'app/core/auth/account.service';
+import { BaseDataUtils } from 'app/shared/base/base-data-utils.service';
+import { ParseLinks } from 'app/core/util/parse-links.service';
+import { constant } from 'lodash';
+import { GridComponent } from '@syncfusion/ej2-angular-grids';
 
 @Component({
   selector: 'jhi-credit-proposal-risk-acceptance-criteria',
   templateUrl: './credit-proposal-risk-acceptance-criteria-component.html',
-  styleUrls: ['./css/credit-proposal-basic-information.css'],
+  styleUrls: ['./credit-proposal-risk-acceptance-criteria.css'],
 })
-export class CreditProposalRiskAcceptanceCriteriaComponent implements OnInit {
+export class CreditProposalRiskAcceptanceCriteriaComponent extends AbstractEntityEj2GridComponent<ICreditProposal> implements OnInit {
+  dataAttr: Object[];
+  dataSave: any[];
+  constructor(
+    protected creditProposalService: CreditProposalService,
+    protected positionService: PositionService // protected parseLinks: ParseLinks, // protected accoutService: AccountService, // protected activateRoute: ActivatedRoute, // protected dataUtils: BaseDataUtils, // protected router: Router, // protected eventManager: EventManager, // protected messageService: MessageService, // protected confirmationService: ConfirmationService
+  ) {
+    super(
+      creditProposalService
+
+      // parseLinks,
+      // accoutService,
+      // activateRoute,
+      // dataUtils,
+      // router,
+      // eventManager,
+      // messageService,
+      // confirmationService
+    );
+  }
+  private _creditProposal: ICreditProposal;
+  get creditProposal() {
+    return this._creditProposal;
+  }
+  public grid: GridComponent;
   public data: Object[];
+  public dataAttrPass = [
+    {
+      No: 1,
+      Parameter: 'Debitur merupakah individu (Perorangan) , warga negara indonesia dan berdomisili indonesia',
+      value: '',
+    },
+    {
+      No: 2,
+      Parameter: 'Age for individual debtors: Min. 24 years at the time of proposing loan, Max. 65 years at loan maturity date',
+      value: '',
+    },
+    {
+      No: 3,
+      Parameter: 'Business location ≤ 30 KM from Hana Bank branch booking unit',
+      value: '',
+    },
+    {
+      No: 4,
+      Parameter: 'Is the debtor industry included in the watch list industry?',
+      value: '',
+    },
+    {
+      No: 5,
+      Parameter: 'Not included in the National Black List (DHN) of Bank Indonesia',
+      value: '',
+    },
+    {
+      No: 6,
+      Parameter: 'The purpose of applying for credit is not for buying land',
+      value: '',
+    },
+    {
+      No: 7,
+      Parameter: 'Not a Political Exposed Person (PEP) -> includes spouse, BOD & BOC debtors',
+      value: '',
+    },
+  ];
+  public creditProposaldata: ICreditProposal = new CreditProposal();
+
+  public value: string;
+  // public Yes: string;
+  // public selectedRadioButtonValue: string[] = [];
+  // public CollateralStatus: string[] = [
+  //   'Vacant',
+  //   'Occupied by debtor / debtors family',
+  //   'Leased to third party (with lease period ≤ 2 years)',
+  //   'Leased to third party (with lease period > 2 years)',
+  // ];
+
+  // public CollateralInsurance: string[] = ['Increasing', 'Stable (Changes ±10%)', 'Decreasing'];
+
+  // public CollateralBasedOnLv: string[] = [
+  //   'Covered by Partner Insurance Company',
+  //   'Covered by Non-partner Insurance Company',
+  //   'Not covered with insurance',
+  // ];
+
+  // public save(): void {
+  //   this._item.attributes = {
+  //     GeneralRiskAcceptanceCriteria: JSON.stringify({ dataAttr }),
+  //     aas: []
+  //   };
+
+  //   console.log('andi', this.creditProposaldata);
+  //   console.log('oke', this.dataAttrPass);
+  // }
+
+  public onSelect(value: string, data: any): void {
+    this.dataAttrPass[data.No - 1].value = value;
+  }
 
   public dialogVisible: boolean;
   public width?: string;
   public height?: string;
   public animationSettings?: Object;
   public closeOnEscape?: boolean;
-  // public showCloseIcon?: boolean;
   Dialog: any;
-
-  constructor() {
-    this.width = '70%';
-    this.height = '90%';
-    this.dialogVisible = false;
-    this.animationSettings = { effect: 'Zoom', duration: 400, delay: 0 };
-    this.closeOnEscape = false;
-    // this.showCloseIcon = true;
-  }
 
   public onOverlayClick(): void {
     this.dialogVisible = false;
   }
 
-  // public btnClose(): void {
-  //   this.showCloseIcon = false;
-  // }
-
   public btnAdd(): void {
     this.dialogVisible = true;
   }
 
+  // public Value: string;
+
+  // public OnSelect(value: string, data: any): void {
+  //   this.dataAttrPass[data].value = value;
+  //   console.log('Ini Value', this.dataAttrPass);
+  // }
+
+  // public dataValue = [
+  //   {
+  //     value: '',
+  //   },
+  // ];
+
+  public dataGrid?: any = [];
+
+  public subParameter: string;
+  public parameter: string;
+  public remarks: string;
+  public CollateralStatus: string;
+  public CollateralInsurance: string;
+  public CollateralBasedOnLv: string;
+
+  @ViewChild('ddposition')
+  public dropDownListObject: DropDownListComponent;
+  public dropdownSub: string[] = [];
+
+  attributes: any;
+  public _item: ICreditProposal = new CreditProposal();
+
+  @Input()
+  get item() {
+    return this._item;
+  }
+
+  set item(item: any) {
+    this._item = item;
+  }
+
+  public btnSave($event: any): void {
+    this.dataAttrPass = [...this.dataAttrPass];
+    this.dataGrid = [
+      ...this.dataGrid,
+
+      {
+        subParameter: this.subParameter,
+        parameter: this.parameter,
+        remarks: this.remarks,
+
+        CollateralStatus: this.CollateralStatus,
+        CollateralInsurance: this.CollateralInsurance,
+        CollateralBasedOnLv: this.CollateralBasedOnLv,
+      },
+    ];
+
+    this._item.attributes = {
+      // GeneralRiskAcceptanceCriteria: JSON.stringify({ dataAttr }),
+      GeneralRiskAcceptanceCriteria: JSON.stringify(this.dataAttrPass),
+      RiskAcceptanceCriteria: JSON.stringify(this.dataGrid),
+    };
+    alert('succes add data');
+    console.log('buat Grid', this.dataGrid);
+    console.log('buata attr', dataAttr);
+    console.log('buat attr pas', this.dataAttrPass);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // this.dataAttrPass = JSON.parse(changes._item.currentValue.attributes.acceptance);
+    this.dataGrid = JSON.parse(changes._item.currentValue.attributes.acceptance);
+  }
+
+  // Coba Dummy Data Delete
+  public deleteData(): void {
+    const newData = this.dataGrid.splice(1, this.dataGrid.length);
+    this.dataGrid = newData;
+    alert(`succes Delete data`);
+    console.log('succes');
+  }
+
   ngOnInit(): void {
-    this.data = data;
+    this.data = dataAttr;
+
+    this.dataGrid;
+    // = this.dataValue
+    this.width = '50%';
+    this.height = '80%';
   }
 }
-export const data: Object[] = [
+
+export const dataAttr: Object[] = [
   {
     No: 1,
     Parameter: 'Debitur merupakah individu (Perorangan) , warga negara indonesia dan berdomisili indonesia',
@@ -89,45 +271,4 @@ export const data: Object[] = [
     Verified: !7,
     value: 'PEP',
   },
-  // {
-  //   No: 8,
-  //   Parameter: 'Berlokasi dekat dengan Tempat Pembuangan Sampah Akhir (TPA) dengan jarak \u{2264} 1 km.',
-  //   Verified: !8,
-  // },
-  // {
-  //   No: 9,
-  //   Parameter: 'Diginakan dan atau diperuntukan (zoning) sebagai sawah/ladang/pertanian/rawa-rawa.',
-  //   Verified: !9,
-  // },
-  // {
-  //   No: 10,
-  //   Parameter: 'Jaminan merupakan kawasan cagar budaya.',
-  //   Verified: !10,
-  // },
-  // {
-  //   No: 11,
-  //   Parameter: 'SHM atau HGB atau SHMSRS di atas Hak Pengelolaan.',
-  //   Verified: !11,
-  // },
-  // {
-  //   No: 12,
-  //   Parameter:
-  //     'Sebagian area tanahnya digunakan untuk mendirikan Base Transceiver Station atau BTS (tidak termasuk BTS yang didirikan diatas bangunan).',
-  //   Verified: !12,
-  // },
-  // {
-  //   No: 13,
-  //   Parameter: 'Rumah sarang burung.',
-  //   Verified: !13,
-  // },
-  // {
-  //   No: 14,
-  //   Parameter: 'HGB atau MoU di atas Hak Milik orang lain (Perumnas).',
-  //   Verified: !14,
-  // },
-  // {
-  //   No: 15,
-  //   Parameter: 'Terletak di pinggir laut (bukan pantai) atau rel kereta api.',
-  //   Verified: !15,
-  // },
 ];
