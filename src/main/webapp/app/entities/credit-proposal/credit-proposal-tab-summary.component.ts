1237cf24fc1ed403e1f1a2d7f7577ed71302cfb1
styvenz <styvenzenwijaya@yahoo.com>
2022-09-09 00:55:50 +0700

    Fix summary : remove multiline

--- src/main/webapp/app/entities/credit-proposal/credit-proposal-tab-summary.component.ts
+++ src/main/webapp/app/entities/credit-proposal/credit-proposal-tab-summary.component.ts
@@ -1,18 +1,5 @@
 import { Component, OnInit, ViewChild, Input, SimpleChanges, OnChanges } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AccountService } from 'app/core/auth/account.service';
-import { ITEMS_PER_PAGE } from 'app/config/pagination.constants';
 import { CreditProposal, ICreditProposal } from './credit-proposal.model';
-import { CreditProposalService } from './credit-proposal.service';
-import { ConfirmationService, MessageService } from 'primeng/api';
-import { AbstractEntityEj2GridComponent } from 'app/shared/base/abstract-entity-ej2-grid.component';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { BaseDataUtils } from 'app/shared/base/base-data-utils.service';
-import { ParseLinks } from 'app/core/util/parse-links.service';
-import { AlertService } from 'app/core/util/alert.service';
-import { EventManager } from 'app/core/util/event-manager.service';
-import { AnimationSettingsModel, DialogComponent } from '@syncfusion/ej2-angular-popups';
-import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'jhi-credit-proposal-tab-summary',
@@ -23,13 +10,8 @@ export class CreditProposalTabSummaryComponent {
   public state: string;
   public dialogVisible: false;
   public data: object[];
-  public FileTemplate: string;
 
   public _item?: ICreditProposal = new CreditProposal();
-  public strength?: string;
-  public opportunities?: string;
-  public weaknesses?: string;
-  public threats?: string;
 
   @Input('item')
   get item() {
@@ -40,36 +22,7 @@ export class CreditProposalTabSummaryComponent {
     this._item = item;
   }
 
-  public tools: object = {
-    items: [
-      'FontName',
-      'FontSize',
-      'Bold',
-      'Italic',
-      'Underline',
-      'StrikeThrough',
-      'FontColor',
-      'BackgroundColor',
-      'OrderedList',
-      'UnorderedList',
-      'Indent',
-      'Outdent',
-      'SuperScript',
-      'SubScript',
-      'Alignments',
-      'CreateLink',
-    ]
-  };
-
   attributes: any;
-
-  constructor(protected creditProposalService: CreditProposalService) {}
-
-  save(): void {
-    this.creditProposalService.create(this.item).subscribe(res => {
-      console.log('cek', res);
-    });
-  }
 
   public generate(data: any): void {
     this.state = 'idle';
