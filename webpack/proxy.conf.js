a06649163ba239d564707bfff0c07c9e99c3be63
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2022-09-12 19:30:00 +0700

    .

---
+++ webpack/proxy.conf.js
@@ -0,0 +1,29 @@
+function setupProxy({ tls }) {
+  const conf = [
+    {
+      context: [
+        '/api',
+        '/services',
+        '/management',
+        '/v3/api-docs',
+        '/h2-console',
+        '/oauth2',
+        '/login',
+        '/auth',
+        '/health',
+        '/storage',
+        '/httpbin',
+        '/strapi',
+      ],
+      target: `http${tls ? 's' : ''}://localhost:8190`,
+      secure: false,
+      changeOrigin: tls,
+      headers: {
+        'X-Tenant': '10000',
+      },
+    },
+  ];
+  return conf;
+}
+
+module.exports = setupProxy;
