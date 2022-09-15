27f2dd11a3b7a457b3c8c97e7762a419df161ad0
anjarfals07 <anjarfals07@gmail.com>
2022-09-15 15:49:46 +0700

    add repayment capability layout + function

---
+++ src/main/webapp/app/entities/credit-proposal/repayment-capability/credit-proposal-repayment-capability.component.ts
@@ -0,0 +1,69 @@
+import { Component, Inject, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ICreditProposal } from '../credit-proposal.model';
+import lodash from 'lodash';
+import { BankAccountAnalyst, IBankAccountAnalyst } from '../bank-account-analyst/bank-account-analyst.model';
+import { TABLE } from '@syncfusion/ej2-angular-richtexteditor';
+import { IRepaymentCapability, IRepaymentCapabilityDetail, RepaymentCapability } from './repayment-capability.constant';
+
+@Component({
+  selector: 'jhi-credit-proposal-repayment-capability',
+  templateUrl: './credit-proposal-repayment-capability.component.html',
+  styleUrls: ['./repayment-capability.scss'],
+})
+export class CreditProposalRepaymentCapabilityComponent implements OnChanges {
+  private _creditProposal: ICreditProposal;
+
+  @Input()
+  get creditProposal() {
+    return this._creditProposal;
+  }
+  set creditProposal(data: ICreditProposal) {
+    this._creditProposal = data;
+  }
+
+  public creditMutation = 0;
+  public creditMutationAv = 0;
+  public debitMutationAv = 0;
+  public totalAv = 0;
+  public totalIncome = 0;
+  public existingFs = 0;
+  public idAnalis = '';
+  public repayment: IRepaymentCapabilityDetail;
+
+  fungsiCreditMutation() {
+    for (let i = 0; i < this._creditProposal.attributes['bankAnalyst'][i]['detail'].length; i++) {
+      // console.log('Tester', this._creditProposal.attributes['bankAnalyst'][i]['detail'][i]['credit']);
+      if (
+        this._creditProposal.attributes['bankAnalyst'][i]['detail'][i]['credit'] === undefined ||
+        this._creditProposal.attributes['bankAnalyst'][i]['detail'][i]['debit'] === undefined
+      ) {
+        console.log('masuk');
+      } else {
+        this.creditMutation =
+          this.creditMutation +
+          Number(
+            (this._creditProposal.attributes['bankAnalyst'][i]['detail'][i]['credit'] / 1000000) *
+              (this.creditProposal.attributes['proformaLaporanKeuangan'][0]['detail']['totalSales'] -
+                this.creditProposal.attributes['proformaLaporanKeuangan'][0]['detail']['cogs'] -
+                this.creditProposal.attributes['proformaLaporanKeuangan'][0]['detail']['sga'] /
+                  this.creditProposal.attributes['proformaLaporanKeuangan'][0]['detail']['totalSales'])
+          );
+
+        this.totalAv =
+          this.totalAv +
+          Number(
+            this._creditProposal.attributes['bankAnalyst'][i]['detail'][i]['balance'] +
+              this._creditProposal.attributes['bankAnalyst'][i]['detail'][i]['balance']
+          );
+        console.log('Credit', this._creditProposal.attributes['bankAnalyst'][i]['detail'][i]['credit']);
+
+        console.log('Tester', this._creditProposal.attributes['bankAnalyst'][i]['detail'][i]['credit']);
+      }
+    }
+  }
+
+  constructor() {}
+  ngOnChanges(changes: SimpleChanges): void {
+    this.fungsiCreditMutation();
+  }
+}
