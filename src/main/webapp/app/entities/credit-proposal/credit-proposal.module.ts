afcc8c7c5d30bde0861c421a5e8007788f9bd577
imalzy <imalzaya19@gmail.com>
2022-09-15 21:53:46 +0700

    Repayment spreadsheet

--- src/main/webapp/app/entities/credit-proposal/credit-proposal.module.ts
+++ src/main/webapp/app/entities/credit-proposal/credit-proposal.module.ts
@@ -27,6 +27,8 @@ import { SlikSummaryBusinessGroupDialogComponent } from './slik-summary/business
 import { SlikSummaryBusinessGroupComponent } from './slik-summary/business-group/slik-summary-business-group.component';
 import { CreditProposalCollateralInfoComponent } from './collateral-info/credit-proposal-collateral-info.component';
 import { CreditProposalCollateralInfoDialogComponent } from './collateral-info/dialog/credit-proposal-collateral-info-dialog.component';
+import { RepaymentSpreadsheetComponent } from './repayment-spreadsheet/repayment-spreadsheet.component';
+
 @NgModule({
   imports: [SharedModule, SharedLibsModule, SharedEntityModule, RouterModule.forChild(creditProposalRoute)],
   declarations: [
@@ -50,6 +52,7 @@ import { CreditProposalCollateralInfoDialogComponent } from './collateral-info/d
     SlikSummaryBusinessGroupDialogComponent,
     CreditProposalCollateralInfoComponent,
     CreditProposalCollateralInfoDialogComponent,
+    RepaymentSpreadsheetComponent,
   ],
   entryComponents: [
     CreditProposalBankAccountAnalystDialogComponent,
--- src/main/webapp/app/entities/credit-proposal/repayment-spreadsheet/repayment-spreadsheet.component.ts
+++ src/main/webapp/app/entities/credit-proposal/repayment-spreadsheet/repayment-spreadsheet.component.ts
@@ -1,15 +1,87 @@
-import { Component, OnInit } from '@angular/core';
-
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { BeforeOpenEventArgs, BeforeSaveEventArgs, SpreadsheetComponent } from '@syncfusion/ej2-angular-spreadsheet';
+import { StorageService } from 'app/entities/storage/storage.service';
+import { EMPTY, Subject, switchMap } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'jhi-repayment-spreadsheet',
   templateUrl: './repayment-spreadsheet.component.html',
 })
-export class RepaymentSpreadsheetComponent implements OnInit {
+export class RepaymentSpreadsheetComponent implements OnInit, OnDestroy {
+  private ngUnsubscribe = new Subject();
+  @ViewChild('spreadsheet') public spreadsheetObj: SpreadsheetComponent;
+  public data: object[];
 
-  constructor() { }
+  private bucket = 'hana';
+  private key: string = 'credit_proposal/repayment_capability';
+  private paramsId: string;
+  private isIdHasData: boolean = true;
+
+  constructor(private storageService: StorageService, private actRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    console.log('repayment-spreadsheet');
+    this.actRoute.params.pipe(takeUntil(this.ngUnsubscribe)).subscribe(params => {
+      this.paramsId = params['id'];
+    });
+    this.created();
   }
 
+  beforeOpen(args: BeforeOpenEventArgs): void {
+    if (args && args.file) {
+      const documentRepayment: File = args.file as File;
+      const metaData = {
+        objectName: `credit_proposal/repayment_capability/${this.paramsId}/template_repayment_capability`,
+      };
+      // console.log(documentRepayment);
+      // console.log(metaData);
+
+      const formData = new FormData();
+      formData.append('file', documentRepayment);
+
+      this.storageService.uploadMeta('hana', formData, metaData).subscribe(res => {
+        console.log(res);
+      });
+    }
+  }
+
+  beforeSave(args: BeforeSaveEventArgs): void {
+    args.fileName = 'template_repayment_capability';
+    args.saveType = 'Xlsx';
+  }
+
+  created(): void {
+    if (this.paramsId) {
+      this.storageService
+        .getObjects(this.bucket, {
+          key: this.isIdHasData ? `${this.key}/${this.paramsId}` : `${this.key}`,
+        })
+        .pipe(takeUntil(this.ngUnsubscribe))
+        .subscribe((res: any) => {
+          if (res.body.length > 0) {
+            this.getFile(res.body[0]?.url);
+            this.isIdHasData = true;
+          } else {
+            this.isIdHasData = false;
+            this.created();
+          }
+        });
+    }
+  }
+
+  getFile(urlFile: string): void {
+    this.storageService
+      .fileBlob(urlFile)
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(res => {
+        const file = new File([res.body], 'template_repayment_capability.xlsx');
+        this.spreadsheetObj.open({ file });
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next(true);
+    this.ngUnsubscribe.complete();
+  }
 }
