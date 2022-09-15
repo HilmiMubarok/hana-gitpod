96bb1599d8883f00fa67d040302fea2076e2298e
imalzy <imalzaya19@gmail.com>
2022-09-15 16:44:54 +0700

    add componet repayment-spreadsheet

---
+++ src/main/webapp/app/entities/credit-proposal/repayment-spreadsheet/repayment-spreadsheet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-repayment-spreadsheet',
+  templateUrl: './repayment-spreadsheet.component.html',
+})
+export class RepaymentSpreadsheetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('repayment-spreadsheet');
+  }
+
+}
