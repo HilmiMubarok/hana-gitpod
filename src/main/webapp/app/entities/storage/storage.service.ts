3ee89e4bacbd8d5eb54a8d42434a7634f9a3ab6a
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-09-16 15:34:11 +0700

    fix storage service

--- src/main/webapp/app/entities/storage/storage.service.ts
+++ src/main/webapp/app/entities/storage/storage.service.ts
@@ -44,6 +44,10 @@ export class StorageService {
     return this.http.delete<any>(this.resourceUrl + '/' + bucket + '/files/' + key, { observe: 'response' });
   }
 
+   public fileBlob(url):Observable<HttpResponse<Blob>>{
+    return this.http.get(url, {observe: 'response', responseType: 'blob'});
+  }
+
   private params(parameters: IObject): HttpParams {
     return new HttpParams({ fromObject: parameters });
   }
