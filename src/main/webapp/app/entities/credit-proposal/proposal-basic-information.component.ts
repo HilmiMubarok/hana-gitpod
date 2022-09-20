043736a53bd4e1cb0e48b650446fe753bacddbdc
imalzy <imalzaya19@gmail.com>
2022-09-20 10:25:27 +0700

    add above or below

--- src/main/webapp/app/entities/credit-proposal/proposal-basic-information.component.ts
+++ src/main/webapp/app/entities/credit-proposal/proposal-basic-information.component.ts
@@ -64,7 +64,7 @@ export class ProposalBasicInformationComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.selectedMenu = 'BASIC INFORMATION';
+    this.selectedMenu = 'FINANCIAL STATEMENT';
     const passSummary = {
       strength: '',
       opportunities: '',
