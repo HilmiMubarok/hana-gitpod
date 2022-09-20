5c97a52cfd580afff97dc3a8b4345d937568e8ba
styvenz <styvenzenwijaya@yahoo.com>
2022-09-20 07:48:27 +0700

    1. Add skeleton loan-analys 2. Fix prefix breadcrumb collateral-appraisal new appraise 3. Clean up import & variabel credit-proposal-index & credit-proposal-main

---
+++ src/main/webapp/app/entities/loan-analys/dialogs/loan-analys-dialog-single-assign.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-loan-analys-dialog-single-assign',
+  templateUrl: './loan-analys-dialog-single-assign.component.html',
+  styleUrls: ['./loan-analys-dialog-single-assign.css']
+})
+export class LoanAnalysDialogSingleAssignComponent {
+  
+}
---
+++ src/main/webapp/app/entities/loan-analys/opinion/loan-analys-opinion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-loan-analys-opinion',
+  templateUrl: './loan-analys-opinion.component.html',
+  styleUrls: ['./loan-analys-opinion.css']
+})
+export class LoanAnalysOpinionComponent {
+  
+}
