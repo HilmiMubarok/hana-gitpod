007091d17c1df6a92c4ea5849b8216471afdeb53
styvenz <styvenzenwijaya@yahoo.com>
2022-09-22 12:17:15 +0700

    1. Add matrix input(s) @collateral-appraisal info 2. Fix save collateral-appraisal 3. Fix add correspondence

--- src/main/webapp/app/entities/shared-entity.module.ts
+++ src/main/webapp/app/entities/shared-entity.module.ts
@@ -278,8 +278,8 @@ import { CorrespondenceDialogComponent } from './credit-proposal/correspondence/
     DocumentComponent,
     DocumentUploadDialogComponent,
     DocumentDetailDialogComponent,
-	  CreditProposalCorrespondenceComponent,
-	  CorrespondenceDialogComponent
+	CreditProposalCorrespondenceComponent,
+	CorrespondenceDialogComponent
     /* jhipster-needle-declaration-entity-as-list */
   ],
   entryComponents: [],
