44113c1042cf1165b740fc8a3a0e696f0d9b362d
andi lauw <andi.safariansyah7@gmail.com>
2022-09-28 15:15:32 +0700

    dialog && collateral-info.component.html collateral-info.component.ts

--- src/main/webapp/app/entities/credit-proposal/collateral-info/credit-proposal-collateral-info.component.ts
+++ src/main/webapp/app/entities/credit-proposal/collateral-info/credit-proposal-collateral-info.component.ts
@@ -15,6 +15,7 @@ import {
   ICreditProposalCollateralBinding,
   ICreditProposalCollateralInsurance,
 } from './credit-proposal-collateral-info.model';
+import { MenuEventArgs, MenuItemModel } from '@syncfusion/ej2-angular-navigations';
 
 @Component({
   selector: 'jhi-credit-proposal-collateral-info',
@@ -45,6 +46,12 @@ export class CreditProposalCollateralInfoComponent implements OnChanges {
   public totalLVInt: number;
   private _creditProposal: ICreditProposal;
 
+  public selectedMenu: string;
+  public menuItems: MenuItemModel[] = [{ text: 'INFORMATION' }, { text: 'CHECKLIST' }];
+  public selectMenuItem(args: MenuEventArgs): void {
+    this.selectedMenu = args.item.text;
+  }
+
   @Input()
   get creditProposal() {
     return this._creditProposal;
@@ -64,6 +71,8 @@ export class CreditProposalCollateralInfoComponent implements OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
+    this.selectedMenu = 'INFORMATION';
+    console.log('TESSSS', this.creditProposal.attributes['proposalType']);
     if (changes['creditProposal']) {
       if (this.creditProposal.collaterals.length > 0) {
         for (let i = 0; i < this.creditProposal.collaterals.length; i++) {
