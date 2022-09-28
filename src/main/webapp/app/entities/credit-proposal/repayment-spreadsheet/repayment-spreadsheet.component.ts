cd4e08cb37f8b2368a52da5385e4e657f52dcbde
imalzy <imalzaya19@gmail.com>
2022-09-28 17:28:42 +0700

    remove comment line

--- src/main/webapp/app/entities/credit-proposal/repayment-spreadsheet/repayment-spreadsheet.component.ts
+++ src/main/webapp/app/entities/credit-proposal/repayment-spreadsheet/repayment-spreadsheet.component.ts
@@ -345,65 +345,8 @@ export class RepaymentSpreadsheetComponent implements OnInit, OnDestroy, OnChang
       this.spreadsheetObj.updateCell({ formula: `=INDEX(ftp!$G$3:$G$8,MATCH(K${startCell+i},ftp!$A$3:$A$8,0))` }, `calculator2!L${startCell + i}`);
     }
     this.spreadsheetObj?.numberFormat('0.00%', '=L5:L10');
-
-    // for (let baris = 0; baris < 6; baris++) {
-    //   for(let kol = 0; kol < baris; kol++){
-    //     document.write(str);
-    //   }
-    //   document.write("<br/>")
-    // }
-
-    //   const temp = new Spreadsheet({
-    //     sheets: [
-    //       {
-    //         name: 'Monthly Budget',
-    //         selectedRange: 'D13',
-    //         rows: [
-    //           {
-    //             cells: [
-    //               { value: 'Category', style: { fontWeight: 'bold', textAlign: 'center' } },
-    //               { value: 'Planned cost', style: { fontWeight: 'bold', textAlign: 'center' } },
-    //               { value: 'Actual cost', style: { fontWeight: 'bold', textAlign: 'center' } },
-    //               { value: 'Difference', style: { fontWeight: 'bold', textAlign: 'center' } },
-    //             ],
-    //           },
-    //           {
-    //             cells: [{ value: 'Food' }, { value: '$7000' }, { value: '$8120' }, { formula: '=B2-C2', format: '$#,##0.00' }],
-    //           },
-    //           {
-    //             cells: [{ value: 'Loan' }, { value: '$1500' }, { value: '$1500' }, { formula: '=B3-C3', format: '$#,##0.00' }],
-    //           },
-    //           {
-    //             cells: [{ value: 'Medical' }, { value: '$300' }, { value: '$0' }, { formula: '=B4-C4', format: '$#,##0.00' }],
-    //           },
-    //           {
-    //             index: 5,
-    //             cells: [
-    //               { index: 2, value: 'Total Difference:', style: { fontWeight: 'bold', textAlign: 'right' } },
-    //               { formula: '=D2+D4', format: '$#,##0.00', style: { fontWeight: 'bold' } },
-    //             ],
-    //           },
-    //         ],
-    //         columns: [{ width: 110 }, { width: 115 }, { width: 110 }, { width: 100 }],
-    //       },
-    //     ],
-    //   });
-
-    //  console.log(temp)
-
-    // this.spreadsheetObj.updateCell(
-    //   {
-    //     value: 'Imal Zaya harahap',
-    //   },
-    //   'C12'
-    // );
-
     // this.spreadsheetObj?.setRowHeight(30, 1);
   }
-
-  // created() {
-  //   console.log('running');
-  // }
 }
 
 interface ICalculator {
