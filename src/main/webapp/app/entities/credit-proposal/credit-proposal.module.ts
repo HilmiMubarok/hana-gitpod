5a9a160bdff656e998b02fb961b22c064dcf4035
Ricky <ricky_riq@yahoo.com>
2022-09-28 13:11:18 +0700

    .

--- src/main/webapp/app/entities/credit-proposal/credit-proposal.module.ts
+++ src/main/webapp/app/entities/credit-proposal/credit-proposal.module.ts
@@ -16,13 +16,6 @@ import { CreditProposalFinancialStatementComponent } from './financial-statement
 import { ProposalBasicInformationComponent } from './proposal-basic-information.component';
 import { CreditProposalBankAccountAnalystComponent } from './bank-account-analyst/bank-account-analyst.component';
 import { CreditProposalBankAccountAnalystDialogComponent } from './bank-account-analyst/bank-account-analyst-dialog.component';
-import { SlikSummaryComponent } from './slik-summary/slik-summary.component';
-import { SlikSummaryDebiturComponent } from './slik-summary/debitur/slik-summary-debitur.component';
-import { SlikSummaryDebiturDialogComponent } from './slik-summary/debitur/slik-summary-debitur-dialog.component';
-import { SlikSummaryShareHolderComponent } from './slik-summary/share-holder/slik-summary-share-holder.component';
-import { SlikSummaryShareHolderDialogComponent } from './slik-summary/share-holder/slik-summary-share-holder-dialog.component';
-import { SlikSummaryBusinessGroupDialogComponent } from './slik-summary/business-group/slik-summary-business-group-dialog.component';
-import { SlikSummaryBusinessGroupComponent } from './slik-summary/business-group/slik-summary-business-group.component';
 import { CreditProposalCollateralInfoComponent } from './collateral-info/credit-proposal-collateral-info.component';
 import { CreditProposalCollateralInfoDialogComponent } from './collateral-info/dialog/credit-proposal-collateral-info-dialog.component';
 
@@ -32,13 +25,10 @@ import { DocumentChecklistDialogComponent } from './document-checklist/document-
 import { CreditProposalRepaymentCapabilityComponent } from './repayment-capability/credit-proposal-repayment-capability.component';
 import { RepaymentSpreadsheetComponent } from './repayment-spreadsheet/repayment-spreadsheet.component';
 
-import { CreditProposalLoanFacilityDetailComponent } from './credit-proposal-loan-facility-detail.component';
 import { CreditProposalApprovalTabSummaryComponent } from './credit-proposal-approval-tab-summary.component';
 import { CreditProposalListSlikSummaryListComponent } from './credit-proposal-slik-summary-list.component';
 import { CreditProposalSlikSummaryDetailComponent } from './credit-proposal-slik-summary-detail.component';
-import { CreditProposalTabLoanFacilityDetailComponent } from './credit-proposal-tab-loan-facility-detail.component';
 import { CreditProposalBankAccountAnalysisComponent } from './credit-proposal-bank-account-analysis';
-import { CreditProposalTabLoanFacilityDetailGridComponent } from './credit-proposal-tab-loan-facility-detail.grid.component';
 import { CreditProposalTabSummaryComponent } from './credit-proposal-tab-summary.component';
 import { CreditProposalTabRepaymentCapabilityComponent } from './credit-proposal-tab-repayment-capability.component';
 import { CreditProposalRiskAcceptanceCriteriaComponent } from './risk-criteria/credit-proposal-risk-acceptance-criteria-component';
@@ -65,6 +55,7 @@ import { CreditProposalListMaterialComponent } from './credit-proposal-list-mate
 
 import { ProposePricingLoanFacilityDetailComponent } from './propose-pricing/propose-pricing-loan-facility-detail.component';
 import { TradeCheckingComponent } from './trade-checking/credit-proposal-trade-checking.component';
+import { CreditProposalOpinionHistoryComponent } from './credit-proposal-opinion-history.component';
 
 @NgModule({
   imports: [SharedModule, SharedLibsModule, SharedEntityModule, RouterModule.forChild(creditProposalRoute)],
@@ -78,13 +69,7 @@ import { TradeCheckingComponent } from './trade-checking/credit-proposal-trade-c
     CreditProposalBankAccountAnalystComponent,
     ProposalBasicInformationComponent,
     CreditProposalBankAccountAnalystDialogComponent,
-    SlikSummaryComponent,
-    SlikSummaryDebiturComponent,
-    SlikSummaryDebiturDialogComponent,
-    SlikSummaryShareHolderComponent,
-    SlikSummaryShareHolderDialogComponent,
-    SlikSummaryBusinessGroupComponent,
-    SlikSummaryBusinessGroupDialogComponent,
+
     CreditProposalCollateralInfoComponent,
     CreditProposalCollateralInfoDialogComponent,
     CreditProposalTabCustomerProfitabilityComponent,
@@ -92,13 +77,10 @@ import { TradeCheckingComponent } from './trade-checking/credit-proposal-trade-c
     DocumentChecklistDialogComponent,
     CreditProposalRepaymentCapabilityComponent,
     RepaymentSpreadsheetComponent,
-    CreditProposalLoanFacilityDetailComponent,
     CreditProposalApprovalTabSummaryComponent,
     CreditProposalListSlikSummaryListComponent,
     CreditProposalSlikSummaryDetailComponent,
-    CreditProposalTabLoanFacilityDetailComponent,
     CreditProposalBankAccountAnalysisComponent,
-    CreditProposalTabLoanFacilityDetailGridComponent,
     CreditProposalTabSummaryComponent,
     CreditProposalTabRepaymentCapabilityComponent,
     CreditProposalRiskAcceptanceCriteriaComponent,
@@ -115,7 +97,7 @@ import { TradeCheckingComponent } from './trade-checking/credit-proposal-trade-c
     CreditProposalTradeCheckingSupplierDialogComponent,
     CreditProposalTradeCheckingSupplierDialogEditComponent,
     CreditProposalTabCovenantComponent,
-	CreditProposalCovenantDocumentTabDeviationComponent,
+    CreditProposalCovenantDocumentTabDeviationComponent,
     CreditProposalPersonalInfoComponent,
     ProposalBasicInformationViewComponent,
     CreditProposalPersonComponent,
