040126acd982943aa4a2b6b9860bbe43559cc5dd
yusupefendi <effendi@appinzpire.net>
2022-09-29 21:04:29 +0700

    handle error filed

---
+++ src/main/webapp/app/entities/credit-proposal/credit-proposal-tab-summary.model.ts
@@ -0,0 +1,14 @@
+export interface IProposalSummary {
+    message?: string;
+  }
+  
+  export class CreditTabSummary implements IProposalSummary {
+    constructor(
+      public message?: string,
+      public TabSummary?: IProposalSummary[]
+    ) {
+      this.message='<p>Strength :</p><br/><p>Opportunities : </p><br/><p>Weaknesses :</p><br/><p>Threats :</p>'
+      this.TabSummary = [];
+    }
+  }
+  
