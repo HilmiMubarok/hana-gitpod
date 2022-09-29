9c9e4f297964ea27d389075e40574a0c90b22c5c
Tony Robert <90840294+Tonirobert@users.noreply.github.com>
2022-09-29 19:40:31 +0700

    layout previous proposal

---
+++ src/main/webapp/app/entities/loan-analys/previous-proposal/loan-analys-previous-proposal.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ICreditProposal } from 'app/entities/credit-proposal/credit-proposal.model';
+import { CreditProposalService } from 'app/entities/credit-proposal/credit-proposal.service';
+
+@Component({
+  selector: 'jhi-loan-analys-previous-proposal',
+  templateUrl: './loan-analys-previous-proposal.component.html',
+  styleUrls: ['../previous-dar/loan-analys-previous-dar.css'],
+})
+export class LoanAnalysPreviousProposalComponent {
+  private id: number;
+  // private creditProposal: ICreditProposal;
+  public creditProposal: ICreditProposal;
+  public creditProposalItem: ICreditProposal;
+
+  constructor(private creditProposalService: CreditProposalService, protected activatedRoute: ActivatedRoute, private router: Router) {
+    this.creditProposal = this.activatedRoute.snapshot.data['loanAnalys'];
+    this.activatedRoute.params.subscribe(params => {
+      this.id = params['id'];
+    });
+  }
+}
