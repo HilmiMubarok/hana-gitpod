de14800b58141560f94ebb241b9d77b5a2f8fd74
Styvenzen Wijaya <styvenzenwijaya@yahoo.com>
2022-10-06 08:23:36 +0000

    Update shared-entity.module.ts

--- src/main/webapp/app/entities/shared-entity.module.ts
+++ src/main/webapp/app/entities/shared-entity.module.ts
@@ -166,8 +166,6 @@ import { LoanAnalysPreviousProposalComponent } from './loan-analys/previous/prev
 import { LoanAnalysPreviousDarComponent } from './loan-analys/previous/previous-dar/loan-analys-previous-dar.component'; /* jhipster-needle-import-entity-as-list - JHipster will add entity modules imports here */
 import { SlikSummaryComparisonComponent } from './credit-proposal/slik-summary/comparison-slik/slik-summary-comparison.component';
 import { LoanAnalysSlikIdebComponent } from './credit-proposal/slik-summary/ideb/loan-analys-slik-ideb.component';
-import { CollateralViewRealEstateComponent } from './collateral/collateral-view-realestate.component';
-import { CollateralPropertyListComponent } from './collateral-property/collateral-property-list.component';
 
 @NgModule({
   imports: [
@@ -337,9 +335,7 @@ import { CollateralPropertyListComponent } from './collateral-property/collatera
     CreditProposalTabExposureComponent,
     LoanAnalysPreviousProposalComponent,
     LoanAnalysPreviousDarComponent,
-    CreditProposalCollateralInfoRemarksComponent,
-    CollateralViewRealEstateComponent,
-    CollateralPropertyListComponent
+    CreditProposalCollateralInfoRemarksComponent
     /* jhipster-needle-declaration-entity-as-list */
   ],
   entryComponents: [],
@@ -457,29 +453,27 @@ import { CollateralPropertyListComponent } from './collateral-property/collatera
     SlikSummaryShareHolderComponent, // Remove Me
     SlikSummaryShareHolderDialogComponent, // Remove Me
     SlikSummaryBusinessGroupComponent, // Remove Me
-    SlikSummaryComparisonComponent,
-    LoanAnalysSlikIdebComponent,
-    CreditProposalPersonalInfoComponent,
-    CreditProposalPersonComponent,
-    CreditProposalTabSummaryComponent,
+    SlikSummaryComparisonComponent, // Remove Me
+    LoanAnalysSlikIdebComponent, // Remove Me
+    CreditProposalPersonalInfoComponent, // Remove Me
+    CreditProposalPersonComponent, // Remove Me
+    CreditProposalTabSummaryComponent, // Remove Me
     SlikSummaryBusinessGroupDialogComponent, // Remove Me
     CreditProposalCollateralInfoComponent, // Remove Me
     CreditProposalTabCovenantComponent, // Remove Me
-    CreditProposalCovenantDocumentTabDeviationComponent,
-    AddCoborowerComponent,
-    CreditProposalTabExposureComponent,
-    TotalExposureComponent,
-    LegalLendingComponent,
-    IndustryLimitComponent,
-    PostalAddressViewCustomComponent,
-    LoanAnalysPreviousProposalComponent,
-    LoanAnalysPreviousDarComponent,
-    CreditProposalCollateralInfoRemarksComponent,
-    DocumentUploadDialogComponent,
-    SurveyBatchViewComponent,
-    DocumentDetailDialogComponent,
-    CollateralViewRealEstateComponent,
-    CollateralPropertyListComponent,
+    CreditProposalCovenantDocumentTabDeviationComponent, // Remove Me
+    AddCoborowerComponent, // Remove Me
+    CreditProposalTabExposureComponent, // Remove Me
+    TotalExposureComponent, // Remove Me
+    LegalLendingComponent, // Remove Me
+    IndustryLimitComponent, // Remove Me
+    PostalAddressViewCustomComponent, // Remove Me
+    LoanAnalysPreviousProposalComponent, // Remove Me
+    LoanAnalysPreviousDarComponent, // Remove Me
+    CreditProposalCollateralInfoRemarksComponent, // Remove Me
+    DocumentUploadDialogComponent, // Remove Me
+    SurveyBatchViewComponent, // Remove Me
+    DocumentDetailDialogComponent // Remove Me
     /* jhipster-needle-as-list-export-shared-module - JHipster will add entity exports imports here */
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
