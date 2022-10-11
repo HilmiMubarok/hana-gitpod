f14a731916fd8c78b8e4ed93908e4b81b66dbf1f
andi lauw <andi.safariansyah7@gmail.com>
2022-10-11 17:13:23 +0700

    .

--- src/main/webapp/app/entities/credit-proposal/risk-criteria/back-to-back/credit-proposal-risk-acceptance-criteria-back-to-back-component.ts
+++ src/main/webapp/app/entities/credit-proposal/risk-criteria/back-to-back/credit-proposal-risk-acceptance-criteria-back-to-back-component.ts
@@ -26,7 +26,11 @@ export class CreditProposalAceptanceCriteriaBackToBackComponent implements OnIni
   }
 
   public documentType: string;
-  public remarks: string;
+  public remarks?: any = [];
+  public remarksTwo?: any = [];
+  public remarksThere?: any = [];
+  public remarksFour?: any = [];
+  public remarksFive?: any = [];
   public value: string;
 
   attributes: any;
@@ -41,32 +45,65 @@ export class CreditProposalAceptanceCriteriaBackToBackComponent implements OnIni
     this._item = item;
   }
 
-  public btnSave($event: any): void {}
+  public OnSelect(value: string, data: any): void {
+    console.log('bot', data, value);
+
+    this.dataGridOne[data.No - 1].value = value;
+    this.item.attributes['cpRacBack'].topGrid = this.dataGridOne;
+  }
+
+  public OnnSelect(value: string, data: any): void {
+    // console.log('bot', data, value);
+
+    this.dataGridTwo[data.No - 1].value = value;
+    this.item.attributes['cpRacBack'].topGridTwo = this.dataGridTwo;
+  }
+
+  public OnNSelect(value: string, data: any): void {
+    // console.log('bot', data, value);
+
+    this.dataGridThre[data.No - 1].value = value;
+    this.item.attributes['cpRacBack'].topGridThere = this.dataGridThre;
+  }
+
+  public OnNSeleect(value: string, data: any): void {
+    // console.log('bot', data, value);
+
+    this.dataGridFour[data.No - 1].value = value;
+    this.item.attributes['cpRacBack'].topGridFour = this.dataGridFour;
+  }
+
+  public OnNSelectt(value: string, data: any): void {
+    // console.log('bot', data, value);
+
+    this.dataGridFive[data.No - 1].value = value;
+    this.item.attributes['cpRacBack'].topGridFive = this.dataGridFive;
+  }
 
   public dataGridOne = [
     {
       No: 1,
       documentType: 'Have met the Anti Money Laundring requirements',
-      Verified: !0,
       value: 'YES',
+      remarks: '',
     },
     {
       No: 2,
       documentType: 'It has been checked that the debtor and the owner of the collateral do not have problems in taxation',
-      Verified: !2,
       value: 'YES',
+      remarks: '',
     },
     {
       No: 3,
       documentType: 'It has been checked that the debtor and the owner of the collateral do not have negative information',
-      Verified: !3,
       value: 'YES',
+      remarks: '',
     },
     {
       No: 4,
       documentType: 'Prospective debtors are not included in the PEP (Politically Exposed Person)',
-      Verified: !4,
       value: 'YES',
+      remarks: '',
     },
   ];
 
@@ -75,20 +112,23 @@ export class CreditProposalAceptanceCriteriaBackToBackComponent implements OnIni
       No: 1,
       documentType:
         'The minimum time deposit tenor is 12 (twelve) months and/or the minimum deposit tenor is the same as the credit facility period.',
-      Verified: !0,
+
       value: 'YES',
+      remarksTwo: '',
     },
     {
       No: 2,
       documentType: 'Time deposits as a collateral and loans must be recorded at the same branch office',
-      Verified: !2,
+
       value: 'YES',
+      remarksTwo: '',
     },
     {
       No: 3,
       documentType: 'Time Deposit placement must be done all at once and should not be done in stages',
-      Verified: !3,
+
       value: 'YES',
+      remarksTwo: '',
     },
   ];
 
@@ -96,21 +136,24 @@ export class CreditProposalAceptanceCriteriaBackToBackComponent implements OnIni
     {
       No: 1,
       documentType: 'Hana Bank SBLC Financial Format Standard',
-      Verified: !0,
+
       value: 'YES',
+      remarksThere: '',
     },
     {
       No: 2,
       documentType: 'Issued by banks thats categorized as prime bank',
-      Verified: !2,
+
       value: 'YES',
+      remarksThere: '',
     },
     {
       No: 3,
       documentType:
         'The maturity date of the Standby L/C is at least 1 (one) month longer than the end date of the credit agreement or for SBLC from Hana Seoul, the SBLC period is 14 calendar days longer than the credit facility term.',
-      Verified: !3,
+
       value: 'YES',
+      remarksThere: '',
     },
   ];
 
@@ -118,14 +161,16 @@ export class CreditProposalAceptanceCriteriaBackToBackComponent implements OnIni
     {
       No: 1,
       documentType: 'Collateral and loan must be recorded at the same branch office.',
-      Verified: !0,
+
       value: 'YES',
+      remarksFour: '',
     },
     {
       No: 2,
       documentType: 'The placement of Savings/Giro funds must be all at once and may not be gradual.',
-      Verified: !2,
+
       value: 'YES',
+      remarksFour: '',
     },
   ];
 
@@ -133,15 +178,84 @@ export class CreditProposalAceptanceCriteriaBackToBackComponent implements OnIni
     {
       No: 1,
       documentType: 'Debiting must be done all at once and should not be done in stages.',
-      Verified: !0,
+
       value: 'YES',
+      remarksFive: '',
     },
   ];
+  onKeyUpEvent() {
+    for (let h = 0; h < this.dataGridOne.length; h++) {
+      this.dataGridOne[h].remarks = this.remarks[h];
+    }
+
+    this.item.attributes['cpRacBack'].remarks = this.dataGridOne;
+  }
+
+  OnKeyUpEvent() {
+    for (let h = 0; h < this.dataGridTwo.length; h++) {
+      this.dataGridTwo[h].remarksTwo = this.remarksTwo[h];
+    }
+
+    this.item.attributes['cpRacBack'].remarksTwo = this.dataGridTwo;
+  }
+
+  OnKeyyUpEvent() {
+    for (let h = 0; h < this.dataGridThre.length; h++) {
+      this.dataGridThre[h].remarksThere = this.remarksThere[h];
+    }
+
+    this.item.attributes['cpRacBack'].remarksThere = this.remarksThere;
+  }
+
+  OnKeyyUppEvent() {
+    for (let h = 0; h < this.dataGridFour.length; h++) {
+      this.dataGridFour[h].remarksFour = this.remarksFour[h];
+    }
+
+    this.item.attributes['cpRacBack'].remarksFour = this.remarksFour;
+  }
+
+  OnKeyyUppEventt() {
+    for (let h = 0; h < this.dataGridFive.length; h++) {
+      this.dataGridFive[h].remarksFive = this.remarksFive[h];
+    }
+
+    this.item.attributes['cpRacBack'].remarksFive = this.remarksFive;
+  }
 
   ngOnInit(): void {
-    if (this.item.attributes['cpRacBack'].checklistValueBelow.length !== 0) {
+    if (this.item.attributes['cpRacBack'].topGrid.length !== 0) {
       for (let i = 0; i < this.item.attributes['cpRacBack'].topGrid.length; i++) {
         this.dataGridOne = this.item.attributes['cpRacBack'].topGrid;
+        this.remarks[i] = this.item.attributes['cpRacBack'].topGrid[i].remarks;
+      }
+    }
+
+    if (this.item.attributes['cpRacBack'].topGridTwo.length !== 0) {
+      for (let i = 0; i < this.item.attributes['cpRacBack'].topGridTwo.length; i++) {
+        this.dataGridTwo = this.item.attributes['cpRacBack'].topGridTwo;
+        this.remarksTwo[i] = this.item.attributes['cpRacBack'].topGridTwo[i].remarksTwo;
+      }
+    }
+
+    if (this.item.attributes['cpRacBack'].topGridThere.length !== 0) {
+      for (let i = 0; i < this.item.attributes['cpRacBack'].topGridThere.length; i++) {
+        this.dataGridThre = this.item.attributes['cpRacBack'].topGridThere;
+        this.remarksThere[i] = this.item.attributes['cpRacBack'].topGridThere[i].remarksThere;
+      }
+    }
+
+    if (this.item.attributes['cpRacBack'].topGridFour.length !== 0) {
+      for (let i = 0; i < this.item.attributes['cpRacBack'].topGridFour.length; i++) {
+        this.dataGridFour = this.item.attributes['cpRacBack'].topGridFour;
+        this.remarksFour[i] = this.item.attributes['cpRacBack'].topGridFour[i].remarksFour;
+      }
+    }
+
+    if (this.item.attributes['cpRacBack'].topGridFive.length !== 0) {
+      for (let i = 0; i < this.item.attributes['cpRacBack'].topGridFive.length; i++) {
+        this.dataGridFive = this.item.attributes['cpRacBack'].topGridFive;
+        this.remarksFive[i] = this.item.attributes['cpRacBack'].topGridFive[i].remarksFive;
       }
     }
   }
--- src/main/webapp/app/entities/credit-proposal/risk-criteria/below/risk-criteria-below.model.ts
+++ src/main/webapp/app/entities/credit-proposal/risk-criteria/below/risk-criteria-below.model.ts
@@ -1,7 +1,16 @@
 export interface IBelow {
   parameterBelow?: string;
   value?: string;
-  remaks?: string;
+  status?: string;
+  remarks?: string;
+  CsRemaks?: string;
+  CvRemaks?: string;
+  CiRemaks?: string;
+  CaRemaks?: string;
+  Cs?: string;
+  Cv?: string;
+  Ci?: string;
+  Ca?: string;
   collateralStatus?: string;
   collateralCoverage?: string;
   creditApplication?: string;
@@ -12,24 +21,52 @@ export class CpRacBelow implements IBelow {
   constructor(
     public parameterBelow?: string,
     public value?: string,
-    public remaks?: string,
+    public status?: string,
+
+    public remarks?: string,
+    public Cv?: string,
+    public Cs?: string,
+    public Ci?: string,
+    public Ca?: string,
+    public CsRemaks?: string,
+    public CvRemaks?: string,
+    public CiRemaks?: string,
+    public CaRemaks?: string,
     public collateralStatus?: string,
     public collateralCoverage?: string,
     public creditApplication?: string,
     public collateralInsurance?: string,
     public cpValueBot?: IBelow[],
-    public cpValueBelow?: IBelow[],
+    // public inputRemaks?: IBelow[],
+    public cpValeuTwo?: IBelow[],
+    public cpValeuThere?: IBelow[],
+    public cpValeuFour?: IBelow[],
+    public cpValeuFive?: IBelow[],
     public lovBelow?: IBelow[]
   ) {
     this.parameterBelow = '';
     this.value = '';
-    this.remaks = '';
+    this.status = '';
+    this.remarks = '';
+    this.Cs = '';
+    this.Cv = '';
+    this.Ci = '';
+    CsRemaks = '';
+    CvRemaks = '';
+    CiRemaks = '';
+    CaRemaks = '';
+
     this.collateralStatus = '';
     this.collateralCoverage = '';
     this.creditApplication = '';
     this.collateralInsurance = '';
+
     this.cpValueBot = [];
-    this.cpValueBelow = [];
+    // this.inputRemaks = [];
+    this.cpValeuTwo = [];
+    this.cpValeuThere = [];
+    this.cpValeuFour = [];
+    this.cpValeuFive = [];
     this.lovBelow = [];
   }
 }
