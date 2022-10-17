8e301ec00ea9f0d7c94a2528b6d8e7d80266b29b
Styvenzen Wijaya <styvenzenwijaya@yahoo.com>
2022-10-17 11:48:57 +0000

    Update credit-proposal.module.ts

--- src/main/webapp/app/entities/credit-proposal/credit-proposal.module.ts
+++ src/main/webapp/app/entities/credit-proposal/credit-proposal.module.ts
@@ -31,7 +31,6 @@ import { CreditProposalTradeCheckingSupplierDialogComponent } from './trade-chec
 import { CreditProposalTradeCheckingSupplierDialogEditComponent } from './trade-checking/supplier/edit/credit-proposal-trade-checking-supplier-dialog-edit.component';
 import { CreditProposalNewDialogComponent } from './credit-proposal-new-dialog.component';
 import { CreditProposalListMaterialComponent } from './credit-proposal-list-material.component';
-import { CreditProposalCollateralInfoChecklistComponent } from './collateral-info/checklist/credit-proposal-collateral-info-checklist.component';
 import { TradeCheckingComponent } from './trade-checking/credit-proposal-trade-checking.component';
 import { CreditProposalLoanFacilityDialogComponent } from './loan-facility/dialog/loan-facility-dialog.component';
 
@@ -63,8 +62,7 @@ import { CreditProposalLoanFacilityDialogComponent } from './loan-facility/dialo
     ProposalBasicInformationViewComponent,
     CreditProposalNewComponent,
     CreditProposalNewDialogComponent,
-    CreditProposalListMaterialComponent,
-    CreditProposalCollateralInfoChecklistComponent,
+    CreditProposalListMaterialComponent
   ],
   entryComponents: [CreditProposalNewDialogComponent],
   providers: [PageService, ToolbarService, EditService],
