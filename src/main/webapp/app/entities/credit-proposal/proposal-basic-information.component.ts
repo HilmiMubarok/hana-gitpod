fbcc20a0508b91a271b9814790bf512ab16f3289
styvenz <styvenzenwijaya@yahoo.com>
2022-10-25 07:00:51 +0700

    Fix save credit proposal

--- src/main/webapp/app/entities/credit-proposal/proposal-basic-information.component.ts
+++ src/main/webapp/app/entities/credit-proposal/proposal-basic-information.component.ts
@@ -236,7 +236,8 @@ export class ProposalBasicInformationComponent implements OnInit {
     copyCreditProposal.attributes['collateralPrevious'] = JSON.stringify(copyCreditProposal.attributes['collateralPrevious']);
     copyCreditProposal.attributes['facilityTakeOver'] = JSON.stringify(copyCreditProposal.attributes['facilityTakeOver']);
     copyCreditProposal.attributes['facilityTakeOverAfterBank'] = JSON.stringify(copyCreditProposal.attributes['facilityTakeOverAfterBank']);
-    copyCreditProposal.attributes['complienceRecommendation'] = JSON.stringify(copyCreditProposal.attributes['complienceRecommendation']);
+    copyCreditProposal.attributes['complienceReccommendation'] = JSON.stringify(copyCreditProposal.attributes['complienceReccommendation']);
+
     return copyCreditProposal;
   }
 
