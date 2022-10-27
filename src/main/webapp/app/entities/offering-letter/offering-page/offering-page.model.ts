64479fabb324bcf9d1dffcb968312eb839ae28c0
yusupefendi <effendi@appinzpire.net>
2022-10-27 13:32:16 +0700

    offering-letter input date and offering letter number

--- src/main/webapp/app/entities/offering-letter/offering-page/offering-page.model.ts
+++ src/main/webapp/app/entities/offering-letter/offering-page/offering-page.model.ts
@@ -5,13 +5,17 @@ export interface IOfferingLetter {
   debitorNameGroup?: string;
   debitorNamePerson?: string;
   debitorType?: string;
+  noLetter?: string;
+  dateOffering?: string;
 }
 
 export class OfferingLetter implements IOfferingLetter {
-  constructor(public id?: number, public debitorNameGroup?: string, public debitorNamePerson?: string, public debitorType?: string) {
+  constructor(public id?: number, public noLetter?: string, public dateOffering?: string, public debitorNameGroup?: string, public debitorNamePerson?: string, public debitorType?: string) {
     this.id = uuid.v4();
     this.debitorNameGroup = '';
     this.debitorNamePerson = '';
     this.debitorType = '';
+    this.noLetter = '';
+    this.dateOffering = '';
   }
 }
