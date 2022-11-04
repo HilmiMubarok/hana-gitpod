0814c916ae31097bd17ccedfdcd263cd203a3ec3
Styvenzen Wijaya <styvenzenwijaya@yahoo.com>
2022-11-04 11:53:08 +0000

    Revert "Merge branch '387-resolve-dialog-collateral-info' into 'main'"
    This reverts merge request !446

--- src/main/webapp/app/entities/application-product/application-product.model.ts
+++ src/main/webapp/app/entities/application-product/application-product.model.ts
@@ -80,7 +80,6 @@ export interface IApplicationProductAttribute {
   subLimitFromExitingFacility?: string;
   indexFacilityMain?: string;
   loanType?: string;
-  disbursementCondition?: string;
 }
 
 export class ApplicationProductAttribute implements IApplicationProductAttribute {
@@ -130,8 +129,7 @@ export class ApplicationProductAttribute implements IApplicationProductAttribute
     public remark?: string,
     public subLimitFromExitingFacility?: string,
     public indexFacilityMain?: string,
-    public loanType?: string,
-    public disbursementCondition?: string
+    public loanType?: string
   ) {
     this.applicationType = 'Existing';
     this.facilityType = '';
@@ -177,6 +175,5 @@ export class ApplicationProductAttribute implements IApplicationProductAttribute
     this.subLimitFromExitingFacility = '';
     this.indexFacilityMain = '';
     this.loanType = '';
-    this.disbursementCondition= '';
   }
 }
