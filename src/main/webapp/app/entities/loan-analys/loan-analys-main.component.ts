54d4868932178047de3a5877ea6565f10d6547af
styvenz <styvenzenwijaya@yahoo.com>
2022-11-05 13:08:41 +0700

    1. Fix click status event @credit proposal 2. Fix assign to @loan analysis

--- src/main/webapp/app/entities/loan-analys/loan-analys-main.component.ts
+++ src/main/webapp/app/entities/loan-analys/loan-analys-main.component.ts
@@ -201,6 +201,7 @@ export class LoanAnalysMainComponent implements OnInit {
         this.applicationRole.partyName = this.position[i].employeeFirstName;
         this.applicationRole.roleId = this.position[i].positionTypeId;
         this.applicationRole.roleDescription = this.position[i].positionTypeDescription;
+		this.applicationRole.applicationId = this.creditProposal.id;
       }
     }
   }
