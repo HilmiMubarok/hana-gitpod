06d3cb7046815fc5c3cc00ddfa613c8f59b6751c
yusupefendi <effendi@appinzpire.net>
2022-11-10 21:37:19 +0700

    can not save main

--- src/main/webapp/app/entities/credit-proposal/collateral-info/bellow-grid/bellow-grid.component.ts
+++ src/main/webapp/app/entities/credit-proposal/collateral-info/bellow-grid/bellow-grid.component.ts
@@ -483,7 +483,7 @@ export class BellowGridComponent implements OnChanges {
             collaterals[i].collateralTypeId !== COLLATERAL_TYPE['property'] ||
             collaterals[i].collateralTypeId !== COLLATERAL_TYPE['machine']
           ) {
-            result = result + data.marketValue;
+            result = result + data?.marketValue;
           }
         }
       }
