6c1515ceaa88321b8bf333dadf5476261549918d
ismoyo23 <p4sukanlinuc>
2022-11-10 13:55:30 +0700

    update

--- src/main/webapp/app/entities/collateral-appraisal/info/collateral-appraisal-info.component.ts
+++ src/main/webapp/app/entities/collateral-appraisal/info/collateral-appraisal-info.component.ts
@@ -203,8 +203,8 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
     }
 
     if (changes.statusAppraisal.currentValue.length > 0) {
-      console.log('dataaa', changes.statusAppraisal.currentValue);
-      for (let i = 0; i < changes.statusAppraisal.currentValue.length; i++) {
+      for (let i = changes.statusAppraisal.currentValue.length - 1; i >= 0; i--) {
+        console.log('data log', changes.statusAppraisal.currentValue[i]);
         if (changes.statusAppraisal.currentValue[i].status === 'APPROVAL' || changes.statusAppraisal.currentValue[i].status === 'VISITED') {
           if (changes.statusAppraisal.currentValue[i].status === 'APPROVAL') {
             this.approvalDate = changes.statusAppraisal.currentValue[i].createdDate;
