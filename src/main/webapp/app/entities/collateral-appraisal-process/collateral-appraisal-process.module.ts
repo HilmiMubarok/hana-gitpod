a816bab29f9c8ca8c03be1255606b89b6af5c05e
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2022-11-23 13:07:50 +0700

    route and module appraisal process

--- src/main/webapp/app/entities/collateral-appraisal-process/collateral-appraisal-process.module.ts
+++ src/main/webapp/app/entities/collateral-appraisal-process/collateral-appraisal-process.module.ts
@@ -3,11 +3,12 @@ import { RouterModule } from '@angular/router';
 import { SharedLibsModule } from 'app/shared/shared-libs.module';
 import { SharedModule } from 'app/shared/shared.module';
 import { SharedEntityModule } from '../shared-entity.module';
+import { CollateralAppraisalProcessMaterialComponent } from './collateral-appraisal-process-material.component';
 import { CollateralAppraisalProcessRoute } from './collateral-appraisal-process.route';
 
 @NgModule({
   imports: [SharedModule, SharedLibsModule, SharedEntityModule, RouterModule.forChild(CollateralAppraisalProcessRoute)],
-  declarations: [],
+  declarations: [CollateralAppraisalProcessMaterialComponent],
   entryComponents: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
