47e2d0fd3f0833b875578dc3929064b613057499
anjarfals07 <anjarfals07@gmail.com>
2022-12-07 19:47:21 +0700

    add opinion on cp approval and word opinion

--- src/main/webapp/app/entities/credit-proposal/opinion-history/credit-proposal-opinion-history.component.ts
+++ src/main/webapp/app/entities/credit-proposal/opinion-history/credit-proposal-opinion-history.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, OnChanges, SimpleChanges } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 
@@ -10,15 +10,41 @@ import { ICreditProposal } from '../credit-proposal.model';
 import { CreditProposalDialogOpinionHistoryComponent } from './dialog-opinion-history/credit-proposal-dialog-opinion-history.component';
 
 import lodash from 'lodash';
+import {
+  DocumentEditorComponent,
+  DocumentEditorContainerComponent,
+  DocumentEditorKeyDownEventArgs,
+  EditorService,
+  SelectionService,
+  SfdtExportService,
+} from '@syncfusion/ej2-angular-documenteditor';
+import { ActivatedRoute, Router } from '@angular/router';
+import { StorageService } from 'app/entities/storage/storage.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'jhi-credit-proposal-opinion-history',
   templateUrl: './credit-proposal-opinion-history.component.html',
   styleUrls: ['./opinion-history.css'],
+  providers: [SelectionService, EditorService, SfdtExportService],
 })
-export class CreditProposalOpinionHistoryComponent implements OnInit {
+export class CreditProposalOpinionHistoryComponent implements OnInit, OnChanges {
+  @ViewChild('document_editor_container')
+  public container: DocumentEditorContainerComponent;
+  @ViewChild('document_editor')
+  public documentEditor: DocumentEditorComponent;
+  @Input() saveWordMinio;
+  @Input() cp: ICreditProposal;
+
   public _creditProposalItem: ICreditProposal;
   public notes: any;
+
+  private bucket: string;
+  private ngUnsubscribe = new Subject();
+  private paramsIdGet: string;
+  private getKey: string;
+  private fileGet: File;
+
   @Input()
   get creditProposalItem() {
     return this._creditProposalItem;
@@ -30,9 +56,14 @@ export class CreditProposalOpinionHistoryComponent implements OnInit {
       this.notes = lodash.cloneDeep(this.creditProposalItem.notes);
       for (let i = 0; i < this.notes.length; i++) {
         this.notes[i].message = this.notes[i].message ? this.notes[i].message.replace(/<(?:.|\n)*?>/gm, '') : '';
+        this.notes[i].condition = this.notes[i].condition ? this.notes[i].condition.replace(/<(?:.|\n)*?>/gm, '') : '';
         this.notes[i].createDate = this.notes[i].createDate ? this.datePipe.transform(this.notes[i].createDate, 'yyyy-MM-dd') : '';
       }
     }
+    // if (this.saveWordMinio) {
+    //   this.triggeredSave();
+    // }
+    // this.getContainer();
   }
 
   public tools: ToolbarModule = {
@@ -55,20 +86,52 @@ export class CreditProposalOpinionHistoryComponent implements OnInit {
     ],
   };
 
-  constructor(public accountService: AccountService, public dialog: MatDialog, public datePipe: DatePipe) {}
+  constructor(
+    public accountService: AccountService,
+    public dialog: MatDialog,
+    public datePipe: DatePipe,
+    protected activatedRoute: ActivatedRoute,
+    protected router: Router,
+    private storageService: StorageService
+  ) {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    // if (changes.creditProposalItem.currentValue.notes.length > 0) {
+    //   this.notes = lodash.cloneDeep(changes.creditProposalItem.currentValue.notes);
+    //   for (let i = 0; i < this.notes.length; i++) {
+    //     this.notes[i].message = this.notes[i].message ? this.notes[i].message.replace(/<(?:.|\n)*?>/gm, '') : '';
+    //     this.notes[i].condition = this.notes[i].condition ? this.notes[i].condition.replace(/<(?:.|\n)*?>/gm, '') : '';
+    //     this.notes[i].createDate = this.notes[i].createDate ? this.datePipe.transform(this.notes[i].createDate, 'yyyy-MM-dd') : '';
+    //   }
+    // }
+    if (this.saveWordMinio) {
+      this.triggeredSave();
+    }
+    this.getContainer();
+  }
   ngOnInit(): void {
+    this.bucket = 'hana';
+    this.activatedRoute.params.subscribe(params => {
+      this.paramsIdGet = params['id'];
+      this.getKey = 'credit_proposal/remark/opinion/' + this.paramsIdGet + '/sfdt';
+      this.getContainer();
+    });
     this.accountService.identity().subscribe(account => {
       const currentAccount = account;
       this.creditProposalItem.attributes['tempLoggedInNotes'] = '';
+      this.creditProposalItem.attributes['tempLoggedInRecomendation'] = '';
+      this.creditProposalItem.attributes['tempLoggedInCondition'] = '';
       if (this.creditProposalItem.notes.length > 0) {
         for (let i = 0; i < this.creditProposalItem.notes.length; i++) {
           if (this.creditProposalItem.notes[i].userId === currentAccount.login) {
             this.creditProposalItem.attributes['tempLoggedInNotes'] = this.creditProposalItem.notes[i].message;
+            this.creditProposalItem.attributes['tempLoggedInRecomendation'] = this.creditProposalItem.notes[i].recomendation;
+            this.creditProposalItem.attributes['tempLoggedInCondition'] = this.creditProposalItem.notes[i].condition;
           }
         }
       }
     });
+    this.getContainer();
   }
 
   public openDialog(element: INotes = null): void {
@@ -81,5 +144,96 @@ export class CreditProposalOpinionHistoryComponent implements OnInit {
     };
 
     const dialogRef = this.dialog.open(CreditProposalDialogOpinionHistoryComponent, predicate);
+  }
+
+  // Word Save
+  public triggeredSave(): void {
+    let paramsId = '';
+    this.activatedRoute.params.subscribe(params => {
+      paramsId = params['id'];
+    });
+    const key = 'credit_proposal/remark/opinion';
+
+    const timeStamp = Math.floor(Date.now() / 1000);
+
+    const docEditor = this.container?.documentEditor as DocumentEditorComponent;
+
+    docEditor.saveAsBlob('Docx').then((exportedDocument: Blob) => {
+      const fileType = 'word';
+      const fileName = 'credit-proposal-remark-' + paramsId + '-opinion-' + fileType + '.docs';
+      const metaData = {
+        objectName: `${key}/${paramsId}/${fileType}/${fileName}`,
+      };
+      const formData = new FormData();
+      formData.append('file', new File([exportedDocument], fileName));
+
+      this.storageService.uploadMeta('hana', formData, metaData).subscribe();
+    });
+
+    docEditor.saveAsBlob('Sfdt').then((exportedDocument: Blob) => {
+      const fileType = 'sfdt';
+      const fileName = 'credit-proposal-remark-' + paramsId + '-opinion-' + fileType + '.sfdt';
+      const metaData = {
+        objectName: `${key}/${paramsId}/${fileType}/${fileName}`,
+      };
+      const formData = new FormData();
+      formData.append('file', new File([exportedDocument], fileName));
+
+      this.storageService.uploadMeta('hana', formData, metaData).subscribe();
+    });
+  }
+
+  public onKeyDown(args: DocumentEditorKeyDownEventArgs): void {
+    const keyCode: string = args.event.key;
+    const isCtrlKey: boolean = args.event.ctrlKey || args.event.metaKey ? true : keyCode === '17' ? true : false;
+    // 67 is the character code for 'C'
+    console.log('keycode', keyCode);
+    console.log('isCtrlKey', isCtrlKey);
+    if (isCtrlKey && keyCode === '86') {
+      // To prevent copy operation set isHandled to true
+      args.isHandled = true;
+      console.log('ini paste');
+    }
+  }
+  private getContainer(): void {
+    const obj = {
+      key: this.getKey,
+    };
+    this.storageService
+      .getObjects(this.bucket, obj)
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(response => {
+        if (response.body.length > 0) {
+          this.storageService
+            .fileBlob(response.body[response.body.length - 1]['url'])
+            .pipe(takeUntil(this.ngUnsubscribe))
+            .subscribe(res => {
+              this.fileGet = new File([res.body], 'credit-proposal-remark-' + this.paramsIdGet + '-opinion-sfdt.sfdt');
+              const fileReader: FileReader = new FileReader();
+              fileReader.onload = (e: any) => {
+                const docEditor = this.container?.documentEditor as DocumentEditorComponent;
+                // const contents: string = e.target.result;
+                const contents: any = e.target.result;
+                console.log('target', e.target);
+                docEditor.open(contents);
+                // .blocks[0].inlines[0].text
+                // e.sections[0].blocks[0].inlines['text']
+                console.log('ini contents', e.target.sections);
+                console.log('ini container', this.container);
+                console.log('tes res', res);
+              };
+              fileReader.readAsText(this.fileGet);
+              console.log('ini file get', this.fileGet);
+            });
+        }
+      });
+  }
+  myFunction(value: string) {
+    console.log('cek value', value);
+  }
+
+  onCreate(): void {
+    // this.container.serviceUrl = 'http://45.32.114.128:8190/services/los/api/wordeditor/';
+    this.container.serviceUrl = 'https://ej2services.syncfusion.com/production/web-services/api/documenteditor/';
   }
 }
--- src/main/webapp/app/entities/credit-proposal/opinion-history/dialog-opinion-history/credit-proposal-dialog-opinion-history.component.ts
+++ src/main/webapp/app/entities/credit-proposal/opinion-history/dialog-opinion-history/credit-proposal-dialog-opinion-history.component.ts
@@ -1,15 +1,36 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, SimpleChanges, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+  DocumentEditorComponent,
+  DocumentEditorContainerComponent,
+  EditorService,
+  SelectionService,
+  SfdtExportService,
+} from '@syncfusion/ej2-angular-documenteditor';
+import { StorageService } from 'app/entities/storage/storage.service';
+import { Subject, takeUntil } from 'rxjs';
 import { ICreditProposal } from '../../credit-proposal.model';
 
 @Component({
   selector: 'jhi-credit-proposal-dialog-opinion-history',
   templateUrl: './credit-proposal-dialog-opinion-history.component.html',
-  styleUrls: ['../../css/credit-proposal-basic-information.css'],
+  styleUrls: ['../opinion-history.css'],
+  providers: [SelectionService, EditorService, SfdtExportService],
 })
-export class CreditProposalDialogOpinionHistoryComponent {
+export class CreditProposalDialogOpinionHistoryComponent implements OnInit {
+  @ViewChild('document_editor_container')
+  public container: DocumentEditorContainerComponent;
+  @ViewChild('document_editor')
+  public documentEditor: DocumentEditorComponent;
+
   public notes: any;
   public creditProposalItem: ICreditProposal;
+  private bucket: string;
+  private ngUnsubscribe = new Subject();
+  private paramsIdGet: string;
+  private getKey: string;
+  private fileGet: File;
 
   constructor(
     @Inject(MAT_DIALOG_DATA)
@@ -17,9 +38,56 @@ export class CreditProposalDialogOpinionHistoryComponent {
       notes: any;
       creditProposalItem: ICreditProposal;
     },
-    _dialog: MatDialogRef<CreditProposalDialogOpinionHistoryComponent>
+    _dialog: MatDialogRef<CreditProposalDialogOpinionHistoryComponent>,
+    private storageService: StorageService,
+    protected activatedRoute: ActivatedRoute,
+    protected router: Router
   ) {
     this.notes = this.dataNotes.notes;
     this.creditProposalItem = this.dataNotes.creditProposalItem;
+  }
+  ngOnInit(): void {
+    this.bucket = 'hana';
+    this.activatedRoute.params.subscribe(params => {
+      this.paramsIdGet = params['id'];
+      this.getKey = 'credit_proposal/remark/opinion/' + this.creditProposalItem.id + '/sfdt';
+      this.getContainer();
+    });
+  }
+  onDocumentChange() {
+    this.container.restrictEditing = true;
+  }
+  private getContainer(): void {
+    const obj = {
+      key: this.getKey,
+    };
+    this.storageService
+      .getObjects(this.bucket, obj)
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(response => {
+        if (response.body.length > 0) {
+          this.storageService
+            .fileBlob(response.body[response.body.length - 1]['url'])
+            .pipe(takeUntil(this.ngUnsubscribe))
+            .subscribe(res => {
+              this.fileGet = new File([res.body], 'credit-proposal-remark-' + this.creditProposalItem.id + '-opinion-sfdt.sfdt');
+              const fileReader: FileReader = new FileReader();
+              fileReader.onload = (e: any) => {
+                const docEditor = this.container?.documentEditor as DocumentEditorComponent;
+                // const contents: string = e.target.result;
+                const contents: any = e.target.result;
+                console.log('target', e.target);
+                docEditor.open(contents);
+                // .blocks[0].inlines[0].text
+                // e.sections[0].blocks[0].inlines['text']
+                console.log('ini contents', e.target.sections);
+                console.log('ini container', this.container);
+                console.log('tes res', res);
+              };
+              fileReader.readAsText(this.fileGet);
+              console.log('ini file get', this.fileGet);
+            });
+        }
+      });
   }
 }
