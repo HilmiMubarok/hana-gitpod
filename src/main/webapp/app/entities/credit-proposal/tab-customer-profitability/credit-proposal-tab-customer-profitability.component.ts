2a4ae006cb5238666e141be0f3ab7d67cf60c78d
Tony Robert <90840294+Tonirobert@users.noreply.github.com>
2022-12-09 20:51:35 +0700

    remak to word customer pratability

--- src/main/webapp/app/entities/credit-proposal/tab-customer-profitability/credit-proposal-tab-customer-profitability.component.ts
+++ src/main/webapp/app/entities/credit-proposal/tab-customer-profitability/credit-proposal-tab-customer-profitability.component.ts
@@ -8,18 +8,43 @@ import { CreditProposal, ICreditProposal } from '../credit-proposal.model';
 import { CreditProposalService } from '../credit-proposal.service';
 import { ICustomer, TabCustomerProfitability } from './tab-customert-profitability.model';
 import { CurrencyMaskConfig } from 'ngx-currency';
+import {
+  DocumentEditorComponent,
+  DocumentEditorContainerComponent,
+  DocumentEditorKeyDownEventArgs,
+  EditorService,
+  SelectionService,
+  SfdtExportService,
+} from '@syncfusion/ej2-angular-documenteditor';
+import { StorageService } from 'app/entities/storage/storage.service';
+import { takeUntil, Subject } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'jhi-credit-proposal-tab-customer-profitability',
   templateUrl: './credit-proposal-tab-customer-profitability.component.html',
   styleUrls: ['./credit-proposal-tab-customer-profitability.scss'],
+  providers: [SelectionService, EditorService, SfdtExportService],
 })
 export class CreditProposalTabCustomerProfitabilityComponent implements OnInit {
+  private _item: ICreditProposal;
+
+  private bucket: string;
+  private ngUnsubscribe = new Subject();
+  private paramsIdGet: string;
+  private getKey: string;
+  private fileGet: File;
+
+  attributes: any;
+  // public _item: ICreditProposal;
   dataAttr: Object[];
   dataSave: any[];
   constructor(
     protected creditProposalService: CreditProposalService,
-    protected positionService: PositionService // protected parseLinks: ParseLinks, // protected accoutService: AccountService, // protected activateRoute: ActivatedRoute, // protected dataUtils: BaseDataUtils, // protected router: Router, // protected eventManager: EventManager, // protected messageService: MessageService, // protected confirmationService: ConfirmationService
+    protected positionService: PositionService,
+    private router: Router,
+    protected activatedRoute: ActivatedRoute,
+    private storageService: StorageService // protected parseLinks: ParseLinks, // protected accoutService: AccountService, // protected activateRoute: ActivatedRoute, // protected dataUtils: BaseDataUtils, // protected router: Router, // protected eventManager: EventManager, // protected messageService: MessageService, // protected confirmationService: ConfirmationService
   ) {}
 
   public creditProposaldata: ICreditProposal = new CreditProposal();
@@ -37,7 +62,7 @@ export class CreditProposalTabCustomerProfitabilityComponent implements OnInit {
   public roa: number;
   public value: string;
   public parameter: string;
-  public remarks: string;
+  // public remarks: string;
   public remarks1?: any = [];
 
   public dataAttrPass = [
@@ -110,10 +135,7 @@ export class CreditProposalTabCustomerProfitabilityComponent implements OnInit {
 
     this.item.attributes['tabCustomer'].remarks1 = this.dataAttrPass;
   }
-
-  attributes: any;
-  public _item: ICreditProposal;
-
+  @Input() saveWordMinio: any;
   @Input()
   get item() {
     return this._item;
@@ -171,13 +193,24 @@ export class CreditProposalTabCustomerProfitabilityComponent implements OnInit {
         avarage: this.avarage,
         profit: this.profit,
         Parameter: this.parameter,
-        remarks: this.remarks,
+        // remarks: this.remarks,
         remak: this.remarks1,
       },
     ];
   }
 
+  @ViewChild('document_editor_container')
+  public container: DocumentEditorContainerComponent;
+  @ViewChild('document_editor')
+  public documentEditor: DocumentEditorComponent;
+
   ngOnInit(): void {
+    this.bucket = 'hana';
+    this.activatedRoute.params.subscribe(params => {
+      this.paramsIdGet = params['id'];
+      this.getKey = 'credit_proposal/remark/customer-prafitability/' + this.paramsIdGet + '/sfdt';
+      this.getContainer();
+    });
     this.item.attributes['tabCustomer'].totalLoanProvision = this.totalLoan();
     this.item.attributes['tabCustomer'].totalDepositInsurancePremium = this.totalLoanDeposit();
     this.item.attributes['tabCustomer'].profit = this.totalProfit();
@@ -188,8 +221,96 @@ export class CreditProposalTabCustomerProfitabilityComponent implements OnInit {
         this.remarks1[i] = this.item.attributes['tabCustomer'].GeneralTabCustomerProfitability[i].remarks1;
       }
     }
-
+    this.getContainer();
     this.width = '50%';
     this.height = '80%';
   }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.saveWordMinio) {
+      this.triggeredSave();
+    }
+  }
+  public onKeyDown(args: DocumentEditorKeyDownEventArgs): void {
+    const keyCode: string = args.event.key;
+    const isCtrlKey: boolean = args.event.ctrlKey || args.event.metaKey ? true : keyCode === '17' ? true : false;
+    console.log('keycode', keyCode);
+    console.log('isCtrlKey', isCtrlKey);
+    if (isCtrlKey && keyCode === '86') {
+      args.isHandled = true;
+      console.log('ini paste');
+    }
+  }
+
+  onCreate(): void {
+    this.container.serviceUrl = 'https://ej2services.syncfusion.com/production/web-services/api/documenteditor/';
+  }
+
+  public triggeredSave(): void {
+    let paramsId = '';
+    this.activatedRoute.params.subscribe(params => {
+      paramsId = params['id'];
+    });
+    const key = 'credit_proposal/remark/customer-prafitability';
+
+    const timeStamp = Math.floor(Date.now() / 1000);
+
+    const docEditor = this.container?.documentEditor as DocumentEditorComponent;
+
+    docEditor.saveAsBlob('Docx').then((exportedDocument: Blob) => {
+      const fileType = 'word';
+      const fileName = 'credit-proposal-remark-' + paramsId + '-customer-prafitability' + fileType + '.docs';
+      const metaData = {
+        objectName: `${key}/${paramsId}/${fileType}/${fileName}`,
+      };
+      const formData = new FormData();
+      formData.append('file', new File([exportedDocument], fileName));
+
+      this.storageService.uploadMeta('hana', formData, metaData).subscribe();
+    });
+
+    docEditor.saveAsBlob('Sfdt').then((exportedDocument: Blob) => {
+      const fileType = 'sfdt';
+      const fileName = 'credit-proposal-remark-' + paramsId + '-customer-prafitability' + fileType + '.sfdt';
+      const metaData = {
+        objectName: `${key}/${paramsId}/${fileType}/${fileName}`,
+      };
+      const formData = new FormData();
+      formData.append('file', new File([exportedDocument], fileName));
+
+      this.storageService.uploadMeta('hana', formData, metaData).subscribe();
+    });
+  }
+  private getContainer(): void {
+    let paramsId = '';
+    this.activatedRoute.params.subscribe(params => {
+      paramsId = params['id'];
+    });
+    const obj = {
+      key: 'credit_proposal/remark/customer-prafitability/' + paramsId + '/sfdt',
+    };
+    this.storageService
+      .getObjects(this.bucket, obj)
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(response => {
+        console.log('test', obj);
+        if (response.body.length > 0) {
+          this.storageService
+            .fileBlob(response.body[response.body.length - 1]['url'])
+            .pipe(takeUntil(this.ngUnsubscribe))
+            .subscribe(res => {
+              this.fileGet = new File([res.body], 'credit-proposal-remark-' + this.paramsIdGet + '-customer-prafitability-sfdt.sfdt');
+              const fileReader: FileReader = new FileReader();
+              fileReader.onload = (e: any) => {
+                const docEditor = this.container?.documentEditor as DocumentEditorComponent;
+                const contents: string = e.target.result;
+                docEditor.open(contents);
+              };
+              fileReader.readAsText(this.fileGet);
+            });
+        }
+      });
+  }
+  // public klik() {
+  //   this.triggeredSave();
+  // }
 }
