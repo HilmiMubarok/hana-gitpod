0c05d77b01a8444fcc7b0f08c9f76bb592122c9c
Rizki Rinaldi <risky.gundar@gmail.com>
2022-12-19 10:16:23 +0700

    resolve idd

--- src/main/webapp/app/entities/party-cif/customer-info/party-cif-customer-info.component.ts
+++ src/main/webapp/app/entities/party-cif/customer-info/party-cif-customer-info.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {
+  IPartyPostalAddressWarehouse,
+  PartyPostalAddressWarehouse,
+} from 'app/entities/party-postal-address/party-postal-address-warehouse.model';
 import { IPartyPostalAddress, PartyPostalAddress } from 'app/entities/party-postal-address/party-postal-address.model';
 import { PartyPostalAddressService } from 'app/entities/party-postal-address/party-postal-address.service';
 import { IPostalAddress, PostalAddress } from 'app/entities/postal-address/postal-address.model';
@@ -19,7 +23,7 @@ export class PartyCifCustomerInfoComponent implements OnChanges {
 
   public domicileLocation: IPartyPostalAddress;
   public primaryLocation: IPartyPostalAddress;
-  public warehouseLocation: IPartyPostalAddress;
+  public warehouseLocation: IPartyPostalAddressWarehouse;
   public purposeTypes: IPurposeType[];
 
   @Input()
@@ -48,10 +52,16 @@ export class PartyCifCustomerInfoComponent implements OnChanges {
         lodash.find(param, function (o) {
           return o.purposeTypeId === PURPOSE_TYPE.DOMICILE.toString();
         }) || new PartyPostalAddress();
-      this.warehouseLocation =
-        lodash.find(param, function (o) {
-          return o.purposeTypeId === PURPOSE_TYPE.WAREHOUSE;
-        }) || new PartyPostalAddress();
+      // this.warehouseLocation =
+      //   lodash.find(param, function (o) {
+      //     return o.purposeTypeId === PURPOSE_TYPE.WAREHOUSE;
+      //   }) || new PartyPostalAddress();
+      this.warehouseLocation = this.partyCif.addresses.find(obj => obj.purposeTypeId === PURPOSE_TYPE.WAREHOUSE);
+      if (this.warehouseLocation === undefined) {
+        this.warehouseLocation = new PartyPostalAddressWarehouse();
+        this.warehouseLocation.purposeTypeId = PURPOSE_TYPE.WAREHOUSE;
+        this.partyCif.addresses.push(this.warehouseLocation);
+      }
     }
   }
 }
