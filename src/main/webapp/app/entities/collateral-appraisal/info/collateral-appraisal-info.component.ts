32fee185d17c352a2b1e17bf71a4509d703c2dfa
Ridwan Zulkarnaen <rzulkarnaen01@gmail.com>
2022-12-19 13:22:58 +0700

    form rm info

--- src/main/webapp/app/entities/collateral-appraisal/info/collateral-appraisal-info.component.ts
+++ src/main/webapp/app/entities/collateral-appraisal/info/collateral-appraisal-info.component.ts
@@ -30,7 +30,7 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
   // export class CollateralAppraisalInfoComponent implements OnInit {
   public segments: IInternal[];
   public regionals: IInternal[];
-  public branchs: IInternal[];
+  public branches;
   public positionRM: IPosition[];
   public rmSegment: IInternal;
   public rmRegional: IInternal;
@@ -41,6 +41,7 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
   public _collateralAPpraisal: ICollateralAppraisal;
   public account: Account;
   public kjppValue: any;
+  public disableRmInfo:boolean;
 
   @Input()
   get collateralAppraisal() {
@@ -66,7 +67,7 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
     this._surveyAppraisal = data;
     this.initializeRole();
     this.setMatrixInput();
-    this.loadInternalInformationRM(this.surveyAppraisal.rm.partyId);
+    // this.loadInternalInformationRM(this.surveyAppraisal.rm.partyId);
   }
 
   @Output() outputTipeOfficerAppraisal = new EventEmitter();
@@ -220,6 +221,7 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
 
     this.surveyAppraisal.jpRenewal === null && this.surveyAppraisal.jpRenewal === false;
     this.loadSurveyBatchKjjp();
+    this.loadBranchNew();
   }
 
   public setRenewal(ev) {
@@ -266,9 +268,9 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
       }
     }
     if (changes['collateralAppraisal']) {
-      if (this.surveyAppraisal.rm.partyId) {
-        this.loadInternalInformationRM(this.surveyAppraisal.rm.partyId);
-      }
+      // if (this.surveyAppraisal.rm.partyId) {
+      //   this.loadInternalInformationRM(this.surveyAppraisal.rm.partyId);
+      // }
     }
 
     if (changes.statusAppraisal.currentValue.length > 0) {
@@ -286,7 +288,7 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
   }
 
   private loadInternalInformationRM(partyId: string): void {
-    this.branchs = [];
+    this.branches = [];
     this.segments = [];
     this.regionals = [];
     this.findPositionByIdParty(partyId).then((res: IPosition) => {
@@ -354,6 +356,8 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
       this.positionRM = lodash.filter(res.body, function (o) {
         return o.partyId !== null;
       });
+
+      console.log("PosRM", this.positionRM);
     });
   }
 
@@ -375,8 +379,54 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
   private loadBranch(value: string): Promise<void> {
     return new Promise<void>((resolve, reject) => {
       this.internalService.queryFilterBy({ idParent: value, size: 9999, page: 0 }).subscribe(res => {
-        this.branchs = res.body;
+        this.branches = res.body;
         resolve();
+      });
+    });
+  }
+
+  private loadBranchNew(): void {
+    const tmpBranch = [];
+    this.internalService
+      .query({
+        page: 0,
+        size: 999,
+      })
+      .subscribe(response => {
+        for (let a = 0; a < response.body.length; a++) {
+          if (response.body[a].internalTypeId === 'BRANCH') {
+            tmpBranch.push(response.body[a]);
+          }
+        }
+        this.branches = tmpBranch;
+        this.loadSegment();
+      });
+  }
+
+  public selectBranch(event: any): void {
+    const value: string = event['value'];
+    if (value) {
+      console.log("value", value);
+      console.log("branches", this.branches);
+      const branch = lodash.find(this.branches, function (o) {
+        return o.id === value;
+      });
+
+      console.log("branch", branch);
+      this.loadInternalInformationBranch(branch.parentId);
+    }
+  }
+
+  private loadInternalInformationBranch(parentId): void {
+    this.segments = [];
+    this.regionals = [];
+    this.loadInternalById(parentId).then(res4 => {
+      // this.rmRegional = res4;
+      this.loadRegional(res4.parentId.toString()).then(res5 => {
+        this.loadInternalById(res4.parentId.toString()).then(res6 => {
+          this.rmSegment = res6;
+          this.loadSegment();
+        });
       });
     });
   }
@@ -516,8 +566,11 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
 
   private checkLogin() {
     this.accountService.identity().subscribe(account => {
+      console.log("account", account);
       if (account) {
         this.account = account;
+
+        this.disableRmInfo = this.account.login === "admin" ? false : true;
       }
     });
   }
