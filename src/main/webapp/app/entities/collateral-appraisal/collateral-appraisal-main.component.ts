49efa2bfab92c3c0905fe1362291759f9811da15
ismoyo23 <p4sukanlinuc>
2022-12-20 16:40:05 +0700

    hilangkan-validasi-external-appraisal-sementara

--- src/main/webapp/app/entities/collateral-appraisal/collateral-appraisal-main.component.ts
+++ src/main/webapp/app/entities/collateral-appraisal/collateral-appraisal-main.component.ts
@@ -800,6 +800,7 @@ export class CollateralAppraisalMainComponent implements OnInit {
       officerAppraisal: true,
       kjpp: true,
     };
+
     if (this.surveyAppraisal.apprOfficer === 'Internal') {
       this.isRedirectToBucket = false;
       if (!this.surveyAppraisal.surveyorArea) {
@@ -810,20 +811,21 @@ export class CollateralAppraisalMainComponent implements OnInit {
         this._showNotification('error', 'Masukkan Officer Appraisal terlebih dahulu');
         mustValidateOnAssignment.officerAppraisal = false;
       }
-    } else {
-      if (!this.kjppIndependentAppraisalValue) {
-        this._showNotification('error', 'Masukkan KJPP / Independent Appraisal terlebih dahulu');
-        mustValidateOnAssignment.kjpp = false;
-      }
-      if (!this.teamReviewerValue) {
-        this._showNotification('error', 'Masukkan Officer Appraisal terlebih dahulu');
-        mustValidateOnAssignment.officerAppraisal = false;
-      }
-      if (!this.wilayahKotaExternalValue) {
-        this._showNotification('error', 'Masukkan Wilayah/kota terlebih dahulu');
-        mustValidateOnAssignment.wilayah = false;
-      }
     }
+    // else {
+    //   if (!this.kjppIndependentAppraisalValue) {
+    //     this._showNotification('error', 'Masukkan KJPP / Independent Appraisal terlebih dahulu');
+    //     mustValidateOnAssignment.kjpp = false;
+    //   }
+    //   if (!this.teamReviewerValue) {
+    //     this._showNotification('error', 'Masukkan Officer Appraisal terlebih dahulu');
+    //     mustValidateOnAssignment.officerAppraisal = false;
+    //   }
+    //   if (!this.wilayahKotaExternalValue) {
+    //     this._showNotification('error', 'Masukkan Wilayah/kota terlebih dahulu');
+    //     mustValidateOnAssignment.wilayah = false;
+    //   }
+    // }
 
     return this._validateProcess(mustValidateOnAssignment);
   }
