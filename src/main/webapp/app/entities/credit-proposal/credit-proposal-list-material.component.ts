af832d172b11825b38532807a4dd0ebd4080fc5e
styvenz <styvenzenwijaya@yahoo.com>
2022-12-20 19:57:53 +0700

    .

--- src/main/webapp/app/entities/credit-proposal/credit-proposal-list-material.component.ts
+++ src/main/webapp/app/entities/credit-proposal/credit-proposal-list-material.component.ts
@@ -149,7 +149,7 @@ export class CreditProposalListMaterialComponent extends AbstractEntityMaterialC
   private checkReturnStatusDescription(data: ICreditProposal[]) {
 	if (data.length > 0) {
       for (let i = 0; i < data.length; i++) {
-        data[i].statusDescription = data[i].statusDescription.substring(0,2) === 'Ol' ? data[i].statusDescription.substring(3,data[i].statusDescription.length - 3) : data[i].statusDescription;
+        data[i].statusDescription = data[i].statusDescription.substring(0,2) === 'Ol' ? data[i].statusDescription.substring(3,data[i].statusDescription.length) : data[i].statusDescription;
 	  }
 	}
 	return data;
