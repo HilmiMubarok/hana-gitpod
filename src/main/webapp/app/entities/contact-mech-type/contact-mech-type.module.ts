d1b254e2bf6749420318bd4a50a875104286b797
Ricky <ricky_riq@yahoo.com>
2022-12-21 14:51:00 +0700

    .

--- src/main/webapp/app/entities/contact-mech-type/contact-mech-type.module.ts
+++ src/main/webapp/app/entities/contact-mech-type/contact-mech-type.module.ts
@@ -2,15 +2,12 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedEntityModule } from 'app/entities/shared-entity.module';
 import { SharedModule } from 'app/shared/shared.module';
-import { ContactMechTypeComponent } from './contact-mech-type.component';
-import { ContactMechTypeDetailComponent } from './contact-mech-type-detail.component';
-import { ContactMechTypeUpdateComponent } from './contact-mech-type-update.component';
 import { contactMechTypeRoute } from './contact-mech-type.route';
 
 @NgModule({
   imports: [SharedModule, SharedEntityModule, RouterModule.forChild(contactMechTypeRoute)],
-  declarations: [ContactMechTypeComponent, ContactMechTypeDetailComponent, ContactMechTypeUpdateComponent],
-  entryComponents: [ContactMechTypeComponent, ContactMechTypeUpdateComponent],
+  declarations: [],
+  entryComponents: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class LosgwContactMechTypeModule {}
