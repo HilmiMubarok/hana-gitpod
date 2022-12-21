6a7b265fd5772c769dacc6f452825403c9bb821c
abedn2205 <abedn1075@gmail.com>
2022-12-21 12:22:19 +0700

    959-resolve-loan-final-cheker-dan-checking-review-crecas-937-906

--- src/main/webapp/app/entities/collateral-appraisal/info/collateral-appraisal-info.component.ts
+++ src/main/webapp/app/entities/collateral-appraisal/info/collateral-appraisal-info.component.ts
@@ -222,7 +222,7 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
   }
 
   private loadInternalInformationRM(partyId: string): void {
-    this.branches = [];
+    this.branchs = [];
     this.segments = [];
     this.regionals = [];
     this.findPositionByIdParty(partyId).then((res: IPosition) => {
@@ -311,7 +311,7 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
   private loadBranch(value: string): Promise<void> {
     return new Promise<void>((resolve, reject) => {
       this.internalService.queryFilterBy({ idParent: value, size: 9999, page: 0 }).subscribe(res => {
-        this.branches = res.body;
+        this.branchs = res.body;
         resolve();
       });
     });
@@ -330,7 +330,7 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
             tmpBranch.push(response.body[a]);
           }
         }
-        this.branches = tmpBranch;
+        this.branchs = tmpBranch;
         this.loadSegment();
       });
   }
