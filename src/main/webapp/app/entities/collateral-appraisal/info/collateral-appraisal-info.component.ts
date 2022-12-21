e2fe8335deab8e2601d63560b85fe480665f8c2b
Tony Robert <90840294+Tonirobert@users.noreply.github.com>
2022-12-21 09:54:40 +0700

    solve rm appraisal

--- src/main/webapp/app/entities/collateral-appraisal/info/collateral-appraisal-info.component.ts
+++ src/main/webapp/app/entities/collateral-appraisal/info/collateral-appraisal-info.component.ts
@@ -22,7 +22,7 @@ import { SurveyBatchService } from 'app/entities/survey-batch/survey-batch.servi
 import { PartnerService } from 'app/entities/partner/partner.service';
 @Component({
   selector: 'jhi-collateral-appraisal-info',
-  templateUrl: './collateral-appraisal-info.component.html',
+ templateUrl: './collateral-appraisal-info.component.html',
   styleUrls: ['./collateral-appraisal-info.css'],
 })
 export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
@@ -339,15 +339,15 @@ export class CollateralAppraisalInfoComponent implements OnChanges, OnInit {
       });
   }
 
-  // public selectBranch(event: any): void {
-  //   const value: string = event['value'];
-  //   if (value) {
-  //     const branch = lodash.find(this.branchs, function (o) {
-  //       return o.id === value;
-  //     });
-  //     this.loadInternalInformationBranch(branch.parentId);
-  //   }
-  // }
+  public selectBranch(event: any): void {
+    const value: string = event['value'];
+    if (value) {
+      const branch = lodash.find(this.branchs, function (o) {
+        return o.id === value;
+      });
+      this.loadInternalInformationBranch(branch.parentId);
+    }
+  }
 
   private loadInternalInformationBranch(parentId): void {
     this.segments = [];
