f6b3d38c177b9a449383de0fba5f7ad37e2d4622
rendayvirta96 <rendayvirta96@gmail.com>
2022-12-26 14:23:39 +0700

    .

--- src/main/webapp/app/entities/party-cif/customer-info/party-cif-customer-info-postal-address-warehouse.component.ts
+++ src/main/webapp/app/entities/party-cif/customer-info/party-cif-customer-info-postal-address-warehouse.component.ts
@@ -81,6 +81,8 @@ export class PartyCifCustomerInfoPostalAddressWarehouseComponent
   public initVillage: number;
 
   @Input() isWarehouse: Boolean = false;
+  @Input()
+  public disabled: Boolean = false;
 
   @Input()
   get partyPostalAddress() {
@@ -100,7 +102,6 @@ export class PartyCifCustomerInfoPostalAddressWarehouseComponent
     this._domicileAddress = data;
   }
 
-
   @Input()
   get partyCif() {
     return this._partyCif;
@@ -109,7 +110,6 @@ export class PartyCifCustomerInfoPostalAddressWarehouseComponent
   set partyCif(data: IPartyCif) {
     this._partyCif = data;
   }
-  
 
   constructor(
     protected activatedRoute: ActivatedRoute,
@@ -124,14 +124,14 @@ export class PartyCifCustomerInfoPostalAddressWarehouseComponent
       this.prepareView();
       this.searchwareHouse();
     }
-
   }
 
   ngOnInit(): void {
-    
+    this.cekDataSource();
+
     this.loadPurposeType();
     this.initializeCountry();
-    
+
     this.purposeType = this.partyCif.addresses[this.index].purposeTypeId;
     this.address1 = this.partyCif.addresses[this.index].address.address1;
 
@@ -352,87 +352,96 @@ export class PartyCifCustomerInfoPostalAddressWarehouseComponent
         this.purposeTypes = res.body;
       });
   }
+  public cekDataSource() {
+    if (this.disabled === true) {
+      this.myControlCountry.disable();
+      this.myControlProvince.disable();
+      this.myControlCity.disable();
+      this.myControlDistrict.disable();
+      this.myControlVillage.disable();
+    }
 
-  // private loadCountry(): void {
-  //   this.stateBoundaryService
-  //     .queryFilterBy({
-  //       idBoundaryType: GEO_BOUNDARY_TYPE['country'],
-  //       size: 9999,
-  //     })
-  //     .subscribe(res => {
-  //       this.country = res.body;
-  //     });
-  // }
+    // private loadCountry(): void {
+    //   this.stateBoundaryService
+    //     .queryFilterBy({
+    //       idBoundaryType: GEO_BOUNDARY_TYPE['country'],
+    //       size: 9999,
+    //     })
+    //     .subscribe(res => {
+    //       this.country = res.body;
+    //     });
+    // }
 
-  // loadProvince(): void {
-  //   const idCountry = this.partyPostalAddress.address.countryId;
-  //   if (idCountry) {
-  //     const predicate: object = {
-  //       idBoundaryType: GEO_BOUNDARY_TYPE['province'],
-  //       page: 0,
-  //       size: 9999,
-  //       idParent: idCountry,
-  //     };
+    // loadProvince(): void {
+    //   const idCountry = this.partyPostalAddress.address.countryId;
+    //   if (idCountry) {
+    //     const predicate: object = {
+    //       idBoundaryType: GEO_BOUNDARY_TYPE['province'],
+    //       page: 0,
+    //       size: 9999,
+    //       idParent: idCountry,
+    //     };
 
-  //     this.stateBoundaryService.queryFilterBy(predicate).subscribe(res => {
-  //       this.provinces = res.body;
-  //     });
-  //   }
-  // }
+    //     this.stateBoundaryService.queryFilterBy(predicate).subscribe(res => {
+    //       this.provinces = res.body;
+    //     });
+    //   }
+    // }
 
-  // loadCity(): void {
-  //   const idProvince = this.partyPostalAddress.address.provinceId;
-  //   if (idProvince) {
-  //     const predicate: object = {
-  //       idBoundaryType: GEO_BOUNDARY_TYPE['city'],
-  //       size: 9999,
-  //       idParent: idProvince,
-  //     };
-  //     this.stateBoundaryService.queryFilterBy(predicate).subscribe(res => {
-  //       this.cities = res.body;
-  //     });
-  //   }
-  // }
+    // loadCity(): void {
+    //   const idProvince = this.partyPostalAddress.address.provinceId;
+    //   if (idProvince) {
+    //     const predicate: object = {
+    //       idBoundaryType: GEO_BOUNDARY_TYPE['city'],
+    //       size: 9999,
+    //       idParent: idProvince,
+    //     };
+    //     this.stateBoundaryService.queryFilterBy(predicate).subscribe(res => {
+    //       this.cities = res.body;
+    //     });
+    //   }
+    // }
 
-  // loadDistrict(): void {
-  //   const idCity = this.partyPostalAddress.address.cityId;
-  //   if (idCity) {
-  //     const predicate: object = {
-  //       idBoundaryType: GEO_BOUNDARY_TYPE['district'],
-  //       size: 9999,
-  //       idParent: idCity,
-  //     };
-  //     this.stateBoundaryService.queryFilterBy(predicate).subscribe(res => {
-  //       this.districts = res.body;
-  //     });
-  //   }
-  // }
+    // loadDistrict(): void {
+    //   const idCity = this.partyPostalAddress.address.cityId;
+    //   if (idCity) {
+    //     const predicate: object = {
+    //       idBoundaryType: GEO_BOUNDARY_TYPE['district'],
+    //       size: 9999,
+    //       idParent: idCity,
+    //     };
+    //     this.stateBoundaryService.queryFilterBy(predicate).subscribe(res => {
+    //       this.districts = res.body;
+    //     });
+    //   }
+    // }
 
-  // loadVillage(): void {
-  //   const idDistrict = this.partyPostalAddress.address.districtId;
-  //   if (idDistrict) {
-  //     const predicate: object = {
-  //       idBoundaryType: GEO_BOUNDARY_TYPE['village'],
-  //       size: 9999,
-  //       idParent: idDistrict,
-  //     };
-  //     this.stateBoundaryService.queryFilterBy(predicate).subscribe(res => {
-  //       this.villages = res.body;
-  //     });
-  //   }
-  // }
+    // loadVillage(): void {
+    //   const idDistrict = this.partyPostalAddress.address.districtId;
+    //   if (idDistrict) {
+    //     const predicate: object = {
+    //       idBoundaryType: GEO_BOUNDARY_TYPE['village'],
+    //       size: 9999,
+    //       idParent: idDistrict,
+    //     };
+    //     this.stateBoundaryService.queryFilterBy(predicate).subscribe(res => {
+    //       this.villages = res.body;
+    //     });
+    //   }
+    // }
 
-  //
-  // public warehouseAddressData() {
-  //   if (this.warehouseAddress === 'warehouseAddress') {
-  //     return false;
-  //   }
-  //   return true;
-  // }
+    //
+    // public warehouseAddressData() {
+    //   if (this.warehouseAddress === 'warehouseAddress') {
+    //     return false;
+    //   }
+    //   return true;
+    // }
 
-  // print() {
-  //   console.log({
-  //     postaladdress: this.addresses,
-  //   });
-  // }
+    // print() {
+    //   console.log({
+    //     postaladdress: this.addresses,
+    //   });
+    // }
+  }
 }
