4cfedfa16fa48770ec395ccf7d72cef71567efbb
styvenz <styvenzenwijaya@yahoo.com>
2022-12-28 14:34:35 +0700

    .

--- src/main/webapp/app/entities/collateral-appraisal/addSelect/collateral-appraisal-list.component.ts
+++ src/main/webapp/app/entities/collateral-appraisal/addSelect/collateral-appraisal-list.component.ts
@@ -149,20 +149,18 @@ export class CollateralAppraisalListComponent extends AbstractEntityMaterialComp
     };
 
     this.partyCifService.findLikeCif(this.cifNumber, predicate).subscribe(res => {
-	  // this.initDataForMatTable(res, res.headers);
-	  if (res) {
-		let filteredData = [];
-		this.accountService.identity().subscribe(account => {
-		  filteredData = lodash.filter(res, function (item: IPartyCif) {
-			return item.rm.userLogin === account.login;
-		  });
+	  let filteredData = [];
+	  this.accountService.identity().subscribe(account => {
+		filteredData = lodash.filter(res, function (item: IPartyCif) {
+		  console.log('item @lodash-filter : ', item);
+		  return item.rm.userLogin === account.login;
 		});
-		if (filteredData.length > 0) {
-		  this.initDataForMatTable(res, res.headers);
-		} else {
-		  this.messageService.add({ severity: 'info', summary: 'Data Tidak Ada', detail: 'Cif ini tidak terdaftar atas RM yang login' });
-		  this.loading = false;
-		}
+	  });
+	  if (filteredData.length > 0) {
+		this.initDataForMatTable(res, res.headers);
+	  } else {
+		this.messageService.add({ severity: 'info', summary: 'Data Tidak Ada', detail: 'Cif ini tidak terdaftar atas RM yang login' });
+		this.loading = false;
 	  }
     });
   }
