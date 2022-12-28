009a6b2b6da3d20b65df93d53cf0096db6331616
Muhammad Anjar <anjarfals07@gmail.com>
2022-12-28 07:18:48 +0000

    Update survey-batch-collateral-appraisal-main.component.ts

--- src/main/webapp/app/entities/survey-batch/survey-batch-collateral-appraisal-main.component.ts
+++ src/main/webapp/app/entities/survey-batch/survey-batch-collateral-appraisal-main.component.ts
@@ -327,10 +327,6 @@ export class SurveyBatchCollateralAppraisalMainComponent implements OnInit {
     );
     return copySurveyAppraisal;
   }
-
-  public onAssignTo(ev) {
-    this.surveyAppraisal = ev;
-  }
   public onSave(): void {
     const copySurveyAppraisal: ISurveyAppraisals = this.preSave();
     if (copySurveyAppraisal.id) {
