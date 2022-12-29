7e0975376008bec4d8b4aafc648ccf70f81a1b2b
Rizki Rinaldi <risky.gundar@gmail.com>
2022-12-29 20:31:04 +0700

    path minio financial analsyt cp

--- src/main/webapp/app/entities/credit-proposal/repayment-spreadsheet/remarks/financial-statement-remarks.component.ts
+++ src/main/webapp/app/entities/credit-proposal/repayment-spreadsheet/remarks/financial-statement-remarks.component.ts
@@ -120,7 +120,7 @@ export class CreditProposalFinancialStatementRemarksComponent implements OnInit,
       const formData = new FormData();
       formData.append('file', new File([exportedDocument], fileName));
 
-      this.storageService.uploadMeta('hana', formData, metaData).subscribe();
+      this.storageService.uploadMeta(this.bucket, formData, metaData).subscribe();
     });
 
     docEditor.saveAsBlob('Sfdt').then((exportedDocument: Blob) => {
@@ -132,7 +132,7 @@ export class CreditProposalFinancialStatementRemarksComponent implements OnInit,
       const formData = new FormData();
       formData.append('file', new File([exportedDocument], fileName));
 
-      this.storageService.uploadMeta('hana', formData, metaData).subscribe();
+      this.storageService.uploadMeta(this.bucket, formData, metaData).subscribe();
     });
   }
   private getContainer(): void {
