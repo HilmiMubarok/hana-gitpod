bc05e4bdd60a9acb3dc8f8928ebc458fde10d0c2
Ricky <ricky_riq@yahoo.com>
2022-12-30 21:11:42 +0700

    .

--- src/main/webapp/app/entities/credit-proposal/proposal-basic-information.component.ts
+++ src/main/webapp/app/entities/credit-proposal/proposal-basic-information.component.ts
@@ -341,7 +341,7 @@ export class ProposalBasicInformationComponent implements OnInit {
   public routeSubMenu(menu: object): void {
     if (menu['id'] === ID_GREATER_15_BN) {
       this.creditProposal.attributes.proposalType = 'Total Exposure > IDR 15 Bn';
-      if (this.parentPath === 'credit-proposal-status/v2') {
+      if (this.parentPath === 'credit-proposal-status') {
         this.subMenu = SUBMENU_CREDITPROPOSAL_GREATER_FIFTEEN;
       } else {
         this.subMenu = [
@@ -377,7 +377,7 @@ export class ProposalBasicInformationComponent implements OnInit {
     }
     if (menu['id'] === ID_BACK_TO_BACK) {
       this.creditProposal.attributes.proposalType = 'Total Exposure Back to Back';
-      if (this.parentPath === 'credit-proposal-status/v2') {
+      if (this.parentPath === 'credit-proposal-status') {
         this.subMenu = SUBMENU_CREDITPROPOSAL_BACK_TO_BACK;
       } else {
         this.subMenu = [
@@ -396,7 +396,7 @@ export class ProposalBasicInformationComponent implements OnInit {
     this.routeHelper =
       this.router.url.split('/')[1] + '/' + this.router.url.split('/')[2] + '/' + this.router.url.split('/')[3].substr(0, 4);
 
-    this.router.navigate([this.routeHelper + '/edit'], { queryParams: { subroute: menu['id'] } });
+    this.router.navigate([this.routeHelper], { queryParams: { subroute: menu['id'] } });
   }
 
   public previousState(): void {
