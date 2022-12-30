1fdfb7a7c5bdeda0e20bd55b25c5e3483c18cb12
styvenz <styvenzenwijaya@yahoo.com>
2022-12-30 17:30:12 +0700

    .

--- src/main/webapp/app/entities/credit-proposal/exposure/total-exposure/total-exposure.component.ts
+++ src/main/webapp/app/entities/credit-proposal/exposure/total-exposure/total-exposure.component.ts
@@ -131,7 +131,7 @@ export class TotalExposureComponent extends AbstractEntityMaterialComponent<IPar
               parsed.Provision = source[y].FILN22_FEE_AMT;
               parsed.AdminFee = source[y].FILN22_FEE_AMT;
               parsed.FirstDisbursementDate = source[y].FXFIG_TRX_DT;
-			  parsed.Tenor = (Number(new Date(source[y].FILN10_TOT_EXP_IL)) - Number(new Date(source[y].FXFIG_TRX_DT))) / 3600;
+			  parsed.Tenor = (Number(new Date(source[y].FILN10_TOT_EXP_IL)) - Number(new Date(source[y].FXFIG_TRX_DT))) / 86400000;
               parsed.LoanType = this.fakeFacilityService.getFacilityType(source[y].FILN11_COM_ID);
               parsed.CCY = source[y].LNB_BASE_LON_CCY;
 
