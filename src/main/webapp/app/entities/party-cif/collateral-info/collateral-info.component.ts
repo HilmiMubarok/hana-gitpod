8bdda6446b9c5ff8a447d2af178f9953d16e5471
inan.wijaya4619 <inan.wijaya4619@gmail.com>
2023-01-04 16:15:14 +0700

    fix create data baru in collateral info idd

--- src/main/webapp/app/entities/party-cif/collateral-info/collateral-info.component.ts
+++ src/main/webapp/app/entities/party-cif/collateral-info/collateral-info.component.ts
@@ -385,6 +385,7 @@ export class PartyCifCollateralInfoComponent extends AbstractEntityMaterialCompo
           });
         } else {
           this.collateralService.create(this.collateralService.preSaveConvert(res)).subscribe(res2 => {
+            this.pushCollateral(res2.body);
             this.loadByPartyId(this.partyId);
           });
         }
@@ -395,6 +396,10 @@ export class PartyCifCollateralInfoComponent extends AbstractEntityMaterialCompo
   private updateCollateral(res: ICollateral) {
     const index = this.partyCif.collaterals.findIndex(obj => obj.id === res.id);
     this.partyCif.collaterals[index] = res;
+  }
+
+  private pushCollateral(res: ICollateral) {
+    this.partyCif.collaterals.push(res);
   }
 
   public openResult(element: ICollateral) {
