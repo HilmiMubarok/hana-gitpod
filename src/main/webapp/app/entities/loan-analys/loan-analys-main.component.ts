0d35781a07b16b14df47f0a2160ecc83ef58d66c
rendayvirta96 <rendayvirta96@gmail.com>
2023-01-12 20:46:24 +0700

    .

--- src/main/webapp/app/entities/loan-analys/loan-analys-main.component.ts
+++ src/main/webapp/app/entities/loan-analys/loan-analys-main.component.ts
@@ -365,7 +365,7 @@ export class LoanAnalysMainComponent implements OnInit {
       tempRouter === 'la-SME-CRC' ||
       tempRouter === 'la-approval' ||
       tempRouter === 'loan-committee-approval' ||
-	  tempRouter === 'cc-review'
+      tempRouter === 'cc-review'
     ) {
       let tempHelper = 0;
       let tempOpinionType = '';
@@ -473,7 +473,9 @@ export class LoanAnalysMainComponent implements OnInit {
     copyCreditProposal.attributes['bankAnalystMessage'] = JSON.stringify(copyCreditProposal.attributes['bankAnalystMessage']);
     copyCreditProposal.attributes['previous'] = JSON.stringify(copyCreditProposal.attributes['previous']);
     copyCreditProposal.attributes['offeringLetterPreparation'] = JSON.stringify(copyCreditProposal.attributes['offeringLetterPreparation']);
-    copyCreditProposal.attributes['creditProposalCollateralData'] = JSON.stringify(copyCreditProposal.attributes['creditProposalCollateralData']);
+    copyCreditProposal.attributes['creditProposalCollateralData'] = JSON.stringify(
+      copyCreditProposal.attributes['creditProposalCollateralData']
+    );
     copyCreditProposal.attributes['retriveData'] = JSON.stringify(copyCreditProposal.attributes['retriveData']);
     copyCreditProposal.attributes['remarksFinancialStatement'] = JSON.stringify(copyCreditProposal.attributes['remarksFinancialStatement']);
     copyCreditProposal.attributes['tradeCheckingRemarks'] = JSON.stringify(copyCreditProposal.attributes['tradeCheckingRemarks']);
@@ -490,7 +492,7 @@ export class LoanAnalysMainComponent implements OnInit {
   setPositionLogin(newItem: string) {
     this.positionLoginFromEmit = newItem;
   }
-  
+
   setOpinionRecomendationCompliance(newItem: string) {
     this.recomendation = newItem;
   }
@@ -597,6 +599,7 @@ export class LoanAnalysMainComponent implements OnInit {
   public onSave(): void {
     if (this.creditProposal.id) {
       this.creditProposalService.update(this.preSave()).subscribe(res => {
+        this.creditProposal.products = res.body.products;
         const tempRouter = this.router.url.split('/')[1];
         if (
           tempRouter === 'la-analyst' ||
@@ -628,6 +631,7 @@ export class LoanAnalysMainComponent implements OnInit {
       });
     } else {
       this.creditProposalService.create(this.preSave()).subscribe(res => {
+        this.creditProposal.products = res.body.products;
         const tempRouter = this.router.url.split('/')[1];
         if (
           tempRouter === 'la-analyst' ||
