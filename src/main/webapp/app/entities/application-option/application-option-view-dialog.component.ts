af24f47da236964e24efea018c1ceb8160905228
styvenz <styvenzenwijaya@yahoo.com>
2023-01-17 20:27:42 +0700

    .

--- src/main/webapp/app/entities/application-option/application-option-view-dialog.component.ts
+++ src/main/webapp/app/entities/application-option/application-option-view-dialog.component.ts
@@ -2,6 +2,9 @@ import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
 import { IApplicationOption } from './application-option.model';
+import * as _moment from 'moment';
+
+
 import lodash from 'lodash';
 
 @Component({
@@ -13,6 +16,7 @@ import lodash from 'lodash';
 })
 export class ApplicationOptionViewDialogComponent {
   public applicationOption: IApplicationOption;
+  public moment: any;
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: {
@@ -20,11 +24,12 @@ export class ApplicationOptionViewDialogComponent {
     },
     private _dialog: MatDialogRef<ApplicationOptionViewDialogComponent>
   ) {
+	this.moment = _moment;
     this.applicationOption = lodash.cloneDeep(this.data.applicationOption);
   }
 
   public updateDate(): void {
-	this.applicationOption.value = moment(this.applicationOption.value);
+	this.applicationOption.value = this.moment(this.applicationOption.value);
   }
 
   public save(): void {
