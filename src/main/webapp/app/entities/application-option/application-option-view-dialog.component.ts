e11b719b1c9e2d34fbec1ffa161e0a6a9523e547
styvenz <styvenzenwijaya@yahoo.com>
2023-01-17 18:22:28 +0700

    .

--- src/main/webapp/app/entities/application-option/application-option-view-dialog.component.ts
+++ src/main/webapp/app/entities/application-option/application-option-view-dialog.component.ts
@@ -1,11 +1,15 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
 import { IApplicationOption } from './application-option.model';
 import lodash from 'lodash';
 
 @Component({
   selector: 'jhi-application-option-view-dialog',
   templateUrl: './application-option-view-dialog.component.html',
+  providers: [
+	{provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: {useUtc: true}}
+  ] 
 })
 export class ApplicationOptionViewDialogComponent {
   public applicationOption: IApplicationOption;
--- src/main/webapp/app/entities/shared-entity.module.ts
+++ src/main/webapp/app/entities/shared-entity.module.ts
@@ -18,6 +18,7 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
@@ -355,6 +356,7 @@ import { FacilityInfoGroupComponent } from './debtor-data/facility-info/facility
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
+	MatMomentDateModule,
     MatIconModule,
     MatButtonModule,
     MatCardModule,
