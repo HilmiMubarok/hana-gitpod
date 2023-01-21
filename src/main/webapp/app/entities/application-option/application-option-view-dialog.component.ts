03b854f1c88f05981747e2e926b60342d6785630
Yusupcoody <efendiyusup520@gmail.com>
2023-01-21 15:51:40 +0700

    date as of

--- src/main/webapp/app/entities/application-option/application-option-view-dialog.component.ts
+++ src/main/webapp/app/entities/application-option/application-option-view-dialog.component.ts
@@ -4,15 +4,12 @@ import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapte
 import { IApplicationOption } from './application-option.model';
 import * as _moment from 'moment';
 
-
 import lodash from 'lodash';
 
 @Component({
   selector: 'jhi-application-option-view-dialog',
   templateUrl: './application-option-view-dialog.component.html',
-  providers: [
-	{provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: {useUtc: true}}
-  ] 
+  providers: [{ provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } }],
 })
 export class ApplicationOptionViewDialogComponent {
   public applicationOption: IApplicationOption;
@@ -24,13 +21,13 @@ export class ApplicationOptionViewDialogComponent {
     },
     private _dialog: MatDialogRef<ApplicationOptionViewDialogComponent>
   ) {
-	this.moment = _moment;
+    // this.moment = _moment;
     this.applicationOption = lodash.cloneDeep(this.data.applicationOption);
   }
 
-  public updateDate(): void {
-	this.applicationOption.value = this.moment(this.applicationOption.value);
-  }
+  // public updateDate(): void {
+  //   this.applicationOption.value = this.moment(this.applicationOption.value)
+  // }
 
   public save(): void {
     this._dialog.close(this.applicationOption);
