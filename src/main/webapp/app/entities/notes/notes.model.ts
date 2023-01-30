52c0d72aa1db4bf2f08bc15aad1ce129f956f54f
ismoyo23 <p4sukanlinuc>
2023-01-30 11:14:06 +0700

    1687-status-confirmation-dar-notif update

--- src/main/webapp/app/entities/notes/notes.model.ts
+++ src/main/webapp/app/entities/notes/notes.model.ts
@@ -1,15 +1,23 @@
 export interface INotes {
   id?: number;
   message?: string;
-  applicationId?: number,
   userId?: string;
   positionUserId?: string;
   createDate?: Date;
   recomendation?: string;
   condition?: string;
   type?: string;
-  attributes?: any;
+  applicationId?: number;
+  positionId?: number;
+  positionTypeDescription?: string;
+  employeeFirstName?: string;
+  employeeLastName?: string;
+  fromDate?: Date;
+  thruDate?: Date;
+  path?: string;
   received?: boolean;
+  uuid?: string;
+  attributes?: any
 }
 
 export class Notes implements INotes {
@@ -17,13 +25,21 @@ export class Notes implements INotes {
     public id?: number,
     public message?: string,
     public userId?: string,
-    public applicationId?: number,
     public positionUserId?: string,
     public createDate?: Date,
     public recomendation?: string,
     public condition?: string,
-	public type?: string,
-    public attributes?: any,
+    public applicationId?: number,
+    public positionId?: number,
+    public positionTypeDescription?: string,
+    public employeeFirstName?: string,
+    public employeeLastName?: string,
+    public type?: string,
+      public fromDate?: Date,
+    public thruDate?: Date,
+      public path?: string,
     public received?: boolean,
+    public uuid?: string,
+      public attributes?: any
   ) {}
 }
