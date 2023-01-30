537ebf99d16b3407fa906246795c06d709b108e1
rendayvirta96 <rendayvirta96@gmail.com>
2023-01-30 17:19:36 +0700

    .

--- src/main/webapp/app/entities/credit-proposal/loan-facility/take-over-after/credit-proposal-tab-loan-facility-take-over-after.grid.component.ts
+++ src/main/webapp/app/entities/credit-proposal/loan-facility/take-over-after/credit-proposal-tab-loan-facility-take-over-after.grid.component.ts
@@ -64,13 +64,10 @@ export class CreditProposalTabLoanFacilityTakeOverAfterGridComponent implements 
     dialogRef.afterClosed().subscribe(res => {
       if (res) {
         this.loanApplication.attributes['facilityTakeOverAfterBank'] = [
-          ...this.creditProposal?.attributes['facilityTakeOverAfterBank'],
+          ...this.creditProposal.attributes['facilityTakeOverAfterBank'],
           res,
         ];
-        this.creditProposal.attributes['facilityTakeOverAfterBank'] = [
-          ...this.creditProposal?.attributes['facilityTakeOverAfterBank'],
-          res,
-        ];
+        this.creditProposal.attributes['facilityTakeOverAfterBank'] = [...this.creditProposal.attributes['facilityTakeOverAfterBank'], res];
       }
     });
   }
