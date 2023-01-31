724d7a303b1f780b1f89f28ab0ac80c1f0f2f8dd
styvenz <styvenzenwijaya@yahoo.com>
2023-01-31 17:59:44 +0700

    .

--- src/main/webapp/app/entities/loan-analys/opinion/loan-analys-opinion.component.ts
+++ src/main/webapp/app/entities/loan-analys/opinion/loan-analys-opinion.component.ts
@@ -433,7 +433,7 @@ export class LoanAnalysOpinionComponent implements OnInit {
 		}
 	  }
 
-	  if (this.tempRouter === 'loan-committee-approval' || this.tempRouter === 'dar-final' || this.tempRouter === 'dar-checker' || this.tempRouter === 'dar-notif') {
+	  if (this.tempRouter === 'loan-committee-approval' || this.tempRouter === 'dar-final' || this.tempRouter === 'dar-checker' || this.tempRouter === 'dar-notif' || this.tempRouter === 'cc-checking' || this.tempRouter === 'cc-review' || this.tempRouter === 'cc-inquiry') {
 		if (this.notes) {
 		  if (this.notes.length > 0) {
 			this.notes = this.notes.filter(note => {
