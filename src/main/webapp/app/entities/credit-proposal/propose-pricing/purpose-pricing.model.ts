ecae0a23050e35f0f6af5c7de954620dea710b28
ismoyo23 <p4sukanlinuc>
2023-02-06 17:47:54 +0700

    CRECAS-1751-search-kjjp-bucket-list

--- src/main/webapp/app/entities/credit-proposal/propose-pricing/purpose-pricing.model.ts
+++ src/main/webapp/app/entities/credit-proposal/propose-pricing/purpose-pricing.model.ts
@@ -1,13 +1,13 @@
 export interface IPurposePricing {
   industry?: string;
   usdToIdr?: number;
-  industryCode?: number;
+  industryCode?: string;
 }
 
 export class PurposePricing implements IPurposePricing {
-  constructor(public industry?: any, public usdToIdr?: number, public industryCode?: number) {
+  constructor(public industry?: any, public usdToIdr?: number, public industryCode?: string) {
     this.industry = '';
     this.usdToIdr = 0;
-    this.industryCode = 0
+    this.industryCode = ''
   }
 }
