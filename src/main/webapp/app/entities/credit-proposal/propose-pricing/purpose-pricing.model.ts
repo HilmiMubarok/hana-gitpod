ce28909e3808b7a26b237c4550273eef00dd45fe
ismoyo23 <p4sukanlinuc>
2023-02-06 15:21:54 +0700

    1689-propose-pricing-industry

--- src/main/webapp/app/entities/credit-proposal/propose-pricing/purpose-pricing.model.ts
+++ src/main/webapp/app/entities/credit-proposal/propose-pricing/purpose-pricing.model.ts
@@ -1,11 +1,13 @@
 export interface IPurposePricing {
   industry?: string;
   usdToIdr?: number;
+  industryCode?: number;
 }
 
 export class PurposePricing implements IPurposePricing {
-  constructor(public industry?: any, public usdToIdr?: number) {
+  constructor(public industry?: any, public usdToIdr?: number, public industryCode?: number) {
     this.industry = '';
     this.usdToIdr = 0;
+    this.industryCode = 0
   }
 }
