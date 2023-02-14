abff0eb71fd81b3718b2b25920763265725a3801
styvenz <styvenzenwijaya@yahoo.com>
2023-02-14 21:19:12 +0700

    .

--- src/main/webapp/app/entities/credit-proposal/busines-activity/credit-proposal-tab-business-activity.component.ts
+++ src/main/webapp/app/entities/credit-proposal/busines-activity/credit-proposal-tab-business-activity.component.ts
@@ -172,7 +172,7 @@ export class CreditProposalTabBusinessActivityComponent implements OnInit {
 	let result = null;
     let cookies: string[] = document.cookie.split(";");
     
-    cookies.forEach(o => {
+    cookies.forEach((o) => {
       let cookie: string[] = o.split("=");
       let name: string = cookie[0].trim();
       if (name === cookieName) {
