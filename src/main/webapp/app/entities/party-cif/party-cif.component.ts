8d29ccbf80904622167baf1e7a963d6a3934c1b4
styvenz <styvenzenwijaya@yahoo.com>
2023-02-16 21:07:14 +0700

    .

--- src/main/webapp/app/entities/party-cif/party-cif.component.ts
+++ src/main/webapp/app/entities/party-cif/party-cif.component.ts
@@ -108,9 +108,8 @@ export class PartyCifComponent extends AbstractEntityMaterialComponent<IPartyCif
 
   public cifNumber: any;
 
-  public updateFromHobis(): void {
-    // this.cifNumber = this.expandedElement?.customerId;
-	this.cifNumber = this.partyCifs.customerId;
+  public updateFromHobis(data: any): void {
+	this.cifNumber = data.customerId;
     if (this.cifNumber !== undefined) {
       this.partyCifService.syncUpdateHobis(this.cifNumber).subscribe(res => {
 		if (res.status === 200) {
