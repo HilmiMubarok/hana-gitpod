4951708ba4db59c441afff34b59498e010107b0c
styvenz <styvenzenwijaya@yahoo.com>
2023-03-07 18:46:24 +0700

    .

--- src/main/webapp/app/entities/credit-proposal/credit-proposal-tab-summary.component.ts
+++ src/main/webapp/app/entities/credit-proposal/credit-proposal-tab-summary.component.ts
@@ -355,13 +355,20 @@ export class CreditProposalTabSummaryComponent implements OnInit, OnChanges {
 
   private viewBlob(title: string, data: any) {
     const win = window.open();
-    win!.document.write(
+	win!.document.write(
+      '<html><head><title>' +
+        title +
+        '</title></head><body> <object data="' +
+        data +
+        '" type="application/pdf"></object>'
+    );
+    /* win!.document.write(
       '<html><head><title>' +
         title +
         '</title></head><body> <iframe src="' +
         data +
         '" frameborder="0" title="xxxxx" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>'
-    );
+    ); */
   }
 
   blobToBase64(blob) {
