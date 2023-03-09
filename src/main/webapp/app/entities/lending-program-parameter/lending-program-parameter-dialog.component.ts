007496a7a865c035d57f2db6c6a09b0959f8790b
41360-412105\Abednego Ompusunggu <abednego_ompusunggu@yahoo.com>
2023-03-09 15:53:20 +0700

    add lending program parameter

--- src/main/webapp/app/entities/lending-program-parameter/lending-program-parameter-dialog.component.ts
+++ src/main/webapp/app/entities/lending-program-parameter/lending-program-parameter-dialog.component.ts
@@ -1,17 +1,28 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { STATUS_PARAMETER } from 'app/shared/constants/status.constants';
 import { ILendingProgramParameter } from './lending-program-parameter.model';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'jhi-lending-program-parameter-dialog',
   templateUrl: './lending-program-parameter-dialog.component.html',
 })
 export class LendingProgramParameterDialogComponent {
+  public lendingProgramParameter: ILendingProgramParameter;
+  public statuses: any;
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: {
       lendingProgramParameter: ILendingProgramParameter;
     },
     private _dialog: MatDialogRef<LendingProgramParameterDialogComponent>
-  ) {}
+  ) {
+    this.lendingProgramParameter = this.data.lendingProgramParameter;
+    this.statuses = STATUS_PARAMETER;
+  }
+
+  public save(): void {
+    this._dialog.close(this.lendingProgramParameter);
+  }
 }
--- src/main/webapp/app/entities/lending-program-parameter/lending-program-parameter.component.ts
+++ src/main/webapp/app/entities/lending-program-parameter/lending-program-parameter.component.ts
@@ -1,71 +1,30 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AccountService } from 'app/core/auth/account.service';
-import { ITEMS_PER_PAGE } from 'app/config/pagination.constants';
-import { ILendingProgramParameter } from './lending-program-parameter.model';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { ILendingProgramParameter, LendingProgramParameter } from './lending-program-parameter.model';
 import { LendingProgramParameterService } from './lending-program-parameter.service';
-import { LazyLoadEvent, ConfirmationService, MessageService } from 'primeng/api';
-import { AbstractEntityComponent } from 'app/shared/base/abstract-entity.component';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { BaseDataUtils } from 'app/shared/base/base-data-utils.service';
-import { ParseLinks } from 'app/core/util/parse-links.service';
-import { AlertService } from 'app/core/util/alert.service';
-import { EventManager } from 'app/core/util/event-manager.service';
 import { LendingProgramParameterDialogComponent } from './lending-program-parameter-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import { AbstractEntityMaterialComponent } from 'app/shared/base/abstract-entity-material.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'jhi-lending-program-parameter',
   templateUrl: './lending-program-parameter.component.html',
 })
-export class LendingProgramParameterComponent extends AbstractEntityComponent<ILendingProgramParameter> {
+export class LendingProgramParameterComponent extends AbstractEntityMaterialComponent<ILendingProgramParameter> implements OnInit {
   displayedColumns: string[] = ['no', 'lending-program', 'start-date', 'end-date', 'status', 'action'];
   constructor(
     protected lendingProgramParameterService: LendingProgramParameterService,
-    protected parseLinks: ParseLinks,
-    protected alertService: AlertService,
-    public accountService: AccountService,
-    protected activatedRoute: ActivatedRoute,
-    protected dataUtils: BaseDataUtils,
-    protected router: Router,
-    protected eventManager: EventManager,
-    protected messageService: MessageService,
-    protected modalService: NgbModal,
-    protected confirmationService: ConfirmationService,
-    protected dialog: MatDialog
+    protected dialog: MatDialog,
+    protected _snackbar: MatSnackBar
   ) {
-    super(
-      lendingProgramParameterService,
-      parseLinks,
-      accountService,
-      activatedRoute,
-      dataUtils,
-      router,
-      eventManager,
-      messageService,
-      confirmationService
-    );
-
-    this.parentRoute = '/lending-program-parameter';
-    this.listChangeEventName = 'lendingProgramParameterListModification';
-    this.entityKeyName = 'id';
-
-    this.routeData = this.activatedRoute.data.subscribe(data => {
-      this.page = data.pagingParams.page;
-      this.previousPage = data.pagingParams.page;
-      this.reverse = data.pagingParams.ascending;
-      this.predicate = data.pagingParams.predicate;
-      activatedRoute.queryParams.subscribe(params => {
-        this.itemsPerPage = params['size'] || ITEMS_PER_PAGE;
-        this.first = (this.page - 1) * this.itemsPerPage || 0;
-      });
-    });
-    this.currentSearch =
-      this.activatedRoute.snapshot && this.activatedRoute.snapshot.params['search'] ? this.activatedRoute.snapshot.params['search'] : '';
+    super(_snackbar, lendingProgramParameterService);
+    this.page = 0;
+    this.itemsPerPage = 10;
+    this.predicate = 'id';
   }
 
-  trackId(index: number, item: ILendingProgramParameter) {
-    return item.id;
+  ngOnInit(): void {
+    this.loadAll();
   }
 
   get lendingProgramParameters() {
@@ -76,9 +35,49 @@ export class LendingProgramParameterComponent extends AbstractEntityComponent<IL
     this.items = lendingProgramParameter;
   }
 
-  public openDialog(): void {
+  private loadAll(): void {
+    this.lendingProgramParameterService
+      .query({
+        page: this.page,
+        size: this.itemsPerPage,
+      })
+      .subscribe({
+        next: res => this.initDataForMatTable(res, res.headers),
+        error: res => this.onError(res.message),
+      });
+  }
+
+  public openDialog(element: ILendingProgramParameter = null): void {
+    let predicate: ILendingProgramParameter;
+    predicate = new LendingProgramParameter();
+
+    if (element) {
+      predicate = element;
+    }
+
     const dialogRef = this.dialog.open(LendingProgramParameterDialogComponent, {
       width: '60%',
+      data: {
+        lendingProgramParameter: predicate,
+      },
     });
+    dialogRef.afterClosed().subscribe((res: ILendingProgramParameter) => {
+      if (res) {
+        if (res.id) {
+          this.lendingProgramParameterService.update(res).subscribe(_res => {
+            this.loadAll();
+            console.log('xxx', res);
+          });
+        } else {
+          this.lendingProgramParameterService.create(res).subscribe(_res => {
+            this.loadAll();
+          });
+        }
+      }
+    });
+  }
+
+  protected postLoadDataLazy(): void {
+    this.loadAll();
   }
 }
