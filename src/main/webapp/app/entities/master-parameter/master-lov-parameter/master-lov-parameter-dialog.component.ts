e01b42bd2b4b2f77de53874e2747735f19c46a4f
Yusupcoody <efendiyusup520@gmail.com>
2023-03-17 18:49:35 +0700

    button save

--- src/main/webapp/app/entities/master-parameter/master-lov-parameter/master-lov-parameter-dialog.component.ts
+++ src/main/webapp/app/entities/master-parameter/master-lov-parameter/master-lov-parameter-dialog.component.ts
@@ -1,6 +1,6 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { STATUS_PARAMETER } from 'app/shared/constants/status.constants';
+import { STATUS_LOV_PARAMETER, STATUS_PARAMETER } from 'app/shared/constants/status.constants';
 import { IGeneralParameter } from '../general-parameter/general-parameter.model';
 import { GeneralParameterService } from '../general-parameter/general-parameter.service';
 import { MasterParameterLegalLendingLimitDialogComponent } from '../legal-lending-limit-parameter/legal-lending-limit-parameter-dialog.component';
@@ -9,24 +9,26 @@ import { MasterParameterLegalLendingLimitDialogComponent } from '../legal-lendin
   selector: 'jhi-master-lov-parameter-dialog',
   templateUrl: './master-lov-parameter-dialog.component.html',
 })
-export class MasterLovParameterDialogComponent {
+export class MasterLovParameterDialogComponent implements OnInit {
   public statuses: any;
-  public listGeneralLov;
+  public listGeneralLov: any;
   public generalParameter: IGeneralParameter;
+  public view: boolean;
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: {
       generalParameter: IGeneralParameter;
+      view: false;
     },
     private _dialog: MatDialogRef<MasterParameterLegalLendingLimitDialogComponent>,
     protected generalParameterService: GeneralParameterService
   ) {
     this.generalParameter = this.data.generalParameter;
-    this.statuses = STATUS_PARAMETER;
+    this.statuses = STATUS_LOV_PARAMETER;
+    this.view = this.data.view;
   }
-
-  public save(): void {
-    this._dialog.close(this.generalParameter);
+  ngOnInit(): void {
+    this.getListType();
   }
 
   public getListType() {
@@ -38,5 +40,9 @@ export class MasterLovParameterDialogComponent {
   public onSelect(element: any) {
     const paramType = element;
     this.generalParameterService.setPrameterType(paramType);
+  }
+
+  public onSave(): void {
+    this._dialog.close(this.generalParameter);
   }
 }
