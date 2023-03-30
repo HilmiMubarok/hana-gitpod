af7acb8196448c7424f82dfb41422ed8b862afaa
Rizki Rinaldi <risky.gundar@gmail.com>
2023-03-30 15:25:47 +0700

    resolve local service remarks managemnt info

--- src/main/webapp/app/entities/credit-proposal/credit-proposal-tab-management-info.component.ts
+++ src/main/webapp/app/entities/credit-proposal/credit-proposal-tab-management-info.component.ts
@@ -61,6 +61,8 @@ export class CreditProposaTabManagementInfoComponent implements OnChanges, OnIni
   public resourceUrl: string;
   public dataCoBorrower: any = [];
 
+  public customHeadersJWT: any;
+
   // address: string;
 
   get item() {
@@ -169,6 +171,9 @@ export class CreditProposaTabManagementInfoComponent implements OnChanges, OnIni
   }
 
   ngOnInit(): void {
+    const token = this.getToken('XSRF-TOKEN');
+    this.customHeadersJWT = [{ 'X-XSRF-TOKEN': token }];
+
     console.log('this', this.organizationLegal);
     this.resourceUrl = this.applicationConfigService.getEndpointFor(MICROSERVICENAME.LOS + '/api/storage');
 
@@ -200,6 +205,21 @@ export class CreditProposaTabManagementInfoComponent implements OnChanges, OnIni
     this.matrixRemoveTag();
     this.getPartyCif();
     this.getPartyCifDate();
+  }
+
+  private getToken(cookieName: string) {
+    let result = null;
+    const cookies: string[] = document.cookie.split(';');
+
+    cookies.forEach(o => {
+      const cookie: string[] = o.split('=');
+      const name: string = cookie[0].trim();
+      if (name === cookieName) {
+        result = cookie[1];
+      }
+    });
+
+    return result;
   }
 
   public getPartyCif() {
@@ -376,7 +396,8 @@ export class CreditProposaTabManagementInfoComponent implements OnChanges, OnIni
 
   onCreate(): void {
     // this.container.serviceUrl = 'http://45.32.114.128:8190/services/los/api/wordeditor/';
-    this.container.serviceUrl = 'https://ej2services.syncfusion.com/production/web-services/api/documenteditor/';
+    // this.container.serviceUrl = 'https://ej2services.syncfusion.com/production/web-services/api/documenteditor/';
+    this.container.serviceUrl = '/services/los/api/wordeditor/';
   }
 
   public onKeyDown(args: DocumentEditorKeyDownEventArgs): void {
