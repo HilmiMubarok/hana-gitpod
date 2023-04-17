c0b50abb898022319e878586b65077552595aed5
styvenz <styvenzenwijaya@yahoo.com>
2023-04-17 15:56:57 +0700

    .

--- src/main/webapp/app/layouts/sidebar/sidebar.component.ts
+++ src/main/webapp/app/layouts/sidebar/sidebar.component.ts
@@ -71,70 +71,74 @@ export class SidebarComponent implements OnInit, AfterViewInit {
 
   public dataSource: any = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
+  private positionIdLocStor: string;
+
   private setMenuFromPosInt(newPosSet: string): void {
-	this.accountService.identity().subscribe(account => {
-	  if (lodash.indexOf(account.authorities, Authority.ADMIN) >= 0) {
-		this.dataSource.data = APPRAISAL_MENU_ADMIN;
-	  } else if (lodash.indexOf(account.authorities, Authority.ADMIN) < 1) {
-		if (newPosSet === 'MASTER_ADMIN') {
-		  this.dataSource.data = MENU_MASTER;
-		} else if (newPosSet === 'SURVEYOR') {
-		  this.dataSource.data = APPRAISAL_MENU_SURVEYOR;
-		} else if (newPosSet === 'RM') {
-		  this.dataSource.data = APPRAISAL_MENU_RM;
-		} else if (newPosSet === 'BM') {
-		  this.dataSource.data = SIDEBAR_MENU_BM;
-		} else if (newPosSet === 'SME_HEAD') {
-		  this.dataSource.data = SIDEBAR_MENU_SME_HEAD;
-		} else if (newPosSet === 'SDH') {
-		  this.dataSource.data = SIDEBAR_MENU_ROLE_SME_HEAD;
-		} else if (newPosSet === 'DH') {
-		  this.dataSource.data = SIDEBAR_MENU_DH;
-		} else if (newPosSet === 'APR_DH') {
-		  this.dataSource.data = SIDEBAR_MENU_APR_DH;
-		} else if (newPosSet === 'TL') {
-		  this.dataSource.data = APPRAISAL_MENU_TL;
-		} else if (newPosSet === 'CRA') {
-		  this.dataSource.data = APPRAISAL_MENU_CRA;
-		} else if (newPosSet === 'CRC1') {
-		  this.dataSource.data = APPRAISAL_MENU_CHECKER1;
-		} else if (newPosSet === 'CRC2') {
-		  this.dataSource.data = APPRAISAL_MENU_CHECKER2;
-		} else if (newPosSet === 'HCR' || newPosSet === 'ROLE_HCR2') {
-		  this.dataSource.data = APPRAISAL_MENU_HCR;
-		} else if (newPosSet === 'BUSINESS_DIR') {
-		  this.dataSource.data = APPRAISAL_MENU_BUSINESS_DIR;
-		} else if (newPosSet === 'CREDIT_DIR') {
-		  this.dataSource.data = APPRAISAL_MENU_CREDIT_DIR;
-		} else if (newPosSet === 'CC_ANALYST') {
-		  this.dataSource.data = APPRAISAL_MENU_CC_ANALYST;
-		} else if (newPosSet === 'FINANCE_DIR') {
-		  this.dataSource.data = APPRAISAL_MENU_FINANCE_DIR;
-		} else if (newPosSet === 'CC_ADMIN') {
-		  this.dataSource.data = APPRAISAL_MENU_CC_ADMIN;
-		} else if (newPosSet === 'CC_DEPT_HEAD') {
-		  this.dataSource.data = APPRAISAL_MENU_CC_DEPT_HEAD;
-		} else if (newPosSet === 'CC_DH') {
-		  this.dataSource.data = APPRAISAL_MENU_CC_DH;
-		} else if (newPosSet === 'CC_DIR') {
-		  this.dataSource.data = APPRAISAL_MENU_CC_DIR;
-		} else if (newPosSet === 'LEGAL_HEAD') {
-		  this.dataSource.data = APPRAISAL_MENU_LEGAL_HEAD;
-		} else if (newPosSet === 'LEGAL_OFFICER') {
-		  this.dataSource.data = APPRAISAL_MENU_LEGAL_OFFICER;
-		} else if (newPosSet === 'CRO') {
-		  this.dataSource.data = APPRAISAL_MENU_CRO;
-		} else if (newPosSet === 'DEPT_HEAD') {
-		  this.dataSource.data = APPRAISAL_DEPT_HEAD;
-		} else if (newPosSet === 'APR_DEPT_HEAD') {
-		  this.dataSource.data = APPRAISAL_APR_DEPT_HEAD;
-		} else if (newPosSet === 'CREDIT_LEGAL_LEAD') {
-		  this.dataSource.data = APPRAISAL_DEPT_CREDIT_LEGAL_LEAD;
-		} else if (newPosSet === 'ADMIN_APPRAISER') {
-		  this.dataSource.data = APPRAISAL_MENU_ADMIN_APPRAISAL;
+	if (this.positionIdLocStor) {
+	  this.accountService.identity().subscribe(account => {
+		if (lodash.indexOf(account.authorities, Authority.ADMIN) >= 0) {
+		  this.dataSource.data = APPRAISAL_MENU_ADMIN;
+		} else if (lodash.indexOf(account.authorities, Authority.ADMIN) < 1) {
+		  if (newPosSet === 'MASTER_ADMIN') {
+			this.dataSource.data = MENU_MASTER;
+		  } else if (newPosSet === 'SURVEYOR') {
+			this.dataSource.data = APPRAISAL_MENU_SURVEYOR;
+		  } else if (newPosSet === 'RM') {
+			this.dataSource.data = APPRAISAL_MENU_RM;
+		  } else if (newPosSet === 'BM') {
+			this.dataSource.data = SIDEBAR_MENU_BM;
+		  } else if (newPosSet === 'SME_HEAD') {
+			this.dataSource.data = SIDEBAR_MENU_SME_HEAD;
+		  } else if (newPosSet === 'SDH') {
+			this.dataSource.data = SIDEBAR_MENU_ROLE_SME_HEAD;
+		  } else if (newPosSet === 'DH') {
+			this.dataSource.data = SIDEBAR_MENU_DH;
+		  } else if (newPosSet === 'APR_DH') {
+			this.dataSource.data = SIDEBAR_MENU_APR_DH;
+		  } else if (newPosSet === 'TL') {
+			this.dataSource.data = APPRAISAL_MENU_TL;
+		  } else if (newPosSet === 'CRA') {
+			this.dataSource.data = APPRAISAL_MENU_CRA;
+		  } else if (newPosSet === 'CRC1') {
+			this.dataSource.data = APPRAISAL_MENU_CHECKER1;
+		  } else if (newPosSet === 'CRC2') {
+			this.dataSource.data = APPRAISAL_MENU_CHECKER2;
+		  } else if (newPosSet === 'HCR' || newPosSet === 'ROLE_HCR2') {
+			this.dataSource.data = APPRAISAL_MENU_HCR;
+		  } else if (newPosSet === 'BUSINESS_DIR') {
+			this.dataSource.data = APPRAISAL_MENU_BUSINESS_DIR;
+		  } else if (newPosSet === 'CREDIT_DIR') {
+			this.dataSource.data = APPRAISAL_MENU_CREDIT_DIR;
+		  } else if (newPosSet === 'CC_ANALYST') {
+			this.dataSource.data = APPRAISAL_MENU_CC_ANALYST;
+		  } else if (newPosSet === 'FINANCE_DIR') {
+			this.dataSource.data = APPRAISAL_MENU_FINANCE_DIR;
+		  } else if (newPosSet === 'CC_ADMIN') {
+			this.dataSource.data = APPRAISAL_MENU_CC_ADMIN;
+		  } else if (newPosSet === 'CC_DEPT_HEAD') {
+			this.dataSource.data = APPRAISAL_MENU_CC_DEPT_HEAD;
+		  } else if (newPosSet === 'CC_DH') {
+			this.dataSource.data = APPRAISAL_MENU_CC_DH;
+		  } else if (newPosSet === 'CC_DIR') {
+			this.dataSource.data = APPRAISAL_MENU_CC_DIR;
+		  } else if (newPosSet === 'LEGAL_HEAD') {
+			this.dataSource.data = APPRAISAL_MENU_LEGAL_HEAD;
+		  } else if (newPosSet === 'LEGAL_OFFICER') {
+			this.dataSource.data = APPRAISAL_MENU_LEGAL_OFFICER;
+		  } else if (newPosSet === 'CRO') {
+			this.dataSource.data = APPRAISAL_MENU_CRO;
+		  } else if (newPosSet === 'DEPT_HEAD') {
+			this.dataSource.data = APPRAISAL_DEPT_HEAD;
+		  } else if (newPosSet === 'APR_DEPT_HEAD') {
+			this.dataSource.data = APPRAISAL_APR_DEPT_HEAD;
+		  } else if (newPosSet === 'CREDIT_LEGAL_LEAD') {
+			this.dataSource.data = APPRAISAL_DEPT_CREDIT_LEGAL_LEAD;
+		  } else if (newPosSet === 'ADMIN_APPRAISER') {
+			this.dataSource.data = APPRAISAL_MENU_ADMIN_APPRAISAL;
+		  }
 		}
-	  }
-	});
+	  });
+	}
 	this.router.navigate(['']);
   }
 
@@ -167,6 +171,7 @@ export class SidebarComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit(): void {
+	this.positionIdLocStor = this.getPositionByLocStor('POS');
     this.checkLogin();
 	this.templateService.triggerChanggedPosIntObservable.subscribe((newPos: string) => {
       this.setMenuFromPosInt(newPos);
