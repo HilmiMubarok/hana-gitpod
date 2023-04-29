d65764d4adcf8e1e6f369b94d0eb665141d7c259
styvenz <styvenzenwijaya@yahoo.com>
2023-04-29 22:58:01 +0700

    .

--- src/main/webapp/app/entities/credit-proposal/collateral-info/group-collateral/group-collateral-list-cp.component.ts
+++ src/main/webapp/app/entities/credit-proposal/collateral-info/group-collateral/group-collateral-list-cp.component.ts
@@ -150,7 +150,7 @@ export class GroupCollateralListCpComponent implements OnInit, OnChanges {
 
   private cleanUpColGroupRel(): void {
 	if (this.creditProposal.collateralProductRelations.length > 0 && this.creditProposal.products.length > 0 && this.listGroupCollateralItems.length > 0) {
-	  for (let i = 0; i < this.creditProposal.collateralProductRelations.length; i++) {
+	  for (let i = this.creditProposal.collateralProductRelations.length - 1; i = 0; i--) {
 		for (let j = 0; j < this.creditProposal.products.length; j++) {
 		  for (let k = 0; k < this.listGroupCollateralItems.length; k++) {
 			if (this.creditProposal.collateralProductRelations[i].applicationProduct.id === this.creditProposal.products[j].id && this.creditProposal.collateralProductRelations[i].collateralId === this.listGroupCollateralItems[k].id) {
