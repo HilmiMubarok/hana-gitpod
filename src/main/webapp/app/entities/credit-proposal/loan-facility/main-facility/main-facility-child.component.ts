3030e10ce68508c2a746ae599567c7703d300175
inan.wijaya4619 <inan.wijaya4619@gmail.com>
2023-05-09 17:39:39 +0700

    integrasi main facility cp

--- src/main/webapp/app/entities/credit-proposal/loan-facility/main-facility/main-facility-child.component.ts
+++ src/main/webapp/app/entities/credit-proposal/loan-facility/main-facility/main-facility-child.component.ts
@@ -1,70 +1,30 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ICategoryList, IMainFacility } from 'app/entities/main-facility/main-facility.model';
 
 @Component({
   selector: 'jhi-main-facility-child',
   templateUrl: './main-facility-child.component.html',
   styleUrls: ['./main-facility-child.component.scss'],
 })
-export class MainFacilityChildComponent implements OnInit, OnChanges {
+export class MainFacilityChildComponent implements OnChanges {
   @Input()
-  get id() {
-    return this._id;
+  get mainData() {
+    return this._mainData;
   }
 
-  set id(items: any) {
-    this._id = items;
+  set mainData(items: IMainFacility) {
+    this._mainData = items;
   }
 
-  private _id: any;
-
-  dataSource: PeriodicElement[];
-  dataSource2: PeriodicElement[];
+  public dataSource: ICategoryList[];
+  private _mainData: IMainFacility;
   displayColumns = ['facilityCategory', 'mainPlafond', 'outstanding', 'changes', 'totalPlafond'];
 
   constructor() {}
 
-  ngOnInit() {
-    console.log('child ', this.id);
-  }
-
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['id']) {
-      console.log('changes ', this.id);
-      const data: PeriodicElement[] = ELEMENT_DATA.filter(obj => obj.position === this.id);
-      console.log('data element find ', data);
-      this.dataSource = data;
+    if (changes['mainData']) {
+      this.dataSource = this.mainData.categoryListDTO;
     }
   }
 }
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-  description: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    position: 1,
-    name: 'anjar',
-    weight: 1.0079,
-    symbol: 'H',
-    description: `Hydrogen`,
-  },
-  {
-    position: 2,
-    name: 'rendy',
-    weight: 1.0079,
-    symbol: 'H',
-    description: `Hydrogen`,
-  },
-  {
-    position: 3,
-    name: 'obet',
-    weight: 1.0079,
-    symbol: 'H',
-    description: `Hydrogen`,
-  },
-];
--- src/main/webapp/app/entities/main-facility/main-facility.model.ts
+++ src/main/webapp/app/entities/main-facility/main-facility.model.ts
@@ -1,23 +1,33 @@
 export interface IMainFacility {
   id?: number;
+  applicationId?: number;
   partyId?: string;
   approvalNumber?: string;
   currency?: string;
+  changes?: number;
   mainPlafond?: number;
+  outstanding?: number;
   availableLimit?: number;
   maturityDate?: Date;
+  newMaturityDate?: Date;
+  totalPlafond?: number;
   categoryListDTO?: ICategoryList[];
 }
 
 export class MainFacility implements IMainFacility {
   constructor(
     public id?: number,
+    public applicationId?: number,
     public partyId?: string,
     public approvalNumber?: string,
     public currency?: string,
+    public changes?: number,
     public mainPlafond?: number,
+    public outstanding?: number,
     public availableLimit?: number,
     public maturityDate?: Date,
+    public newMaturityDate?: Date,
+    public totalPlafond?: number,
     public categoryListDTO?: ICategoryList[]
   ) {
     this.categoryListDTO = [new CategoryList()];
@@ -26,8 +36,11 @@ export class MainFacility implements IMainFacility {
 
 export interface ICategoryList {
   categoryId?: string;
+  changes?: number;
   plafond?: number;
+  mainPlafond?: number;
   outstanding?: number;
+  totalPlafond?: number;
 }
 
 export class CategoryList implements ICategoryList {
