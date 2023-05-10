bcc8eb6c3bf136a56acd9fadc094d1bc1165b747
Rizki Rinaldi <risky.gundar@gmail.com>
2023-05-10 10:44:01 +0700

    resolve appraisal summary

--- src/main/webapp/app/entities/collateral-appraisal/summary/collateral-appraisal-summary.component.ts
+++ src/main/webapp/app/entities/collateral-appraisal/summary/collateral-appraisal-summary.component.ts
@@ -36,6 +36,8 @@ export class CollateralAppraisalSummaryComponent implements OnInit, OnChanges {
   private _item: ICreditProposal;
   public formatType?: string;
 
+  public customHeadersJWT: any;
+
   private BUCKET: string;
   totalKeteranganObjectJaminan: any;
   private ngUnsubscribe = new Subject();
@@ -65,7 +67,7 @@ export class CollateralAppraisalSummaryComponent implements OnInit, OnChanges {
 
   onCreate(): void {
     // this.container.serviceUrl = 'http://45.32.114.128:8190/services/los/api/wordeditor/';
-    this.container.serviceUrl = 'https://ej2services.syncfusion.com/production/web-services/api/documenteditor/';
+    this.container.serviceUrl = '/services/los/api/wordeditor/';
   }
 
   private getContainer(): void {
@@ -161,6 +163,8 @@ export class CollateralAppraisalSummaryComponent implements OnInit, OnChanges {
   }
 
   ngOnInit(): void {
+    const token = this.getToken('XSRF-TOKEN');
+    this.customHeadersJWT = [{ 'X-XSRF-TOKEN': token }];
     // this.resourceUrl = this.applicationConfigService.getEndpointFor(MICROSERVICENAME.LOS + '/api/storage');
     this.getWord();
 
@@ -172,6 +176,21 @@ export class CollateralAppraisalSummaryComponent implements OnInit, OnChanges {
     //     this.getContainer();
     //   });
     // });
+  }
+
+  private getToken(cookieName: string) {
+    let result = null;
+    const cookies: string[] = document.cookie.split(';');
+
+    cookies.forEach(o => {
+      const cookie: string[] = o.split('=');
+      const name: string = cookie[0].trim();
+      if (name === cookieName) {
+        result = cookie[1];
+      }
+    });
+
+    return result;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
