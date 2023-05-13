3de95786e85fb4dc73fcd30688a357b93e5c03b6
styvenz <styvenzenwijaya@yahoo.com>
2023-05-13 17:37:54 +0700

    .

--- src/main/webapp/app/shared/auth/matrixIDD.directive.ts
+++ src/main/webapp/app/shared/auth/matrixIDD.directive.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 import { AccountService } from 'app/core/auth/account.service';
+import { PositionService } from 'app/entities/position/position.service';
 
 import lodash from 'lodash';
 
@@ -13,38 +14,39 @@ export class MatrixIDDDirective implements OnInit, OnDestroy {
   private authorities!: string[];
   private elementType!: string;
   private readonly destroy$ = new Subject<void>();
+  private positionTypeId: string;
 
   @Input()
   set jhiMatrixDirIDD(value: string) {
     this.elementType = value;
   }
 
-  constructor(private accountService: AccountService, private templateRef: TemplateRef<any>, private viewContainerRef: ViewContainerRef) {}
-
-  ngOnInit() {
-    this.viewContainerRef.clear();
-    this.accountService
-      .getAuthenticationState()
-      .pipe(takeUntil(this.destroy$))
-
-      .subscribe(res => {
-        this.authorities = res.authorities;
-        this.checkAccess();
-      });
-  }
+  constructor(private accountService: AccountService, private templateRef: TemplateRef<any>, private viewContainerRef: ViewContainerRef, private positionService: PositionService) {}
 
   private matrixInput(): void {
-    if (lodash.indexOf(this.authorities, 'ROLE_RM') >= 0) {
+	if (this.positionTypeId) {
+	  if (this.positionTypeId === 'RM') {
+		this.viewContainerRef.createEmbeddedView(this.templateRef);
+	  }
+	}
+    /* if (lodash.indexOf(this.authorities, 'ROLE_RM') >= 0) {
       this.viewContainerRef.createEmbeddedView(this.templateRef);
-    }
+    } */
   }
 
   private matrixLabel(): void {
-    if (lodash.indexOf(this.authorities, 'ROLE_RM') >= 0) {
+	if (this.positionTypeId) {
+	  if (this.positionTypeId === 'RM') {
+		// do nothing
+	  } else {
+		this.viewContainerRef.createEmbeddedView(this.templateRef);
+	  }
+	}
+    /* if (lodash.indexOf(this.authorities, 'ROLE_RM') >= 0) {
       // do nothing
     } else {
       this.viewContainerRef.createEmbeddedView(this.templateRef);
-    }
+    } */
   }
 
   private checkAccess(): void {
@@ -53,6 +55,42 @@ export class MatrixIDDDirective implements OnInit, OnDestroy {
     } else {
       this.matrixLabel();
     }
+  }
+  
+  private getLocStor(cookieName: string) {
+    let result = null;
+    const cookies: string[] = document.cookie.split(';');
+
+    cookies.forEach(o => {
+      const cookie: string[] = o.split('=');
+      const name: string = cookie[0].trim();
+      if (name === cookieName) {
+        result = cookie[1];
+      }
+    });
+
+    return result;
+  }
+  
+  private getPositionTypeId(): void {
+	this.positionService.find(this.getLocStor('POS')).subscribe(res => {
+	  this.positionTypeId = res.body.positionTypeId;
+	  this.checkAccess();
+	});
+  }
+
+  ngOnInit() {
+    this.viewContainerRef.clear();
+	this.getPositionTypeId();
+
+    /* this.accountService
+      .getAuthenticationState()
+      .pipe(takeUntil(this.destroy$))
+
+      .subscribe(res => {
+        this.authorities = res.authorities;
+        this.checkAccess();
+      }); */
   }
 
   ngOnDestroy(): void {
