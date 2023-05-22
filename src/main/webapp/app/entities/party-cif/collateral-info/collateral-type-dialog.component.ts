0745bf8e8dfffcb2c2bf063ba3a3310046484a24
anjarfals07 <anjarfals07@gmail.com>
2023-05-22 10:53:54 +0700

    add status active lov collateral grading - imprv 2

--- src/main/webapp/app/entities/party-cif/collateral-info/collateral-type-dialog.component.ts
+++ src/main/webapp/app/entities/party-cif/collateral-info/collateral-type-dialog.component.ts
@@ -150,7 +150,9 @@ export class CollateralTypeDialogComponent implements OnInit, OnChanges {
         size: 9999,
       })
       .subscribe(res => {
-        this.collateralGrading = res.body;
+        this.collateralGrading = lodash.filter(res.body, function (o) {
+          return o.statusId === 'ACTIVE';
+        });
       });
   }
 
