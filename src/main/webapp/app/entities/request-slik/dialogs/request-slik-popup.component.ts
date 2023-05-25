74fa345813e8ca3a22ad0407d62943eb3974d87a
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2023-05-25 19:33:12 +0700

    add popup screen for submit note

---
+++ src/main/webapp/app/entities/request-slik/dialogs/request-slik-popup.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-request-slik-popup',
+  templateUrl: './request-slik-popup.component.html',
+})
+export class RequestSlikPopupComponent {
+  submit() {}
+}
