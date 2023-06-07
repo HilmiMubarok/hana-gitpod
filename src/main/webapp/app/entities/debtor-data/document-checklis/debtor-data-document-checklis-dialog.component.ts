9759bb837cb2c51f15c3b9ced7e59726e54616c9
ismoyo23 <ismoyorplb@gmail.com>
2023-06-07 09:51:20 +0700

    1973-solve-remarks-null-ketika-gak-isi

--- src/main/webapp/app/entities/debtor-data/document-checklis/debtor-data-document-checklis-dialog.component.ts
+++ src/main/webapp/app/entities/debtor-data/document-checklis/debtor-data-document-checklis-dialog.component.ts
@@ -96,7 +96,7 @@ export class DebtorDataDocumentChecklistDialogComponent {
     this.getMinIOData();
     this.filesStatus = this.files.status;
     this.filesdueDate = this.files.dueDate;
-    this.filesRemarks = this.files.remarks === null ? '' : this.files.remarks;
+    this.files.remarks === null || this.files.remarks === 'null' ? '' : this.files.remarks;
 
     this.filesDescription = this.files.description;
   }
