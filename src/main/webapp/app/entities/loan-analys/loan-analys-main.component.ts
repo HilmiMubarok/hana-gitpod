c5bd4705ed08e2c2adc58e216d5cbf456b1d7582
styvenz <styvenzenwijaya@yahoo.com>
2023-06-09 19:53:31 +0700

    .

--- src/main/webapp/app/entities/loan-analys/loan-analys-main.component.ts
+++ src/main/webapp/app/entities/loan-analys/loan-analys-main.component.ts
@@ -484,6 +484,21 @@ export class LoanAnalysMainComponent implements OnInit {
     }
   }
 
+  private getLocStor(cookieName: string) {
+    let result = null;
+    const cookies: string[] = document.cookie.split(';');
+
+    cookies.forEach(o => {
+      const cookie: string[] = o.split('=');
+      const name: string = cookie[0].trim();
+      if (name === cookieName) {
+        result = cookie[1];
+      }
+    });
+
+    return result;
+  }
+
   private getTasks(): void {
     this.creditProposalProcessService.getTasks(this.id).subscribe(res => {
       this.tasks = res.body;
@@ -508,6 +523,7 @@ export class LoanAnalysMainComponent implements OnInit {
     dialogRef.afterClosed().subscribe(_res => {
       if (_res) {
         this.resAttr = _res;
+		this.resAttr.attr.idPosition = this.getLocStor('POS');
 
         if (
           this.creditProposal.statusId === 'CP_LOAN_COMMITTEE' &&
