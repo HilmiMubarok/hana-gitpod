49bd26847a530ffcd1e15ae73489dbb2ac49f216
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2023-06-19 09:59:15 +0700

    show verify

--- src/main/webapp/app/entities/request-slik/request-slik-detail.component.ts
+++ src/main/webapp/app/entities/request-slik/request-slik-detail.component.ts
@@ -194,8 +194,8 @@ export class RequestSlikDetailComponent implements OnInit {
   segment = 'loading...';
 
   roles = {
-    request: ['RM', 'CRO'],
-    approval: ['SME_HEAD', 'DEPT_HEAD', 'HCR1', 'HCR2', 'BUSINESS_SUPPORT'],
+    request: ['RM', 'CRO', 'BUSINESS_SUPPORT'],
+    approval: ['SME_HEAD', 'DEPT_HEAD', 'HCR1', 'HCR2'],
   };
 
   // submitTitle = this.getSubmitTitle()
