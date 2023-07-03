ddaad7bb8a07a91f1f162993c2f5cad2bf4bb2f2
styvenz <styvenzenwijaya@yahoo.com>
2023-07-03 16:15:49 +0700

    .

--- src/main/webapp/app/entities/offering-letter/offering-letter-main.component.ts
+++ src/main/webapp/app/entities/offering-letter/offering-letter-main.component.ts
@@ -246,6 +246,7 @@ export class OfferingLetterMainComponent implements OnInit {
       this.tasks = res.body;
     });
   }
+
   public lovProposalType() {
     this.generalParameterService
       .queryFilterBy({
@@ -257,6 +258,22 @@ export class OfferingLetterMainComponent implements OnInit {
         this.proposType = res.body;
       });
   }
+
+  private getLocStor(cookieName: string) {
+    let result = null;
+    const cookies: string[] = document.cookie.split(';');
+
+    cookies.forEach(o => {
+      const cookie: string[] = o.split('=');
+      const name: string = cookie[0].trim();
+      if (name === cookieName) {
+        result = cookie[1];
+      }
+    });
+
+    return result;
+  }
+
   public processTask(task: IProcessTask): void {
     const dialogRef = this.dialog.open(TaskCommentDialogComponent, {
       width: '80vw',
@@ -265,6 +282,7 @@ export class OfferingLetterMainComponent implements OnInit {
     dialogRef.afterClosed().subscribe(_res => {
       if (_res) {
         this.resAttr = _res;
+        this.resAttr.attr.idPosition = this.getLocStor('POS');
 
         this.onSave('process');
       }
