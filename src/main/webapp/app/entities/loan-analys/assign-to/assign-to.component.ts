64de449602e92d5d18a9dc14bb706f3337f17ede
anjarfals07 <anjarfals07@gmail.com>
2023-07-17 19:30:18 +0700

    change endpoint assign to loan analys

--- src/main/webapp/app/entities/loan-analys/assign-to/assign-to.component.ts
+++ src/main/webapp/app/entities/loan-analys/assign-to/assign-to.component.ts
@@ -45,6 +45,7 @@ export class AssignToComponent implements OnInit {
 
     if (this.router.url.split('/')[1] === 'la-distribution') {
       this.loadPosition(['CRO']);
+      console.log('load', this.loadPosition(['CRO']));
     } else if (this.router.url.split('/')[1] === 'cc-distribution') {
       this.loadPosition(['CC_ANALYST']);
     } else if (this.router.url.split('/')[1] === 'distribution') {
@@ -52,8 +53,9 @@ export class AssignToComponent implements OnInit {
     }
   }
 
-  public loadPosition(position: any): void {
-    this.positionService.queryFilterByNew({ idPositionTypes: position, size: 9999, page: 0 }).subscribe(res => {
+  public loadPosition(positionTypeId: any): void {
+    // Change endpoint position
+    this.positionService.getPositionAssignTo(positionTypeId, this.creditProposal.internalId).subscribe(res => {
       let tempDataAssignTo = {};
       this.position = lodash.filter(res.body, function (o) {
         return o.partyId !== null;
