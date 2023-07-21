4dc9cb79753aaef840ba1c6d08262a95fd679e47
ismoyo23 <ismoyorplb@gmail.com>
2023-07-21 11:03:47 +0700

    332-ubah-posisi-checklist-all-delegation-css

--- src/main/webapp/app/entities/employee/dialog-delegation/dialog-delegation-application.component.ts
+++ src/main/webapp/app/entities/employee/dialog-delegation/dialog-delegation-application.component.ts
@@ -128,25 +128,21 @@ export class DialogDelegationApplicationComponent implements OnInit {
   }
 
   public save(): void {
-    if (this.selectedData.length < 1) {
-      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'List delegation required' });
-    } else {
-      const result = this.employeeData.filter(data => data.id === this.employeeId);
-      this.DelegationApplicationReq.fromEmployee = this.fromEmployee;
-      this.DelegationApplicationReq.toEmployee = result[0];
-      this.DelegationApplicationReq.loanApplications = this.selectedData;
-      this.cashCreditProposalService.addDelegation(this.DelegationApplicationReq).subscribe(
-        () => {
-          this._dialog.close();
-        },
-        error => {
-          this.messageService.add({ severity: 'error', summary: 'Error', detail: error.error.detail });
-          // Fungsi ini akan dijalankan ketika terjadi respons error
+    const result = this.employeeData.filter(data => data.id === this.employeeId);
+    this.DelegationApplicationReq.fromEmployee = this.fromEmployee;
+    this.DelegationApplicationReq.toEmployee = result[0];
+    this.DelegationApplicationReq.loanApplications = this.selectedData;
+    this.cashCreditProposalService.addDelegation(this.DelegationApplicationReq).subscribe(
+      () => {
+        this._dialog.close();
+      },
+      error => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: error.error.detail });
+        // Fungsi ini akan dijalankan ketika terjadi respons error
 
-          // Lakukan penanganan error sesuai kebutuhan, misalnya menampilkan pesan kesalahan ke pengguna
-        }
-      );
-    }
+        // Lakukan penanganan error sesuai kebutuhan, misalnya menampilkan pesan kesalahan ke pengguna
+      }
+    );
   }
 
   public cancel(): void {
