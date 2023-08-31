ce91a53c81a16c77e76dc11fceab00f5ee60d48c
tony robert <tonirobert89@gmail.com>
2023-08-31 11:33:14 +0700

    create menu access position id

--- src/main/webapp/app/entities/menu-access/menu-access.model.ts
+++ src/main/webapp/app/entities/menu-access/menu-access.model.ts
@@ -1,25 +1,80 @@
+// export interface IMenuAccess {
+//   id?: number;
+//   requestNumber?: string;
+//   cif?: string;
+//   debtorName?: string;
+//   customerType?: string;
+//   segment?: string;
+//   requestDate?: string;
+//   purposeCode?: string;
+//   status?: string;
+// }
+
+import { IPosition } from '@syncfusion/ej2-angular-grids';
+
+// export class RequestSlik implements IMenuAccess {
+//   constructor(
+//     public id?: number,
+//     public requestNumber?: string,
+//     public cif?: string,
+//     public debtorName?: string,
+//     public customerType?: string,
+//     public segment?: string,
+//     public requestDate?: string,
+//     public purposeCode?: string,
+//     public status?: string
+//   ) {}
+// }
+
 export interface IMenuAccess {
-  id?: number;
-  requestNumber?: string;
-  cif?: string;
-  debtorName?: string;
-  customerType?: string;
-  segment?: string;
-  requestDate?: string;
-  purposeCode?: string;
-  status?: string;
+  [obj: string]: any;
+  code?: string;
+  description?: string;
+  icon?: string;
+  id?: string;
+  parentId?: string;
+  parentDescription?: string;
+  positions?: IPositionAccess;
 }
 
-export class RequestSlik implements IMenuAccess {
+export class MenuAccess implements IMenuAccess {
+  constructor(
+    public code?: string,
+    public description?: string,
+    public icon?: string,
+    public id?: string,
+    public parentId?: string,
+    public parentDescription?: string,
+    public positions?: IPositionAccess
+  ) {}
+}
+
+export interface IPositionAccess {
+  id?: number;
+  menuItemDescription?: string;
+  menuItemIcon?: string;
+  menuItemId?: string;
+  menuItemcode?: string;
+  parentMenuItemCode?: string;
+  parentMenuItemDescription?: string;
+  parentMenuItemIcon?: string;
+  parentMenuItemId?: string;
+  positionDescription?: string;
+  positionId?: string;
+}
+
+export class PositionAccess implements IPositionAccess {
   constructor(
     public id?: number,
-    public requestNumber?: string,
-    public cif?: string,
-    public debtorName?: string,
-    public customerType?: string,
-    public segment?: string,
-    public requestDate?: string,
-    public purposeCode?: string,
-    public status?: string
+    public menuItemDescription?: string,
+    public menuItemIcon?: string,
+    public menuItemId?: string,
+    public menuItemcode?: string,
+    public parentMenuItemCode?: string,
+    public parentMenuItemDescription?: string,
+    public parentMenuItemIcon?: string,
+    public parentMenuItemId?: string,
+    public positionDescription?: string,
+    public positionId?: string
   ) {}
 }
