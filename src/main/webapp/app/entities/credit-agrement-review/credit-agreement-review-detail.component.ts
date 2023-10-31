d0680116d031e2408593488b77826c566681d323
styvenz <styvenzenwijaya@yahoo.com>
2023-10-31 14:25:01 +0700

    .

--- src/main/webapp/app/entities/credit-agrement-review/credit-agreement-review-detail.component.ts
+++ src/main/webapp/app/entities/credit-agrement-review/credit-agreement-review-detail.component.ts
@@ -503,7 +503,8 @@ export class CreditAgreementReviewDetailComponent implements OnInit {
   }
 
   private getTasks(): void {
-    this.creditAgreementProcessService.getTasks(this.id).subscribe(res => {
+    // this.creditAgreementProcessService.getTasks(this.id).subscribe(res => {
+	this.creditAgreementProcessService.getTasksByPos(this.id, {idPosition: this.getLocStor('POS'), idMenu: this.parentPath }).subscribe(res => {
       this.tasks = res.body;
     });
   }
