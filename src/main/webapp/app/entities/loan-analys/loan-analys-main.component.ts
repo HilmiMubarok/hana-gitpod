6672a69ae0d0bbb0cec4ee6c24943679c1bdbb86
styvenz <styvenzenwijaya@yahoo.com>
2023-11-01 18:32:27 +0700

    .

--- src/main/webapp/app/entities/loan-analys/loan-analys-main.component.ts
+++ src/main/webapp/app/entities/loan-analys/loan-analys-main.component.ts
@@ -1369,7 +1369,7 @@ export class LoanAnalysMainComponent implements OnInit {
 
           if (copyCreditProposal.notes.length > 0) {
             for (let i = 0; i < copyCreditProposal.notes.length; i++) {
-              if (copyCreditProposal.notes[i].positionId === this.positionLoginFromEmit) {
+              if (Number(copyCreditProposal.notes[i].positionId) === this.positionLoginFromEmit) {
 				console.log('in again');
                 if (this.creditProposal.statusId === 'CP_LOAN_COMMITTEE' || this.parentPath === 'loan-committee-approval') {
                   copyCreditProposal.notes[i].applicationId = this.id;
