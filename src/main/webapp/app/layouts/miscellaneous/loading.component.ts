1d639b3b7dcc0b70c00c8e9bb6f1f8e40b4be4e3
Fabian <sujonofabianzap@gmail.com>
2023-11-18 08:00:06 +0700

    dev-diagram-for-table-dialog

---
+++ src/main/webapp/app/layouts/miscellaneous/loading.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'jhi-loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.style.scss'],
+})
+export class LoadingComponent {}
