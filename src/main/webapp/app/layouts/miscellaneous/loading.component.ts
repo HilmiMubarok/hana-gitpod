a72095804e6d00b758f6b29a58146f05c9c8c84c
Fabian <sujonofabianzap@gmail.com>
2023-11-18 08:05:30 +0700

    uncommit

--- src/main/webapp/app/layouts/miscellaneous/loading.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'jhi-loading',
-  templateUrl: './loading.component.html',
-  styleUrls: ['./loading.style.scss'],
-})
-export class LoadingComponent {}
