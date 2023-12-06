423f38de315e6448d97c93989eb68e040269c084
Styvenzen <henystyvenzen@gmail.com>
2023-12-06 11:12:03 +0000

    Revert "resolve loan analyst submit(opinionhistorycr5prod)"
    This reverts commit 3ac9afc676c8fcbfc943795a6d207afb10e69146

--- src/main/webapp/app/entities/loan-analys/loan-analys-main.component.ts
+++ src/main/webapp/app/entities/loan-analys/loan-analys-main.component.ts
@@ -2047,23 +2047,7 @@ export class LoanAnalysMainComponent implements OnInit {
               this.saveUpdate('not-complete', source);
             }
           } else {
-            // this.saveUpdate('not-complete-not-visit', source);
-            this.messageService.add({
-              severity: 'info',
-              summary: 'Warning',
-              detail: 'Please input opinion first before submit or save the data',
-            });
-            this.messageService.add({
-              severity: 'info',
-              summary: 'Warning',
-              detail: 'Please input recomendation first before submit or save the data',
-            });
-            this.messageService.add({
-              severity: 'info',
-              summary: 'Warning',
-              detail: 'Please input condition first before submit or save the data',
-            });
-            this.saveUpdate('not-complete', source);
+            this.saveUpdate('not-complete-not-visit', source);
           }
         }
       } else {
