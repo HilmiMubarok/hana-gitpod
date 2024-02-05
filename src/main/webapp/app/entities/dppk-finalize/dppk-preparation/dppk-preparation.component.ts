cce2e283dc5be79502223c18f975d67d3cbe5f7e
inan.wijaya4619 <inan.wijaya4619@gmail.com>
2024-02-05 15:48:36 +0700

    add item dppk preparation

---
+++ src/main/webapp/app/entities/dppk-finalize/dppk-preparation/dppk-preparation.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ICreditProposal } from 'app/entities/credit-proposal/credit-proposal.model';
+
+@Component({
+  selector: 'jhi-dppk-preparation',
+  templateUrl: './dppk-preparation.component.html',
+  styleUrls: ['./dppk-preparation.component.scss'],
+})
+export class DppkPreparationComponent implements OnInit {
+  public _creditProposal: ICreditProposal;
+
+  @Input()
+  get creditProposal() {
+    return this._creditProposal;
+  }
+
+  set creditProposal(item: ICreditProposal) {
+    this._creditProposal = item;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('test');
+  }
+}
