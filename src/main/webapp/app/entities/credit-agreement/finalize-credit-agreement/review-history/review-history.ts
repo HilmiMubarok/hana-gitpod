fb0c485851972cc8114e30057608c567a5171ddb
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2024-02-19 13:49:51 +0700

    add path

--- src/main/webapp/app/entities/credit-agreement/finalize-credit-agreement/review-history/review-history.ts
+++ src/main/webapp/app/entities/credit-agreement/finalize-credit-agreement/review-history/review-history.ts
@@ -2,12 +2,14 @@ export interface IReviewHistory {
   approverName: string;
   position: string;
   date: string;
+  path?: string;
 }
 
 export class ReviewHistory implements IReviewHistory {
-  constructor(public approverName: string, public position: string, public date: string) {
+  constructor(public approverName: string, public position: string, public date: string, public path?: string) {
     this.approverName = '';
     this.position = '';
     this.date = '';
+    this.path = '';
   }
 }
