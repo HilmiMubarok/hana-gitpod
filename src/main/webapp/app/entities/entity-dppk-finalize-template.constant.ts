4f6642d8170482ab05eefe3e825e76e087765a63
inan.wijaya4619 <inan.wijaya4619@gmail.com>
2024-03-01 10:30:22 +0700

    add-loan-facility-after-in-loan-ops

--- src/main/webapp/app/entities/entity-dppk-finalize-template.constant.ts
+++ src/main/webapp/app/entities/entity-dppk-finalize-template.constant.ts
@@ -1,9 +1,18 @@
 import { BankAccountDialogLoanOperationComponent } from './loan-operation/dppk-finalize-loan-operation/bank-account-loan-operation/bank-account-dialog-loan-operation.component';
 import { BankAccountLoanOperationComponent } from './loan-operation/dppk-finalize-loan-operation/bank-account-loan-operation/bank-account-loan-operation.component';
 import { DppkFinalizeLoanOperationComponent } from './loan-operation/dppk-finalize-loan-operation/dppk-finalize-loan-operation.component';
+import { GenerateDraftDppkLoanOperationComponent } from './loan-operation/dppk-finalize-loan-operation/generate-draft-dppk-loan-operation/generate-draft-dppk-loan-operation.component';
 import { InternalMemoDppkLoanOperationDialogDetailComponent } from './loan-operation/dppk-finalize-loan-operation/internal-memo-dppk-loan-operation/internal-memo-dppk-loan-operation-dialog-detail.component';
 import { InternalMemoDppkLoanOperationDialogComponent } from './loan-operation/dppk-finalize-loan-operation/internal-memo-dppk-loan-operation/internal-memo-dppk-loan-operation-dialog.component';
 import { InternalMemoDppkLoanOperationComponent } from './loan-operation/dppk-finalize-loan-operation/internal-memo-dppk-loan-operation/internal-memo-dppk-loan-operation.component';
+import { LoanOperationTakeOverCollateralAfterDialogComponent } from './loan-operation/loan-facility-detail/loan-operation-take-over-collateral/loan-operation-take-over-collateral-after-dialog.component';
+import { LoanOperationTakeOverCollateralAfterGridComponent } from './loan-operation/loan-facility-detail/loan-operation-take-over-collateral/loan-operation-take-over-collateral-after-grid.component';
+import { LoanOperationTakeOverCollateralBeforeDialogComponent } from './loan-operation/loan-facility-detail/loan-operation-take-over-collateral/loan-operation-take-over-collateral-before-dialog.component';
+import { LoanOperationTakeOverCollateralBeforeGridComponent } from './loan-operation/loan-facility-detail/loan-operation-take-over-collateral/loan-operation-take-over-collateral-before-grid.component';
+import { LoanOperationTakeOverFacilityAfterDialogComponent } from './loan-operation/loan-facility-detail/loan-operation-take-over-facility/loan-operation-take-over-facility-after-dialog.component';
+import { LoanOperationTakeOverFacilityAfterGridComponent } from './loan-operation/loan-facility-detail/loan-operation-take-over-facility/loan-operation-take-over-facility-after-grid.component';
+import { LoanOperationTakeOverFacilityBeforeDialogComponent } from './loan-operation/loan-facility-detail/loan-operation-take-over-facility/loan-operation-take-over-facility-before-dialog.component';
+import { LoanOperationTakeOverFacilityGridBeforeGridComponent } from './loan-operation/loan-facility-detail/loan-operation-take-over-facility/loan-operation-take-over-facility-before-grid.component';
 
 export const entityDppkFinalizeTemplate: any[] = [
   DppkFinalizeLoanOperationComponent,
@@ -12,4 +21,13 @@ export const entityDppkFinalizeTemplate: any[] = [
   InternalMemoDppkLoanOperationComponent,
   InternalMemoDppkLoanOperationDialogComponent,
   InternalMemoDppkLoanOperationDialogDetailComponent,
+  GenerateDraftDppkLoanOperationComponent,
+  LoanOperationTakeOverCollateralAfterDialogComponent,
+  LoanOperationTakeOverCollateralAfterGridComponent,
+  LoanOperationTakeOverCollateralBeforeDialogComponent,
+  LoanOperationTakeOverCollateralBeforeGridComponent,
+  LoanOperationTakeOverFacilityAfterDialogComponent,
+  LoanOperationTakeOverFacilityAfterGridComponent,
+  LoanOperationTakeOverFacilityBeforeDialogComponent,
+  LoanOperationTakeOverFacilityGridBeforeGridComponent,
 ];
---
+++ src/main/webapp/app/entities/loan-operation/loan-facility-detail/loan-operation-take-over-collateral/loan-operation-take-over-collateral-before-dialog.component.ts
@@ -0,0 +1,64 @@
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { ICreditProposal } from 'app/entities/credit-proposal/credit-proposal.model';
+import { ICollateralPrevious } from 'app/entities/credit-proposal/loan-facility/take-over/collateral/collateral-previous.model';
+import { ConfirmDialogComponent } from 'app/layouts/miscellaneous/confirm-dialog.component';
+
+@Component({
+  selector: 'jhi-loan-operation-take-over-collateral-before-dialog',
+  templateUrl: './loan-operation-take-over-collateral-before-dialog.component.html',
+  styleUrls: ['../../../credit-proposal/loan-facility/grid/loan.scss'],
+})
+export class LoanOperationTakeOverCollateralBeforeDialogComponent {
+  @Input() isViewMode: Boolean = false;
+  public creditProposal: ICreditProposal;
+  view: boolean;
+  parentPath: any;
+  collateralPrevious: ICollateralPrevious;
+  constructor(
+    private dialog: MatDialog,
+    public router: Router,
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      object: ICreditProposal;
+      collateralPrevious: ICollateralPrevious;
+      view: boolean;
+    },
+    private _dialog: MatDialogRef<LoanOperationTakeOverCollateralBeforeDialogComponent>
+  ) {
+    if (data['view'] === false) {
+      _dialog.disableClose = true;
+      _dialog.backdropClick().subscribe(_ => {
+        this.openCancelDialog();
+      });
+    }
+    this.creditProposal = this.data.object;
+    this.view = this.data.view;
+    this.collateralPrevious = this.data.collateralPrevious;
+    this.parentPath = this.router.url.split('/')[1];
+  }
+  public save(): void {
+    this._dialog.close(this.collateralPrevious);
+  }
+  numberInputChanged(value) {
+    const num = value.replace(/[IDR,]/g, '');
+    return Number(num);
+  }
+  // cancel confrimation dialog
+  public openCancelDialog(): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '25vw',
+      data: {
+        title: '',
+        message: 'Are you sure to cancel this data?',
+      },
+      panelClass: 'custom-dialog-container-cancel',
+    });
+    dialogRef.afterClosed().subscribe(res => {
+      if (res) {
+        this._dialog.close();
+      }
+    });
+  }
+}
