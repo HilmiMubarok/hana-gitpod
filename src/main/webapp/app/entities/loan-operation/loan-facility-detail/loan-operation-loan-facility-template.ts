2c3f1b397bb3dc6006c6206da6781dd7c4750748
inan.wijaya4619 <inan.wijaya4619@gmail.com>
2024-03-01 14:02:18 +0700

    add-mapping-to-collateral-loan-facility-loan-ops

--- src/main/webapp/app/entities/loan-operation/loan-facility-detail/loan-operation-loan-facility-template.ts
+++ src/main/webapp/app/entities/loan-operation/loan-facility-detail/loan-operation-loan-facility-template.ts
@@ -1,6 +1,15 @@
 import { LoanOperationLoanFacilityDetailDialogComponent } from './dialog/loan-operation-loan-facility-dialog.component';
 import { LoanOperationLoanFacilityDetailGridComponent } from './grid/loan-operation-loan-facility-detail-grid.component';
 import { LoanOperationLoanFacilityDetailComponent } from './loan-operation-loan-facility-detail';
+import { LoanOperationMappingToCollateralComponent } from './loan-operation-mapping-to-collateral/loan-operation-mapping-to-collateral.component';
+import { LoanOperationTakeOverCollateralAfterDialogComponent } from './loan-operation-take-over-collateral/loan-operation-take-over-collateral-after-dialog.component';
+import { LoanOperationTakeOverCollateralAfterGridComponent } from './loan-operation-take-over-collateral/loan-operation-take-over-collateral-after-grid.component';
+import { LoanOperationTakeOverCollateralBeforeDialogComponent } from './loan-operation-take-over-collateral/loan-operation-take-over-collateral-before-dialog.component';
+import { LoanOperationTakeOverCollateralBeforeGridComponent } from './loan-operation-take-over-collateral/loan-operation-take-over-collateral-before-grid.component';
+import { LoanOperationTakeOverFacilityAfterDialogComponent } from './loan-operation-take-over-facility/loan-operation-take-over-facility-after-dialog.component';
+import { LoanOperationTakeOverFacilityAfterGridComponent } from './loan-operation-take-over-facility/loan-operation-take-over-facility-after-grid.component';
+import { LoanOperationTakeOverFacilityBeforeDialogComponent } from './loan-operation-take-over-facility/loan-operation-take-over-facility-before-dialog.component';
+import { LoanOperationTakeOverFacilityGridBeforeGridComponent } from './loan-operation-take-over-facility/loan-operation-take-over-facility-before-grid.component';
 import { LoanOperationMainFacilityChildComponent } from './main-facility/loan-operation-main-facility-child.component';
 import { LoanOperationMainFacilityDialogComponent } from './main-facility/loan-operation-main-facility-dialog.component';
 import { LoanOperationMainFacilityComponent } from './main-facility/loan-operation-main-facility.component';
@@ -12,4 +21,13 @@ export const LoanOperationLoanFacilityTemplate = [
   LoanOperationMainFacilityComponent,
   LoanOperationMainFacilityDialogComponent,
   LoanOperationMainFacilityChildComponent,
+  LoanOperationTakeOverCollateralAfterDialogComponent,
+  LoanOperationTakeOverCollateralAfterGridComponent,
+  LoanOperationTakeOverCollateralBeforeDialogComponent,
+  LoanOperationTakeOverCollateralBeforeGridComponent,
+  LoanOperationTakeOverFacilityAfterDialogComponent,
+  LoanOperationTakeOverFacilityAfterGridComponent,
+  LoanOperationTakeOverFacilityBeforeDialogComponent,
+  LoanOperationTakeOverFacilityGridBeforeGridComponent,
+  LoanOperationMappingToCollateralComponent,
 ];
