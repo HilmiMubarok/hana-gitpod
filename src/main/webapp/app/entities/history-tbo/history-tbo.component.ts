45f492c1867654adccc5b349b7a90dcf073da6db
Rizki Rinaldi <risky.gundar@gmail.com>
2024-03-14 16:29:50 +0700

    tbo-monitoring (new screen)

---
+++ src/main/webapp/app/entities/history-tbo/history-tbo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-history-tbo',
+  templateUrl: './history-tbo.component.html',
+  styleUrls: ['./history-tbo.style.scss'],
+})
+export class HistoryTBOComponent {}
---
+++ src/main/webapp/app/entities/tbo-legal-monitoring/tbo-legal-monitoring.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-tbo-legal-monitoring',
+  templateUrl: './tbo-legal-monitoring.component.html',
+  styleUrls: ['./tbo-legal-monitoring.style.scss'],
+})
+export class TboLegalMonitoringComponent {
+  public openDialog(): void {}
+}
