81b2b621a04c4d40de615853f9b155cfdcc63812
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2024-04-19 16:44:52 +0700

    Add Select All Checkbox Management, Shareholder, and Others

--- src/main/webapp/app/entities/request-slik/others/request-slik-other-grid.component.ts
+++ src/main/webapp/app/entities/request-slik/others/request-slik-other-grid.component.ts
@@ -21,11 +21,24 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 import { RequestSlikDialogSlikFileComponent } from '../dialogs/request-slik-dialog-slik-file.component';
 import { RequestSlikChecklistService } from '../services/request-slik-checklist.service';
 import { RequestSlikStatus } from '../enums/request-slik-status.enum';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'jhi-request-slik-other-grid',
   templateUrl: './request-slik-other-grid.component.html',
   styleUrls: ['./request-slik-other-grid.style.scss', '../../party-cif/party-cif.style.scss'],
+  styles: [
+    `
+      .button-styling {
+        outline: none;
+        border: none;
+        box-shadow: 0 0 5px #319e97;
+        width: 100px;
+        border-radius: 5px;
+        background-color: #315f9e !important;
+      }
+    `,
+  ],
   animations: [
     trigger('detailExpand', [
       state(
@@ -64,6 +77,8 @@ export class RequestSlikOtherGridComponent extends AbstractEntityMaterialCompone
   dataSourceExpand;
   requestSlikId: number;
   nikNpwp;
+  public label = 'Select All';
+  public selection = new SelectionModel<any>(true, []);
 
   @Input()
   get organizationManagement() {
@@ -110,6 +125,16 @@ export class RequestSlikOtherGridComponent extends AbstractEntityMaterialCompone
     this.entityKeyName = 'id';
     this.organizationManagementRes = [];
     this.requestSlikId = Number(this.router.url.split('/')[2]);
+
+    this.selection.changed.subscribe(() => {
+      const numSelected = this.selection.selected.length;
+      const numRows = this.items.data.length;
+      if (numSelected === numRows) {
+        this.label = 'Deselect All';
+      } else {
+        this.label = 'Select All';
+      }
+    });
   }
 
   openDialogSlikFile(reqReffId, fileName) {
@@ -291,13 +316,26 @@ export class RequestSlikOtherGridComponent extends AbstractEntityMaterialCompone
   }
 
   isDetailChecked(row) {
+    const copyData = row;
+
     if (row.person) {
       row = row.person.id;
     }
     if (row.shareHolderOrg) {
       row = row.shareHolderOrg.id;
     }
-    return this.requestSlikService.isDetailChecked(row, this.checklists, 'shareholder');
+
+    const check = this.requestSlikService.isDetailChecked(row, this.checklists, 'shareholder');
+
+    if (check) {
+      this.checklists.forEach(checklist => {
+        if (Number(checklist.idParty) === Number(row)) {
+          this.selection.select(copyData);
+        }
+      });
+    }
+
+    return check;
   }
 
   updateChecklist(ev, check) {
@@ -398,5 +436,88 @@ export class RequestSlikOtherGridComponent extends AbstractEntityMaterialCompone
     return _.some(this.verifyChecklists, _.pick(element, ['id', 'nikNpwp', 'partyId']));
 
     // return _.some(this.verifyChecklists, { id: element.id });
+  }
+
+  // ### CHECKBOX ALL
+
+  doCheckAll() {
+    this.label = 'Deselect All';
+    this.items.data.forEach((data, i) => {
+      this.doCheck(data);
+    });
+  }
+
+  doUncheckAll() {
+    this.label = 'Select All';
+    this.selection.selected.forEach(row => this.doUncheck(row));
+  }
+
+  // ### CHECKBOX SINGLE ROW
+
+  toggleCheckRow(row: any, check: any) {
+    if (check.checked) {
+      this.doCheck(row);
+    } else {
+      this.doUncheck(row);
+    }
+  }
+
+  doCheck(row: any) {
+    this.selection.select(row);
+
+    const data = {
+      idParty: null,
+      idRequestSlik: null,
+      cust: null,
+    };
+
+    // Add additional data for ocrData
+    data.cust = row.person === null ? row.shareHolderOrg : row.person;
+
+    data.idParty = row.person ? row.person.id : row.shareHolderOrg.id;
+    data.idRequestSlik = this.requestSlikId;
+
+    this.requestSlikChecklistService.updateChecklistOcrs(data);
+
+    this.checklistData.emit({
+      data,
+      mode: 'add',
+    });
+  }
+
+  doUncheck(row: any) {
+    this.selection.deselect(row);
+
+    if (this.selection.selected.length === 0) {
+      this.label = 'Select All';
+    }
+
+    const data = {
+      idParty: null,
+      idRequestSlik: null,
+      cust: null,
+    };
+
+    // Add additional data for ocrData
+    data.cust = row.person === null ? row.shareHolderOrg : row.person;
+
+    data.idParty = row.person ? row.person.id : row.shareHolderOrg.id;
+    data.idRequestSlik = this.requestSlikId;
+
+    // get checklist data by requestSlikId
+    this.requestSlikService.getChecklistData(true, this.requestSlikId).subscribe(checklistData => {
+      // get data where partyId === data.idParty
+      const resChecklistData = checklistData.body.data.filter(res => res.idParty === data.idParty);
+
+      resChecklistData.forEach(checklist => {
+        // remove checklist
+        this.requestSlikService.removeChecklist(checklist.id).subscribe();
+      });
+    });
+
+    this.checklistData.emit({
+      data,
+      mode: 'remove',
+    });
   }
 }
