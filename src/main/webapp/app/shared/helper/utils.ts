470952f8e0aac6dec27a39ef258f68a94f77c013
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2024-07-03 10:32:00 +0700

    add function format date in utils

--- src/main/webapp/app/shared/helper/utils.ts
+++ src/main/webapp/app/shared/helper/utils.ts
@@ -56,3 +56,17 @@ export function parsePreviousAtrribute(cp: ICreditProposal): IParsePreviousAtrri
 
   return parsedAttribute;
 }
+
+export function formatDateDob(dateString) {
+  const date = new Date(dateString);
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  if (month === undefined || isNaN(day) || isNaN(year)) {
+    return '-';
+  }
+
+  return `${month}, ${day}, ${year}`;
+}
