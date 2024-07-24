f412f73501d38fb36dd32b3ac2a6190b9e869ee6
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2024-07-24 13:48:34 +0700

    revert config

--- webpack/proxy.conf.js
+++ webpack/proxy.conf.js
@@ -15,10 +15,7 @@ function setupProxy({ tls }) {
         '/httpbin',
         '/strapi',
       ],
-      // target: `http${tls ? 's' : ''}://localhost:8190`,
-      target: `http${tls ? 's' : ''}://34.101.37.193:8190`,
-      // target: `http${tls ? 's' : ''}://45.32.114.128:8190`,
-      // target: `http${tls ? 's' : ''}://192.168.88.171:8080`,
+      target: `http${tls ? 's' : ''}://localhost:8190`,
       secure: false,
       changeOrigin: tls,
       headers: {
