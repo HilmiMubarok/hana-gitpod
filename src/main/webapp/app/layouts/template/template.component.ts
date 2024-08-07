e93341c738e793ab339252b5dfbab3dba0dd53c7
styvenz <styvenzenwijaya@yahoo.com>
2024-08-07 19:03:18 +0700

    .

--- src/main/webapp/app/layouts/template/template.component.ts
+++ src/main/webapp/app/layouts/template/template.component.ts
@@ -18,14 +18,44 @@ export class TemplateComponent implements OnInit, AfterViewInit {
 
   constructor(private accountService: AccountService, private templateService: TemplateService) {}
 
+  public isLogIn = false;
+  private cook = '';
+
+  private getLocStor(cookieName: string) {
+    let result = null;
+    const cookies: string[] = document.cookie.split(';');
+
+    cookies.forEach(o => {
+      const cookie: string[] = o.split('=');
+      const name: string = cookie[0].trim();
+      if (name === cookieName) {
+        result = cookie[1];
+      }
+    });
+
+    return result;
+  }
+
   ngOnInit(): void {
-    this.accountService.identity().subscribe(account => {
+    this.cook = this.getLocStor('POS');
+    this.isLogIn = this.cook === '' ? false : true;
+
+    if (this.isLogIn) {
+      this.accountService.identity().subscribe(account => {
+        if (account) {
+          this.templateService.sidebarStateObservable$.subscribe((newState: string) => {
+            this.sidebarState = newState;
+          });
+        }
+      });
+    }
+    /* this.accountService.identity().subscribe(account => {
       if (account) {
         this.templateService.sidebarStateObservable$.subscribe((newState: string) => {
           this.sidebarState = newState;
         });
       }
-    });
+    }); */
   }
 
   ngAfterViewInit(): void {
