7b667932545226a547485e04aefa576b0d686503
Fabian Sujono <sujonofabianzap@gmail.com>
2024-08-14 16:40:48 +0700

    352-remove-unused-dependencies-from-shared-lib

--- src/main/webapp/app/entities/sample-form/sample-form-content-one.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'jhi-sample-form-content-one',
-  templateUrl: './sample-form-content-one.component.html',
-})
-export class SampleFormContentOneComponent {}
