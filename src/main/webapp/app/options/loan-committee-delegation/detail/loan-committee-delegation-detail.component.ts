821a3d95b5e72247c5928fba87bb7433322d2b7b
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2024-09-05 11:40:09 +0700

    feat: add menu loan committe delegation

---
+++ src/main/webapp/app/options/loan-committee-delegation/detail/loan-committee-delegation-detail.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { LoanCommitteeDelegationService } from '../loan-committee-delegation.service';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+import { ICreditProposal } from 'app/entities/credit-proposal/credit-proposal.model';
+
+@Component({
+  selector: 'jhi-loan-committee-delegation-detail',
+  templateUrl: './loan-committee-delegation-detail.component.html',
+  styleUrls: ['../../correction-application/correction-application.scss'],
+})
+export class LoanCommitteeDelegationDetailComponent implements OnInit {
+  private isLoading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public isLoading$ = this.isLoading.asObservable();
+  public idApplication: number;
+  public applicationNumber: string;
+  public customerNumber: string;
+  public customerName: string;
+  public status: string;
+  public people: MatTableDataSource<any>;
+  public displayedColumns: string[] = ['no', 'internal', 'name', 'position'];
+  public creditProposal: ICreditProposal;
+
+  constructor(private service: LoanCommitteeDelegationService, private route: ActivatedRoute) {
+    // get id in route
+    this.route.params.subscribe(params => {
+      if (params.id) {
+        this.isLoading.next(true);
+
+        this.service.getLoanCommitteeDelegationById(params.id).subscribe(res => {
+          this.creditProposal = res.body;
+
+          this.idApplication = res.body.id;
+          this.applicationNumber = res.body.applicationNumber;
+          this.customerNumber = res.body.customerNumber;
+          this.customerName = res.body.prospectPerson ? res.body.prospectPerson.name : res.body.prospectOrganization.groupName;
+          this.status = res.body.statusDescription;
+
+          this.loadPeople();
+        });
+      }
+    });
+
+    this.people = new MatTableDataSource<any>();
+  }
+
+  loadPeople(): void {
+    this.service.getLoanCommitteeDelegationDetail(this.idApplication).subscribe(res => {
+      this.people.data = res.body;
+      this.isLoading.next(false);
+    });
+  }
+
+  ngOnInit(): void {
+    // this.loadPeople();
+    console.log('INIT');
+  }
+
+  save(): void {
+    console.log('SAVE', { cp: this.creditProposal, people: this.people.data });
+  }
+}
---
+++ src/main/webapp/app/options/loan-committee-delegation/loan-committee-delegation.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ApplicationConfigService } from 'app/core/config/application-config.service';
+import { createRequestOption } from 'app/core/request/request-util';
+import { CreditProposalService } from 'app/entities/credit-proposal/credit-proposal.service';
+import { MICROSERVICENAME } from 'app/shared/constants/config.constants';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoanCommitteeDelegationService {
+  constructor(private http: HttpClient, private applicationConfigService: ApplicationConfigService, private cp: CreditProposalService) {}
+
+  public getLoanCommitteeDelegation(params?: any): Observable<any> {
+    const options = createRequestOption(params);
+    const url = this.applicationConfigService.getEndpointFor(MICROSERVICENAME.LOS + '/api/cash-credit-proposals/correction-note-data');
+    return this.http.get(url, { params: options, observe: 'response' });
+  }
+
+  public getLoanCommitteeDelegationById(id: number): Observable<any> {
+    return this.cp.find(id);
+  }
+
+  public getLoanCommitteeDelegationDetail(id: number): Observable<any> {
+    const url = this.applicationConfigService.getEndpointFor(
+      MICROSERVICENAME.LOS + `/api/cash-position-reporting-structure/note-data/${id}/find-by-application`
+    );
+    return this.http.get(url, { observe: 'response' });
+  }
+}
