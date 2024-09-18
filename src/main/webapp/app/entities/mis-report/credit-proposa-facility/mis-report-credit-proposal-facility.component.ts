9161901e28d804331462289edf510cebe4e63016
inan.wijaya4619 <inan.wijaya4619@gmail.com>
2024-09-18 16:27:07 +0700

    add-function-generate-facility-mis-report

--- src/main/webapp/app/entities/mis-report/credit-proposa-facility/mis-report-credit-proposal-facility.component.ts
+++ src/main/webapp/app/entities/mis-report/credit-proposa-facility/mis-report-credit-proposal-facility.component.ts
@@ -67,4 +67,53 @@ export class MisReportCreditProposalFacilityComponent {
   public previousState(): void {
     window.history.back();
   }
+
+  convertStatusToString(status: Array<string>): string {
+    // if length is 0, return empty string
+    if (status.length === 0) {
+      return '';
+    }
+
+    return status.join(',');
+  }
+
+  generateMISReportCP() {
+    const template_report_data = [
+      { key: 'No.', valueFrom: '', format: 'string' },
+      { key: 'Proposal Number', valueFrom: 'proposalNumber', format: 'string' },
+      { key: 'Proposal Date', valueFrom: 'proposalDate', format: 'date' },
+      { key: 'Segment', valueFrom: 'segment', format: 'string' },
+      { key: 'Customer Status', valueFrom: 'customerStatus', format: 'string' },
+      { key: 'CIF', valueFrom: 'cif', format: 'string' },
+      { key: 'Debtor Name', valueFrom: 'debtorName', format: 'string' },
+      { key: 'Proposal Type', valueFrom: 'proposalType', format: 'string' },
+      { key: 'Product ID', valueFrom: 'productId', format: 'string' },
+      { key: 'Pengajuan', valueFrom: 'pengajuan', format: 'string' },
+      { key: 'Facility', valueFrom: 'facility', format: 'string' },
+      { key: 'Tenor Fasilitas', valueFrom: 'tenorFasilitas', format: 'string' },
+      { key: 'Current Rate (%)', valueFrom: 'currentRate', format: 'string' },
+      { key: 'Rate Proposed', valueFrom: 'rateProposed', format: 'string' },
+      { key: 'Provision Fee', valueFrom: 'provisionFee', format: 'string' },
+      { key: 'Admin Fee', valueFrom: 'adminFee', format: 'string' },
+      { key: 'Currency', valueFrom: 'currency', format: 'string' },
+      { key: 'Initial Limit', valueFrom: 'initialLimit', format: 'string' },
+      { key: 'Outstanding', valueFrom: 'outstanding', format: 'string' },
+      { key: 'Changes', valueFrom: 'changes', format: 'string' },
+      { key: 'Total Plafond', valueFrom: 'totalPlafond', format: 'string' },
+    ];
+
+    const params = {
+      startDate: this.MisReportCPFacility.get('date1')?.value,
+      endDate: this.MisReportCPFacility.get('date2')?.value,
+      status: this.convertStatusToString(this.MisReportCPFacility.get('status')?.value),
+    };
+
+    this.misReportService
+      .generateMisReport(template_report_data, this.misReportService.getMisReportCPFacility(params), 'MIS_Report_Credit_Proposal_Facility')
+      .subscribe({
+        error: () => {
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Failed to generate document' });
+        },
+      });
+  }
 }
