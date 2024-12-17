1fba1180d3dc04d632b276a3895ec49e6f468650
Rendywan Agusvirta <rendayvirta96@gmail.com>
2024-12-17 10:42:59 +0700

    ..

--- src/main/webapp/app/entities/mis-report/credit-proposal-deviation/mis-report-credit-proposal-deviation.component.ts
+++ src/main/webapp/app/entities/mis-report/credit-proposal-deviation/mis-report-credit-proposal-deviation.component.ts
@@ -273,7 +273,7 @@ export class MisReportCreditProposalDeviationComponent extends AbstractExcelMISR
         this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Please, Select Date Range' });
         this.misReportService.setLoading(false);
         return;
-      } else if (!startDate1 && !endDate2 && !statuss) {
+      } else if (!startDate1 || !endDate2 || !statuss) {
         this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Please, Select Parameters' });
         this.misReportService.setLoading(false);
         return;
