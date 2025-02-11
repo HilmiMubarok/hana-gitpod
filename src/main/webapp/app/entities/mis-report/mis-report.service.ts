cd595a8476fc35202f346085f1aa74bc86a0bbe6
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2025-02-11 17:04:56 +0700

    add username parameter in mis cp insurance report

--- src/main/webapp/app/entities/mis-report/mis-report.service.ts
+++ src/main/webapp/app/entities/mis-report/mis-report.service.ts
@@ -7,7 +7,7 @@ import { createRequestOption } from 'app/core/request/request-util';
 
 @Injectable({ providedIn: 'root' })
 export class MisReportService {
-  constructor(private http: HttpClient, protected applicationConfigService: ApplicationConfigService) {}
+  constructor(private http: HttpClient, protected applicationConfigService: ApplicationConfigService) { }
 
   public loadingGenerateDocument: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   public generateDocumentLabel: BehaviorSubject<string> = new BehaviorSubject<string>('Generate Document');
@@ -58,6 +58,17 @@ export class MisReportService {
 
     return this.http
       .get<any>(this.applicationConfigService.getEndpointFor(MICROSERVICENAME.LOS + '/api/app-menu-status-item') + '/filterBy', {
+        params,
+        observe: 'response',
+      })
+      .pipe(map(res => res.body));
+  }
+
+  public getLovUsername(positionTypeId) {
+    const params = new HttpParams().set('positionTypeIds', positionTypeId);
+
+    return this.http
+      .get<any>(this.applicationConfigService.getEndpointFor(MICROSERVICENAME.LOS + '/api/mis/get-persons'), {
         params,
         observe: 'response',
       })
