f9c1d74f185153bbad9c06d0c73d68f48ece4713
Rizkioke <rizkirinaldi999@yahoo.com>
2025-03-05 09:41:54 +0700

    fix(mis-cp-slaloanops-report.component.ts): handle undefined properties in getTanggalEfektifFasilitas

--- src/main/webapp/app/entities/mis-report/mis-cp-slaloanops-report/mis-cp-slaloanops-report.component.ts
+++ src/main/webapp/app/entities/mis-report/mis-cp-slaloanops-report/mis-cp-slaloanops-report.component.ts
@@ -416,21 +416,25 @@ export class MisCpSlaloanopsReportComponent extends AbstractExcelMISReport imple
   private getTanggalEfektifFasilitas(prod: any, proposal = null) {
     switch (prod.pengajuan) {
       case 'New':
-        return this._getEarliestDate(this.getLoanOpsDistributionInDate(proposal));
-      case 'Renewal':
-        return (
-          `${this._formatDateSLA(prod.mainProduct.maturityDate)} s/d ${this._formatDateSLA(prod.mainProduct.proposeMaturityDate)}` || ''
-        );
-      case 'Renewal + Additional':
-        return this._formatDateSLA(prod.mainProduct.mainProduct[0].startPeriodDate) || '';
-      case 'Renewal + Decrease':
-        return this._formatDateSLA(prod.mainProduct.mainProduct[0].startPeriodDate) || '';
-      case 'Existing':
-        return this._formatDateSLA(prod.firstDisbursementDate) || '';
       case 'Additional / Top Up':
-        return this._getEarliestDate(this.getLoanOpsDistributionInDate(proposal));
+        return this._getEarliestDate(this.getLoanOpsDistributionInDate(proposal)) || '';
+
+      case 'Renewal':
+        return prod.mainProduct && prod.mainProduct.maturityDate && prod.mainProduct.proposeMaturityDate
+          ? `${this._formatDateSLA(prod.mainProduct.maturityDate)} s/d ${this._formatDateSLA(prod.mainProduct.proposeMaturityDate)}`
+          : '';
+
+      case 'Renewal + Additional':
+      case 'Renewal + Decrease':
+        return prod.mainProduct?.mainProduct?.length > 0 && prod.mainProduct.mainProduct[0].startPeriodDate
+          ? this._formatDateSLA(prod.mainProduct.mainProduct[0].startPeriodDate)
+          : '';
+
+      case 'Existing':
+        return prod.firstDisbursementDate ? this._formatDateSLA(prod.firstDisbursementDate) : '';
+
       default:
-        return prod.mainProduct.endPeriodRemark || '';
+        return prod.mainProduct?.endPeriodRemark || '';
     }
   }
 
