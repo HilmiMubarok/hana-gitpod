5bd4261219d1d32f692cd431fcfe48552daf16e1
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2025-03-21 14:23:36 +0700

    remove commented code

--- src/main/webapp/app/entities/mis-report/mis-dashboard/mis-dashboard.service.ts
+++ src/main/webapp/app/entities/mis-report/mis-dashboard/mis-dashboard.service.ts
@@ -13,14 +13,6 @@ export class MisDashboardService {
     this.endPoint = applicationConfigService.getEndpointFor(MICROSERVICENAME.LOS + '/api/dashboards/credit-proposal/credit-admin')
   }
 
-  // private formatDate(date = new Date()) {
-  //   const year = date.getFullYear();
-  //   const month = String(date.getMonth() + 1).padStart(2, '0');
-  //   const day = String(date.getDate()).padStart(2, '0');
-
-  //   return `${year}-${month}-${day}`;
-  // }
-
   getBarChartData(date: string) {
     const params = new HttpParams().set('date', date);
     return this.http.get<any>(this.endPoint, { params });
