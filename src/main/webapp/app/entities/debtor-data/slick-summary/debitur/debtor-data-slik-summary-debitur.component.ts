343ac9c4edb5b724bc78b36f6c932e8bbdbc949b
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2025-10-22 11:40:49 +0700

    update mapping limit

--- src/main/webapp/app/entities/debtor-data/slick-summary/debitur/debtor-data-slik-summary-debitur.component.ts
+++ src/main/webapp/app/entities/debtor-data/slick-summary/debitur/debtor-data-slik-summary-debitur.component.ts
@@ -470,7 +470,7 @@ export class DeborDataSlikSummaryDebiturComponent extends AbstractEntityMaterial
     };
     partySlik.partyId = this.partyId;
     partySlik.bank = item.bank;
-    partySlik.limit = item.limit === null ? 0 : Number(item.limit.toString().replace(/\./g, ''));
+    partySlik.limit = item.limit === null ? 0 : Number(item.limit.toString().replace(/[,.]/g, ''));
     partySlik.rate = item.rate == null ? 0 : Number(item.rate.toString().replace(' %', ''));
     partySlik.tenor = item.tenor == null ? 0 : Number(item.tenor.toString().replace(' bulan', ''));
     partySlik.outstanding = item.outstanding == null ? 0 : Number(item.outstanding.toString().replace(/\./g, ''));
